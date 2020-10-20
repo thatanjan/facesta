fd93abe8f7e72017b69516b7cd4551233f0f0cdb
thatAnjan <anjan.cules@gmail.com>
2020-10-20 17:19:59 +0600

    new user functionality added

---
+++ src/models/User.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const User_Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  data: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const User = mongoose.model('user', User_Schema)
+
+export default User
