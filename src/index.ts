1b699ae7a6f4e49287870060d955b0ad2b8ecb51
Anjan18 <anjanbiber@gmail.com>
2020-10-20 15:44:22 +0600

    express server is set

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import express, { Request, Response } from 'express'
 
+// mongoose key
 import mongoURI from './config/keys'
 
+// connect to mongoose
 mongoose
   .connect(mongoURI)
   .then(() => {
@@ -10,3 +13,17 @@ mongoose
   .catch((error) => {
     console.log(error)
   })
+
+// intialize express app
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world')
+})
+
+// starting express server
+app.listen(port, () => {
+  console.log(`server is running at ${port}`)
+})
