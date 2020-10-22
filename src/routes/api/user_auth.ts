010a487ee449ab8b48d236d599eb0e74ac158088
thatAnjan <anjan.cules@gmail.com>
2020-10-22 17:28:51 +0600

    user auth validation is done

--- src/routes/api/user_auth.ts
+++ src/routes/api/user_auth.ts
@@ -7,13 +7,15 @@ import passport from 'passport'
 import User from '../../models/User'
 import { secretKey } from '../../config/keys'
 
+// load input validation
 import validate_register_input from '../../validation/register'
+import validate_login_input from '../../validation/login'
 
 // interface for payload object for jwt
 export interface Payload {
-  id: string
-  name: string
-  avatar: any
+    id: string
+    name: string
+    avatar: any
 }
 
 const router = express.Router()
@@ -23,7 +25,7 @@ const router = express.Router()
 // @access Public
 
 router.get('/', (req: Request, res: Response) => {
-  res.json({ msg: 'user_auth works' })
+    res.json({ msg: 'user_auth works' })
 })
 
 // @route GET api/posts/test
@@ -31,58 +33,58 @@ router.get('/', (req: Request, res: Response) => {
 // @access Public
 
 const create_new_user = (request: Request, response: Response) => {
-  const avatar = gravatar.url(request.body.email, {
-    s: '200',
-    r: 'pg',
-    d: 'mm',
-  })
-
-  const user_model_data = {
-    name: request.body.name,
-    email: request.body.email,
-    avatar,
-    password: request.body.password,
-  }
-
-  const new_user: any = new User(user_model_data)
-  let new_user_password = new_user.password
-  console.log(new_user_password, typeof new_user_password)
-
-  bcryptjs.genSalt(10, (err, salt) => {
-    bcryptjs.hash(new_user_password, salt, (err, hash) => {
-      if (err) throw err
-      new_user_password = hash
-
-      // making the new user password to the hashed password
-
-      new_user.password = new_user_password
-      console.log(new_user_password)
-      new_user
-        .save()
-        .then((user: {}) => response.json(user))
-        .catch((error: any) => console.log(error, 12))
+    const avatar = gravatar.url(request.body.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
     })
-  })
+
+    const user_model_data = {
+        name: request.body.name,
+        email: request.body.email,
+        avatar,
+        password: request.body.password,
+    }
+
+    const new_user: any = new User(user_model_data)
+    let new_user_password = new_user.password
+    console.log(new_user_password, typeof new_user_password)
+
+    bcryptjs.genSalt(10, (err, salt) => {
+        bcryptjs.hash(new_user_password, salt, (err, hash) => {
+            if (err) throw err
+            new_user_password = hash
+
+            // making the new user password to the hashed password
+
+            new_user.password = new_user_password
+            console.log(new_user_password)
+            new_user
+                .save()
+                .then((user: {}) => response.json(user))
+                .catch((error: any) => console.log(error, 12))
+        })
+    })
 }
 
 router.post('/register', (req: Request, res: Response) => {
-  const { errors, isValid } = validate_register_input(req.body)
+    const { errors, isValid } = validate_register_input(req.body)
 
-  // check validation
-  if (!isValid) {
-    return res.status(400).json(errors)
-  }
-
-  User.findOne({ email: req.body.email }).then((user) => {
-    if (user) {
-      return res.status(400).json({ email: 'email already exist' })
-    } else {
-      // create a new user
-      create_new_user(req, res)
-      // console.log(124)
-      // create_new_user
+    // check validation
+    if (!isValid) {
+        return res.status(400).json(errors)
     }
-  })
+
+    User.findOne({ email: req.body.email }).then((user) => {
+        if (user) {
+            return res.status(400).json({ email: 'email already exist' })
+        } else {
+            // create a new user
+            create_new_user(req, res)
+            // console.log(124)
+            // create_new_user
+        }
+    })
 })
 
 // @route GET api/posts/test
@@ -94,48 +96,63 @@ router.post('/register', (req: Request, res: Response) => {
 // @access Public
 
 router.post('/login', ({ body }: Request, res: Response) => {
-  const email = body.email
-  const password = body.password
+    const { errors, isValid } = validate_login_input(body)
 
-  // console.log(email, password)
-  // // find user by <email  />
-
-  User.findOne({ email }).then((user: any) => {
-    if (!user) {
-      console.log('not found')
-      return res.status(404).json({ email: 'user not found' })
+    // check validation
+    if (!isValid) {
+        return res.status(400).json(errors)
     }
-    // check password is same or not
 
-    bcryptjs
-      .compare(password, user.password)
-      .then((isMatch) => {
-        console.log(isMatch)
-        if (isMatch) {
-          // user matched
+    const email = body.email
+    const password = body.password
 
-          const payload: Payload = {
-            id: user.id,
-            name: user.name,
-            avatar: user.avatar,
-          }
+    // console.log(email, password)
+    // // find user by <email  />
 
-          // sign token
-          jwt.sign(payload, secretKey, { expiresIn: 3600 }, (err, token) => {
-            if (err) {
-              console.log(err)
-            }
-            res.json({
-              success: true,
-              token: 'Bearer ' + token,
-            })
-          })
-        } else {
-          return res.status(404).json({ password: 'password incorrect' })
+    User.findOne({ email }).then((user: any) => {
+        // if the user not found
+        if (!user) {
+            errors.email = 'user not found'
+            return res.status(404).json(errors)
         }
-      })
-      .catch((err) => console.log(err))
-  })
+        // check password is same or not
+
+        bcryptjs
+            .compare(password, user.password)
+            .then((isMatch) => {
+                console.log(isMatch)
+                if (isMatch) {
+                    // user matched
+
+                    const payload: Payload = {
+                        id: user.id,
+                        name: user.name,
+                        avatar: user.avatar,
+                    }
+
+                    // sign token
+                    jwt.sign(
+                        payload,
+                        secretKey,
+                        { expiresIn: 3600 },
+                        (err, token) => {
+                            if (err) {
+                                console.log(err)
+                            }
+                            res.json({
+                                success: true,
+                                token: 'Bearer ' + token,
+                            })
+                        }
+                    )
+                } else {
+                    errors.password = 'Password incorrect'
+                    errors.password[0].toUpperCase()
+                    return res.status(404).json(errors)
+                }
+            })
+            .catch((err) => console.log(err))
+    })
 })
 
 // @route GET api/users/current
@@ -143,14 +160,14 @@ router.post('/login', ({ body }: Request, res: Response) => {
 // @access  Private
 
 router.get(
-  '/current',
-  passport.authenticate('jwt', { session: false }),
-  (req: any, res: Response) => {
-    res.json({
-      id: req._id,
-      name: req.name,
-    })
-  }
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req: any, res: Response) => {
+        res.json({
+            id: req._id,
+            name: req.name,
+        })
+    }
 )
 
 export default router
