ca1f9932b336d60263639dc8f97ab355e0be3558
thatAnjan <anjan.cules@gmail.com>
2020-10-22 23:46:02 +0600

    1. in src/inde profiles end point is changed to profile 2. in src/routes/api/profile.ts getting the current user profile functionality is added

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,13 @@ import mongoURI from './config/keys'
 
 // connect to mongoose
 mongoose
-  .connect(mongoURI)
-  .then(() => {
-    console.log('mongoose connected')
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+    .connect(mongoURI)
+    .then(() => {
+        console.log('mongoose connected')
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 // intialize express app
 const app = express()
@@ -41,9 +41,9 @@ JWT_strategy(passport)
 // app routes
 app.use('/api/user', user)
 app.use('/api/posts', posts)
-app.use('/api/profiles', profiles)
+app.use('/api/profile', profiles)
 
 // starting express server
 app.listen(port, () => {
-  console.log(`server is running at ${port}`)
+    console.log(`server is running at ${port}`)
 })
