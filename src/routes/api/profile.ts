863fca2a6a87a349bf9105e3cc9f5c4f0f3b6694
Anjan18 <anjanbiber@gmail.com>
2020-10-22 23:46:02 +0600

    1. in src/inde profiles end point is changed to profile 2. in src/routes/api/profile.ts getting the current user profile functionality is added

--- src/routes/api/profile.ts
+++ src/routes/api/profile.ts
@@ -1,13 +1,40 @@
 import express, { Request, Response } from 'express'
 
+import mongoose from 'mongoose'
+import passport from 'passport'
+
 const router = express.Router()
+
+import Profile from '../../models/Profile'
+import User from '../../models/User'
 
 // @route GET api/profile/test
 // @description tests profile route
 // @access private
 
-router.get('/', (req: Request, res: Response) => {
-  res.json({ msg: 'profiles works' })
-})
+// router.get('/', (req: Request, res: Response) => {
+//     res.json({ msg: 'profiles works' })
+// })
+
+// @route GET api/profile
+//  @description get current user profile
+// @access private
+
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req: any, res: Response) => {
+        const errors: any = {}
+        Profile.findOne({ user: req.user.id })
+            .then((profile) => {
+                if (!profile) {
+                    errors.no_profile = 'There is no profile for theis user'
+                    return res.status(404).json(errors)
+                }
+                res.json(profile)
+            })
+            .catch((err) => res.json(err))
+    }
+)
 
 export default router
