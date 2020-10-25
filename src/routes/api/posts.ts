cc328623418b06c8076d42aac65ae857063d32b2
Anjan18 <anjanbiber@gmail.com>
2020-10-25 23:36:02 +0600

    create post functionality is added.

--- src/routes/api/posts.ts
+++ src/routes/api/posts.ts
@@ -1,4 +1,11 @@
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
+import passport from 'passport'
+
+import Post from '../../models/Posts'
+
+// validation
+import validate_post_input from '../../validation/post'
 
 const router = express.Router()
 
@@ -7,7 +14,32 @@ const router = express.Router()
 // @access private
 
 router.get('/', (req: Request, res: Response) => {
-  res.json({ msg: 'posts works' })
+    res.json({ msg: 'posts works' })
 })
+
+// @route POST api/posts
+// @description  create post
+// @access private
+
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req: any, res: Response) => {
+        const { errors, isValid } = validate_post_input(req.body)
+
+        if (!isValid) {
+            return res.status(400).json(errors)
+        }
+
+        const new_post = new Post({
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id,
+        })
+
+        new_post.save().then((post: object) => res.json(post))
+    }
+)
 
 export default router
