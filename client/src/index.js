c42527f9f3309912e4204569557adfaaa87c5c1c
Anjan18 <anjanbiber@gmail.com>
2020-10-31 17:16:27 +0600

    css is reset by material ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<CssBaseline>
+			<App />
+		</CssBaseline>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
@@ -15,9 +17,9 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister()
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals(console.log)
