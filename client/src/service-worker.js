0f3b1d1aef298a53f9f57426802c912e69c8a80d
Anjan18 <anjanbiber@gmail.com>
2020-11-02 23:31:09 +0600

    a new theme added

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -54,8 +54,7 @@ registerRoute(
 registerRoute(
 	// Add in any other file extensions or routing criteria as needed.
 	({ url }) =>
-		url.origin === self.location.origin &&
-		url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+		url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
 	new StaleWhileRevalidate({
 		cacheName: 'images',
 		plugins: [
