29fb9dcc0fb658844b44ea41af93c6d90808088d
Anjan18 <anjanbiber@gmail.com>
2020-11-08 11:31:07 +0600

    Reducer for load user profile
    Profile actions and reducers are created to load the current user
    profile.
    
    userReducer.js file is renamed to authenticationReducer.js

---
+++ client/src/redux/actions/profileAction.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from 'redux/actions/types'
+
+export const setProfileLoading = () => ({
+	type: PROFILE_LOADING,
+})
+
+export const getCurrentProfile = () => dispatch => {
+	dispatch(setProfileLoading())
+
+	axios
+		.get('/api/profile')
+		.then(({ data }) => {
+			dispatch({
+				type: GET_PROFILE,
+				payload: data,
+			})
+		})
+		.catch(() => ({
+			type: GET_PROFILE,
+			payload: {},
+		}))
+}
--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
-import { authReducer } from 'redux/reducers/user/userReducer'
+import { authReducer } from 'redux/reducers/authentication/authenticationReducer'
+import profileReducer from 'redux/reducers/profile/profileReducer'
 
 export default combineReducers({
 	auth: authReducer,
+	profile: profileReducer,
 })
