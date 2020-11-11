2355b41530f45a538be22be1ae2e6bfabdfe4e44
thatAnjan <anjan.cules@gmail.com>
2020-11-11 16:43:01 +0600

    Private routes bug is fixed
    No one can access any page without logging in except authentication
    pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import DarkModeThemeProvider from 'themes/dark_light_mode'
-
 import PrivateRoutes from 'routes/PrivateRoutes'
 import PrivateAuthRoutes from 'routes/PrivateAuthRoutes'
-import AppBar from 'components/AppHeader/AppHeader'
 
 const App = () => {
 	return (
 		<>
-			<AppBar />
 			<DarkModeThemeProvider>
 				<PrivateRoutes />
 			</DarkModeThemeProvider>
