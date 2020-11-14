142b5884f309c83169fb09cc62726176ab71aa32
Anjan18 <anjanbiber@gmail.com>
2020-11-14 16:16:35 +0600

    Private route bug is fixed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Switch, Route, Redirect } from 'react-router-dom'
 
-import Paper from '@material-ui/core/Paper'
 import PrivateRoutes from 'routes/PrivateRoutes'
 import PrivateAuthRoutes from 'routes/PrivateAuthRoutes'
 
 const App = () => {
 	return (
 		<>
-			<Switch>
-				<PrivateRoutes />
-				<PrivateAuthRoutes />
-
-				{/* <Route render={() => <div children='invalid route' />} /> */}
-			</Switch>
+			<PrivateRoutes />
+			<PrivateAuthRoutes />
 		</>
 	)
 }
 
-const mapStateToProps = ({ auth: { isAuthenticated } }) => ({
-	authenticated: isAuthenticated,
-})
-
-export default connect(mapStateToProps)(App)
+export default App
--- client/src/HOC/PrivateRoute.jsx
+++ client/src/HOC/PrivateRoute.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 
-const PrivateRoute = ({ isAuthenticated, Component, ...rest }) => {
-	// console.log(isAuthenticated)
-	if (isAuthenticated) {
-		return <Route component={Component} {...rest} />
-	} else {
-		return <Redirect to='/authentication/login' />
-	}
-}
-
+// for authentication routes
 const AuthRoute = ({ isAuthenticated, Component, ...rest }) => {
 	if (isAuthenticated) {
 		return <Redirect to='/' />
 	} else {
 		return <Route component={Component} {...rest} />
+	}
+}
+
+// for other routes
+const PrivateRoute = ({ isAuthenticated, Component, ...rest }) => {
+	if (isAuthenticated) {
+		return <Route component={Component} {...rest} />
+	} else {
+		return <Redirect to='/authentication/login' />
 	}
 }
 
