18e6293f7a43e21f0eca5b7f7f54fe959264dadb
Anjan18 <anjanbiber@gmail.com>
2020-11-22 13:17:44 +0600

    1. Previous Horizontal menu is deleted. 2. A new one is added which is horizontally scrollable. 3. Some new options are added.

--- client/src/components/HorizontalMenu/HorizontalMenu.jsx
+++ client/src/components/HorizontalMenu/HorizontalMenu.jsx
@@ -1,35 +1,67 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import { nanoid } from 'nanoid'
 
-const buttonOption = ['About', 'Follwers', 'Follwing', 'Posts', 'More']
+const tabOption = [
+	'About',
+	'Follwers',
+	'Follwing',
+	'Posts',
+	'Videos',
+	'Shared',
+	'Page',
+	'Group',
+	'Favorite',
+]
 
-const useStyles = makeStyles(({ typography: { pxToRem } }) => ({
-	containerStyle: {
-		padding: `${pxToRem(10)} 0`,
+const a11yProps = index => {
+	return {
+		id: `scrollable-auto-tab-${index}`,
+		'aria-controls': `scrollable-auto-tabpanel-${index}`,
+	}
+}
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+		width: '100%',
+		backgroundColor: theme.palette.background.paper,
 	},
 }))
 
-export const HorizontalMenu = () => {
-	const { containerStyle } = useStyles()
+const HorizontalMenu = () => {
+	const classes = useStyles()
+	const [value, setValue] = React.useState(0)
+
+	const handleChange = (event, newValue) => {
+		console.log(newValue)
+		setValue(newValue)
+	}
+
 	return (
-		<>
-			<Grid className={containerStyle} container justify='space-around'>
-				{buttonOption.map(item => (
-					<Grid item>
-						<Button>
-							<Typography>{item}</Typography>
-						</Button>
-					</Grid>
-				))}
-			</Grid>
-		</>
+		<div className={classes.root}>
+			<AppBar position='static' color='default'>
+				<Tabs
+					value={value}
+					onChange={handleChange}
+					indicatorColor='secondary'
+					textColor='secondary'
+					variant='scrollable'
+					scrollButtons='auto'
+					aria-label='scrollable auto tabs example'
+				>
+					{tabOption.map((item, index) => (
+						<Tab key={nanoid()} label={item} {...a11yProps(index)} />
+					))}
+				</Tabs>
+			</AppBar>
+		</div>
 	)
 }
-
 const mapStateToProps = state => ({})
 
 const mapDispatchToProps = {}
