65a316aa2a1b0c76c385764265370746b1826582
Anjan18 <anjanbiber@gmail.com>
2020-11-26 06:52:00 +0600

    NavigationDrawer was having some padding and width problem which has been fixed.

--- client/src/components/Drawers/NavigationDrawer.jsx
+++ client/src/components/Drawers/NavigationDrawer.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import { makeStyles } from '@material-ui/core/styles'
 
 import NavigationDrawerList from 'components/Drawers/NavigationDrawerList'
 
 // actions
 import { openDrawer, closeDrawer } from 'redux/actions/drawerActions'
 
+const useStyles = makeStyles({
+	drawerStyle: {
+		width: '70vw',
+	},
+})
+
 export const NavigationDrawer = ({ isDrawerOpen, closeDrawer, openDrawer }) => {
+	const { drawerStyle } = useStyles()
 	return (
 		<>
 			<SwipeableDrawer
@@ -17,7 +25,9 @@ export const NavigationDrawer = ({ isDrawerOpen, closeDrawer, openDrawer }) => {
 				onClose={closeDrawer}
 				onOpen={openDrawer}
 			>
-				<NavigationDrawerList />
+				<div className={drawerStyle}>
+					<NavigationDrawerList />
+				</div>
 			</SwipeableDrawer>
 		</>
 	)
