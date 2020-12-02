465e49dc841c74369069bf66a3973e0111a5517e
Anjan18 <anjanbiber@gmail.com>
2020-12-02 15:34:38 +0600

    Title: 3 files merged from backup branch.

--- client/src/components/AboutSection/SubSection.jsx
+++ client/src/components/AboutSection/SubSection.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { Suspense, lazy, useState } from 'react'
+import { makeStyles } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Divider from '@material-ui/core/Divider'
 import Button from '@material-ui/core/Button'
 
 import SectionDetails from 'components/AboutSection/SectionDetails'
+
+const NewDetailForm = lazy(() => import('./NewDetailForm'))
 
 class Section {
 	constructor(name, Component) {
@@ -11,33 +16,74 @@ class Section {
 	}
 
 	addProps(props) {
-		// this.props = props
-		console.log(this)
+		this.props = props
+		this.props.buttonText = this.name
+		return this
+	}
+
+	addFormField(fields) {
+		this.formFields = fields
 		return this
 	}
 }
 
-const SubSection = ({ buttonText }) => {
+const useStyles = makeStyles(({ spacing }) => ({
+	buttonStyle: {
+		marginTop: spacing(4),
+	},
+	dividerStyle: {
+		marginTop: spacing(2),
+		height: spacing(0.3),
+		width: '100%',
+	},
+}))
+
+const SubSection = ({ buttonText, formFields }) => {
+	const { buttonStyle, dividerStyle } = useStyles()
+
+	const [isAddingNewDetail, setIsAddingNewDetail] = useState(false)
+
 	return (
 		<>
 			<SectionDetails />
-			<Divider light />
-			<Button variant='contained' color='secondary'>
-				Add a new {buttonText}
-			</Button>
+			<Divider className={dividerStyle} />
+
+			<Grid container justify='flex-end'>
+				<Grid item>
+					<Button
+						variant='contained'
+						color='secondary'
+						className={buttonStyle}
+						onClick={() => setIsAddingNewDetail(true)}
+						disabled={isAddingNewDetail}
+					>
+						Add a new {buttonText}
+					</Button>
+				</Grid>
+			</Grid>
+
+			{isAddingNewDetail && (
+				<Suspense fallback={<CircularProgress />}>
+					<NewDetailForm formFields={formFields} doneAdding={setIsAddingNewDetail} />
+				</Suspense>
+			)}
 		</>
 	)
 }
 
-const education = new Section('Education', SubSection).addProps({
-	buttonText: this.name,
-})
-const experience = new Section('Experience', SubSection).addProps({
-	buttonText: this.name,
-})
-const placesLived = new Section('Places Lived', SubSection).addProps({
-	buttonText: this.name,
-})
+const addButtonText = text => ({ buttonText: `Add a new ${text}` })
+
+const education = new Section('Education', SubSection)
+	.addProps(addButtonText('School'))
+	.addFormField(['School'])
+
+const experience = new Section('Experience', SubSection)
+	.addProps(addButtonText('Experience'))
+	.addFormField(['Company'])
+
+const placesLived = new Section('Places Lived', SubSection)
+	.addProps(addButtonText('Place'))
+	.addFormField(['city', 'town', 'country'])
 
 const options = [education, experience, placesLived]
 
