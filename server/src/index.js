088843cf80a5e2a6267fca70423d943fdb23f2a2
thatAnjan <anjan.cules@gmail.com>
2020-12-09 12:28:38 +0600

    Title: Graphql appollo server setup. Body: Graphql server is created. Express graphql server is removed. A root typedef and resolver is created.

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,13 @@
 import mongoose from 'mongoose'
 import express from 'express'
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-express'
 import jwt from 'express-jwt'
 import bodyParser from 'body-parser'
-import passport from 'passport'
-import { graphqlHTTP } from 'express-graphql'
-
-import schema from 'schema/schema'
-import { JWT_strategy } from 'config/passport.js'
 
 import mongoURI, { secretKey } from 'config/keys'
 
-const { printSchema } = require('graphql')
-
-console.log(printSchema(schema))
+import typeDefs from 'graphql/typedefs'
+import resolvers from 'graphql/resolvers'
 
 mongoose
     .connect(mongoURI)
@@ -25,26 +20,34 @@ mongoose
 
 const app = express()
 
+app.use(
+    jwt({
+        secret: secretKey,
+        algorithms: ['HS256'],
+        credentialsRequired: false,
+    })
+)
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
+
+const server = new ApolloServer({
+    schema,
+    context: ({ request }) => {
+        const user = request || null
+        return { user }
+    },
+})
+
+server.applyMiddleware({ app })
+
 const port = process.env.PORT || 8000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(
-    '/graphql',
-    jwt({
-        secret: secretKey,
-        algorithms: ['HS256'],
-        credentialsRequired: false,
-    }),
-    (req, res, next) =>
-        graphqlHTTP({
-            schema,
-            graphiql: true,
-            context: { user: req.user || 'con' },
-        })(req, res, next)
-)
-
-app.listen(port, () => {
+app.listen({ port }, () => {
     console.log(`server is running at ${port}`)
 })
