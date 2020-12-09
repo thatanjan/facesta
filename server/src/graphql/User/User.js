ee471baa6e74a2a166d2c2042920733e4a8971a6
thatAnjan <anjan.cules@gmail.com>
2020-12-09 18:25:09 +0600

    loggin user is not working

--- server/src/graphql/User/User.js
+++ server/src/graphql/User/User.js
@@ -1,7 +1,8 @@
 import UserType from './UserType'
 import merge from 'lodash/merge'
 import loginUser from 'graphql/User/loginUser'
+import registerUser from 'graphql/User/registerUser'
 
 export const UserTypeDefs = [UserType]
 
-export const UserResolvers = merge(loginUser)
+export const UserResolvers = merge(loginUser, registerUser)
