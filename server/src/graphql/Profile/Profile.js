8d01e7a7bb79ecc2a1ba53cfc7458f3f26b2ef1e
thatAnjan <anjan.cules@gmail.com>
2020-12-12 23:52:46 +0600

    Title: User Profile social media links add and update. Body: User will be able to add and update their social media links in their profile.

--- server/src/graphql/Profile/Profile.js
+++ server/src/graphql/Profile/Profile.js
@@ -5,7 +5,13 @@ import ProfileType from './ProfileType'
 import updatePersonal from './updatePersonal'
 import getPersonal from './getPersonal'
 import addToProfile from './addToProfile'
+import updateSocial from './updateSocial'
 
 export const ProfileTypedefs = [ProfileType]
 
-export const ProfileResolvers = merge(updatePersonal, getPersonal, addToProfile)
+export const ProfileResolvers = merge(
+    updatePersonal,
+    getPersonal,
+    addToProfile,
+    updateSocial
+)
