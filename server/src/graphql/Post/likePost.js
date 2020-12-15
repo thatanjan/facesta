6f5d8a00279ca507b4fc551eb9f0c3963120784e
thatAnjan <anjan.cules@gmail.com>
2020-12-15 17:58:45 +0600

    Title: the whole file is refactored. Body: Each mutation method will call the main function and the function will return a async function which will execute the mutation.

--- server/src/graphql/Post/likePost.js
+++ server/src/graphql/Post/likePost.js
@@ -5,43 +5,66 @@ const queryPostLikes = async (model, id) => await model.findById(id, 'likes')
 
 const hasLiked = (array, id) => array.includes(id)
 
+const operationCheck = (operation) => {
+    if (operation === 'like') {
+        return true
+    }
+
+    if (operation === 'removeLike') {
+        return false
+    }
+}
+
+const modifyLikes = (operation, array, id) => {
+    if (operationCheck(operation)) {
+        array.push(id)
+        console.log(array)
+    }
+
+    if (!operationCheck(operation)) {
+        console.table([operation, array, id])
+        array.pull(id)
+    }
+}
+
+const responseMessage = (operation) => {
+    if (operationCheck(operation)) {
+        return sendMessage(true, 'you have liked this post')
+    }
+
+    if (!operationCheck(operation)) {
+        return sendMessage(true, 'you have remove like from the post')
+    }
+}
+
+const mainFunction = (operation) => {
+    return async (_, { input: { id: postId } }, { user: { id } }) => {
+        const Post = createPostModel(id)
+
+        const likesQuery = await queryPostLikes(Post, postId)
+
+        const { likes } = likesQuery
+
+        if (operation === 'like' && hasLiked(likes, id)) {
+            return sendMessage(false, 'you have already liked this post')
+        }
+
+        if (operation === 'removeLike' && !hasLiked(likes, id)) {
+            return sendMessage(false, 'you have not liked this post.')
+        }
+
+        modifyLikes(operation, likes, id)
+
+        likesQuery.save()
+
+        return responseMessage(operation)
+    }
+}
+
 const resolver = {
     Mutation: {
-        likePost: async (_, { input: { id: postId } }, { user: { id } }) => {
-            const Post = createPostModel(id)
-
-            const likesQuery = await queryPostLikes(Post, postId)
-
-            const { likes } = likesQuery
-
-            if (likes.includes(id)) {
-                // if (hasLiked(likes, id)) {
-                return sendMessage(false, 'you have already liked this post')
-            }
-
-            likes.push(id)
-
-            likesQuery.save()
-
-            return sendMessage(true, 'you have liked this post')
-        },
-        removeLike: async (_, { input: { id: postId } }, { user: { id } }) => {
-            const Post = createPostModel(id)
-
-            const likesQuery = await queryPostLikes(Post, postId)
-
-            const { likes } = likesQuery
-
-            if (!hasLiked(likes, id)) {
-                return sendMessage(false, 'you have not liked this post.')
-            }
-
-            likes.pull(id)
-
-            likesQuery.save()
-
-            return sendMessage(true, 'you have remove the like from the post')
-        },
+        likePost: mainFunction('like'),
+        removeLike: mainFunction('removeLike'),
     },
 }
 
