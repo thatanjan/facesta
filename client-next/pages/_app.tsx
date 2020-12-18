5cdb7e1d486b801dc45d9657217ecabe619703fc
Anjan18 <anjanbiber@gmail.com>
2020-12-18 16:45:44 +0600

    typescript error is removed from _app.tsx and api folder is deleted

--- client-next/pages/_app.tsx
+++ client-next/pages/_app.tsx
@@ -1,7 +1,9 @@
+import type { AppProps } from 'next/app'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />
 }
 
 export default MyApp
