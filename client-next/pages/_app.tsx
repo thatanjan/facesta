5a11f544eebc6fe88be35fef11bc7e72233dc7ae
thatAnjan <anjan.cules@gmail.com>
2020-12-19 17:56:18 +0600

    title tag is removed from the head

--- client-next/pages/_app.tsx
+++ client-next/pages/_app.tsx
@@ -19,7 +19,6 @@ export default function MyApp(props: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>My page</title>
 				<meta
 					name='viewport'
 					content='minimum-scale=1, initial-scale=1, width=device-width'
