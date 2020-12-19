190cc9be94475069f9ff4aa3e4f22da744791d80
thatAnjan <anjan.cules@gmail.com>
2020-12-19 14:38:27 +0600

    material ui is setup

--- client-next/pages/_app.tsx
+++ client-next/pages/_app.tsx
@@ -1,9 +1,35 @@
 import type { AppProps } from 'next/app'
+import React from 'react'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from 'themes/theme'
 
-import '../styles/globals.css'
+export default function MyApp(props: AppProps) {
+	const { Component, pageProps } = props
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	React.useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles: any = document.querySelector('#jss-server-side')
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles)
+		}
+	}, [])
+
+	return (
+		<>
+			<Head>
+				<title>My page</title>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width'
+				/>
+			</Head>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	)
 }
-
-export default MyApp
