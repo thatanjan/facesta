3845c434563387e041083105699f0d2c78a96b16
Anjan18 <anjanbiber@gmail.com>
2020-12-20 12:32:17 +0600

    a new graphql client is created for graphql-request

---
+++ client-next/graphql/graphqlClient.ts
@@ -0,0 +1,16 @@
+import { GraphQLClient } from 'graphql-request'
+const endpoint = 'http://localhost:8000/graphql'
+
+const token = () => {
+	const jwtToken = localStorage.getItem('jwt')
+
+	return jwtToken || ''
+}
+
+const graphQlClient = new GraphQLClient(endpoint, {
+	headers: {
+		authorization: token(),
+	},
+})
+
+export default graphQlClient
