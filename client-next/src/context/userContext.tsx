9792777a047a6ba8c7094eb8e580cdc9593af2dc
Anjan18 <anjanbiber@gmail.com>
2020-12-22 07:56:08 +0600

    Body: User context will only change if user state chnages.

--- client-next/src/context/userContext.tsx
+++ client-next/src/context/userContext.tsx
@@ -1,5 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react'
-import jwtDecode, { JwtPayload } from 'jwt-decode'
+import React, { createContext, useState } from 'react'
 
 interface UserInterface {
 	name: string
@@ -16,38 +15,8 @@ export const UserContext = createContext({})
 const UserContextProvider: React.FC = ({ children }) => {
 	const [user, setUser] = useState(initialState)
 
-	const [shouldUpdate, setShouldUpdate] = useState(false)
-
-	useEffect(() => {}, [shouldUpdate])
-
-	const LSChecker = () => {
-		try {
-			localStorage.setItem('test', 'test')
-			localStorage.removeItem('test')
-			return true
-		} catch (e) {
-			return false
-		}
-	}
-
-	const [LocalStorage, setLocalStorage] = useState('')
-
-	setTimeout(() => {
-		if (LSChecker()) {
-			setLocalStorage(localStorage.jwt)
-		}
-	}, 1)
-
-	useEffect(() => {
-		if (LocalStorage) {
-			const { name, id } = jwtDecode<UserInterface>(LocalStorage)
-
-			setUser({ name, id })
-		}
-	}, [LocalStorage])
-
 	return (
-		<UserContext.Provider value={[user, setUser, shouldUpdate, setShouldUpdate]}>
+		<UserContext.Provider value={[user, setUser]}>
 			{children}
 		</UserContext.Provider>
 	)
