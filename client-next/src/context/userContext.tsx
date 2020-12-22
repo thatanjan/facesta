b6ac211fdbe0d9d81e9bb26379ca8a7a53c64028
Anjan18 <anjanbiber@gmail.com>
2020-12-22 06:57:47 +0600

    Title: User context change based on shouldUpdate state. Body: if shouldUpdate state changes then a function will look for token and then decodd it. Decoded data will be stored in context.

--- client-next/src/context/userContext.tsx
+++ client-next/src/context/userContext.tsx
@@ -1,22 +1,53 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import jwtDecode, { JwtPayload } from 'jwt-decode'
 
 interface UserInterface {
-	userName: string
+	name: string
 	id: string
 }
 
 const initialState: UserInterface = {
-	userName: '',
+	name: '',
 	id: '',
 }
 
-const UserContext = createContext({})
+export const UserContext = createContext({})
 
 const UserContextProvider: React.FC = ({ children }) => {
 	const [user, setUser] = useState(initialState)
 
+	const [shouldUpdate, setShouldUpdate] = useState(false)
+
+	useEffect(() => {}, [shouldUpdate])
+
+	const LSChecker = () => {
+		try {
+			localStorage.setItem('test', 'test')
+			localStorage.removeItem('test')
+			return true
+		} catch (e) {
+			return false
+		}
+	}
+
+	const [LocalStorage, setLocalStorage] = useState('')
+
+	setTimeout(() => {
+		if (LSChecker()) {
+			setLocalStorage(localStorage.jwt)
+		}
+	}, 1)
+
+	useEffect(() => {
+		if (LocalStorage) {
+			const { name, id } = jwtDecode<UserInterface>(LocalStorage)
+
+			setUser({ name, id })
+		}
+	}, [LocalStorage])
+
 	return (
-		<UserContext.Provider value={[user, setUser]}>
+		<UserContext.Provider value={[user, setUser, shouldUpdate, setShouldUpdate]}>
 			{children}
 		</UserContext.Provider>
 	)
