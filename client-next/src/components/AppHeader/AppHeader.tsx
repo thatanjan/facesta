75378b6b12b737b1803c8f2fc5372401b6260206
Anjan18 <anjanbiber@gmail.com>
2020-12-29 09:03:43 +0600

    react suspense is removed

--- client-next/src/components/AppHeader/AppHeader.tsx
+++ client-next/src/components/AppHeader/AppHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, lazy } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { styled, makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
