7f16d78b020bcbacc2e78fdd05e2631d6fef8b01
thatAnjan <anjan.cules@gmail.com>
2020-12-29 12:49:17 +0600

    if the text input is undefined then will return empty string

--- client-next/src/utils/splitText.ts
+++ client-next/src/utils/splitText.ts
@@ -4,7 +4,11 @@ interface Params {
 	divider: string
 }
 
-const splitText = ({ text, position, divider }: Params) => {
+const splitText = ({ text, position, divider }: Params): string => {
+	if (!text) {
+		return ''
+	}
+
 	const mainText = text
 
 	const words: string[] = mainText.split(divider)
