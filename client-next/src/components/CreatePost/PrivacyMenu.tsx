886d855dd255b699580fb75eac1229b248989940
thatAnjan <anjan.cules@gmail.com>
2020-12-29 09:12:49 +0600

    a menu for selecting privacy

---
+++ client-next/src/components/CreatePost/PrivacyMenu.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Fade from '@material-ui/core/Fade'
+import { makeStyles } from '@material-ui/core/styles'
+import FaceIcon from '@material-ui/icons/Face'
+
+import { AnyObject } from 'interfaces/global'
+
+const useStyles = makeStyles((theme: any) => ({
+	menuContainerStyle: {
+		marginTop: theme.spacing(2),
+	},
+	privacyName: {
+		marginLeft: theme.spacing(1),
+	},
+}))
+
+const PrivacyMenu = () => {
+	// const optionRef = createRef()
+
+	const [selectedOption, setSelectedOption] = useState('Privacy')
+
+	const [anchorEl, setAnchorEl] = useState(null)
+	const open = Boolean(anchorEl)
+
+	const handleClick = (event: AnyObject) => {
+		setAnchorEl(event.currentTarget)
+	}
+
+	const handleClose = ({ target }: AnyObject) => {
+		// console.log(event.target)
+
+		if (target.firstChild) {
+			setSelectedOption(target.firstChild.data)
+		}
+
+		setAnchorEl(null)
+	}
+
+	const { menuContainerStyle, privacyName } = useStyles()
+
+	return (
+		<div className={menuContainerStyle}>
+			<Button aria-controls='fade-menu' aria-haspopup='true' onClick={handleClick}>
+				<FaceIcon /> <span className={privacyName}> {selectedOption}</span>
+			</Button>
+			<Menu
+				id='fade-menu'
+				anchorEl={anchorEl}
+				keepMounted
+				open={open}
+				onClose={handleClose}
+				TransitionComponent={Fade}
+			>
+				<MenuItem onClick={handleClose}>Public</MenuItem>
+				<MenuItem onClick={handleClose}>Friends</MenuItem>
+				<MenuItem onClick={handleClose}>Only Me</MenuItem>
+			</Menu>
+		</div>
+	)
+}
+
+export default PrivacyMenu
