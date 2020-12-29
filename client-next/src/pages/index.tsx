d530c3f614471fecdf89e0ec8bbb23f0f57f4a25
thatAnjan <anjan.cules@gmail.com>
2020-12-29 16:29:06 +0600

    drawer and content is added

--- client-next/src/pages/index.tsx
+++ client-next/src/pages/index.tsx
@@ -1,7 +1,17 @@
 import Head from 'next/head'
 import Typography from '@material-ui/core/Typography'
+import useGetUser from 'hooks/userhooks'
+
+import PageLayout from 'HOC/PageLayoutComponent'
+
+import NavigationDrawerList from 'components/Drawers/NavigationDrawerList'
+import Content from 'components/Home/HomepageComponent'
+
+const OtherContent = () => <h1>hello world</h1>
 
 const Home = () => {
+	const { name } = useGetUser()
+
 	return (
 		<div className='home'>
 			<Head>
@@ -9,10 +19,11 @@ const Home = () => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<Typography variant='h1'>Dev Book</Typography>
-			<Typography variant='h4'>
-				A social media application for Developers
-			</Typography>
+			<PageLayout
+				Drawer={() => <NavigationDrawerList />}
+				Content={() => <Content />}
+				RightSection={() => <OtherContent />}
+			/>
 		</div>
 	)
 }
