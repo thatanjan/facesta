5beefd8ea6d05b81591707ad2b376d558c0ef4d3
thatAnjan <anjan.cules@gmail.com>
2020-12-30 07:29:40 +0600

    a button to allow the user to edit their profile

---
+++ client-next/src/components/Buttons/EditButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import EditIcon from '@material-ui/icons/Edit'
+import Button from '@material-ui/core/Button'
+
+const EditButton = () => {
+	return <Button startIcon={<EditIcon />}>Edit Profile</Button>
+}
+
+export default EditButton
