3c22bbe1b3ccc8dfe0599f2b4fab3ab7baa54118
thatAnjan <anjan.cules@gmail.com>
2021-01-06 21:36:53 +0600

    a nextjs link component combined with material-ui style is created

---
+++ client-next/src/components/Links/MuiLink.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react'
+import clsx from 'clsx'
+import { useRouter } from 'next/router'
+import NextLink from 'next/link'
+import { makeStyles } from '@material-ui/core/styles'
+
+import { AnyObject } from 'interfaces/global'
+
+const useStyles = makeStyles({
+	linkStyle: {
+		color: 'inherit',
+		textDecoration: 'none',
+	},
+})
+
+const NextComposed = React.forwardRef(function NextComposed(
+	props: any,
+	ref: any
+) {
+	const { as, href, ...other } = props
+
+	return (
+		<NextLink href={href} as={as}>
+			<a ref={ref} {...other} />
+		</NextLink>
+	)
+})
+
+interface Props extends AnyObject {
+	MuiComponent: Function
+	href: string | any
+}
+
+function Link(props: Props) {
+	const { linkStyle } = useStyles()
+	const {
+		MuiComponent,
+		href,
+		activeClassName = 'active',
+		className: classNameProps,
+		innerRef,
+		naked,
+		...other
+	} = props
+
+	const router = useRouter()
+	const pathname = typeof href === 'string' ? href : href.pathname
+	const className = clsx(classNameProps, linkStyle, {
+		[activeClassName]: router.pathname === pathname && activeClassName,
+	})
+
+	if (naked) {
+		return (
+			<NextComposed className={className} ref={innerRef} href={href} {...other} />
+		)
+	}
+
+	return (
+		<MuiComponent
+			component={NextComposed}
+			className={className}
+			ref={innerRef}
+			href={href}
+			{...other}
+		/>
+	)
+}
+
+export default React.forwardRef((props: Props, ref: any) => (
+	<Link {...props} innerRef={ref} />
+))
