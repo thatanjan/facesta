6ade8c6f3e4c4eec7fed1f52b267281e85d22d3b
Anjan18 <anjanbiber@gmail.com>
2021-01-06 19:05:54 +0600

    new getUser query added

--- server/src/graphql/User/User.js
+++ server/src/graphql/User/User.js
@@ -3,7 +3,8 @@ import merge from 'lodash/merge'
 import loginUser from 'graphql/User/loginUser'
 import registerUser from 'graphql/User/registerUser'
 import deleteUser from 'graphql/User/deleteUser'
+import getUser from 'graphql/User/getUser'
 
 export const UserTypeDefs = [UserType]
 
-export const UserResolvers = merge(loginUser, registerUser, deleteUser)
+export const UserResolvers = merge(loginUser, registerUser, deleteUser, getUser)
