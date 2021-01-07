de838711e08bf3f8c09292c517f72390a9d1057f
Anjan18 <anjanbiber@gmail.com>
2021-01-07 17:23:28 +0600

    refactored and two more query resolver added

--- server/src/graphql/Profile/getPersonal.js
+++ server/src/graphql/Profile/getPersonal.js
@@ -1,12 +1,25 @@
 import Profile from 'models/Profile'
 
+const PERSONAL = 'personal'
+const EXPERIENCE = 'experience'
+const EDUCATION = 'education'
+const SOCIAL = 'social'
+
+const resolverFunction = (field) => {
+    return async (_, { Input: { userId } }) => {
+        const query = await Profile.findOne({ user: userId }, field)
+
+        console.log(query)
+
+        return query[`${field}`]
+    }
+}
+
 const resolver = {
     Query: {
-        getPersonal: async (_, { Input: { id } }) => {
-            const { personal } = await Profile.findOne({ user: id }, 'personal')
-
-            return personal
-        },
+        getPersonal: resolverFunction(PERSONAL),
+        getExperience: resolverFunction(EXPERIENCE),
+        getEducation: resolverFunction(EDUCATION),
     },
 }
 
