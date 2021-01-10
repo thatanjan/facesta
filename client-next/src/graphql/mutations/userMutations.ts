32a2895f4f68f1feb3e90aa3e521fe496955c22d
Anjan18 <anjanbiber@gmail.com>
2021-01-10 23:53:38 +0600

    updatePersonal mutation is refactored and fixed

--- client-next/src/graphql/mutations/userMutations.ts
+++ client-next/src/graphql/mutations/userMutations.ts
@@ -1,9 +1,16 @@
 import { gql } from 'graphql-request'
+// dateOfBirth: $dateOfBirth, website:$website, status: $status, location:$location, skills: $skills,
+// $dateOfBirth: Date
+// ,$website: String
+// ,$status: String
+// ,$location: String
+// ,$skills: [String!]
 
-export const updatePersonal = (output: string) => gql`
-	updatePersonal($userId:ID!){
-		updatePersonal(Input: { userId: $userId }){
-			${output}
+export const updatePersonal = (output: string): string => gql`
+	mutation	updatePersonal($bio: String)
+		{
+					  updatePersonal(Input: {bio:$bio}){
+				${output}
+			}
 		}
-	}
 `
