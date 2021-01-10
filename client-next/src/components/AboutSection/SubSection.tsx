925ae292085f1249689a03185ff913888f3f2cc5
thatAnjan <anjan.cules@gmail.com>
2021-01-10 12:45:24 +0600

    Section class is  moved to new file and some refactoring

--- client-next/src/components/AboutSection/SubSection.tsx
+++ client-next/src/components/AboutSection/SubSection.tsx
@@ -7,9 +7,9 @@ import Button from '@material-ui/core/Button'
 
 import SectionDetails from 'components/AboutSection/SectionDetails'
 
-const NewDetailForm = dynamic(() => import('./NewDetailForm'))
+import { PERSONAL } from './AcoordionOption'
 
-const PERSONAL = 'personal'
+const NewDetailForm = dynamic(() => import('./NewDetailForm'))
 
 const useStyles = makeStyles(({ spacing }) => ({
 	buttonStyle: {
@@ -53,58 +53,4 @@ const SubSection = ({ buttonText, formFields, name }: any) => {
 	)
 }
 
-const addButtonText = (text: string) => ({ buttonText: `Add a new ${text}` })
-
-class Section {
-	name: string
-
-	Component: Function
-
-	// eslint-disable-next-line
-	props: any
-
-	formFields: any
-
-	hook: Function
-
-	constructor(name: string) {
-		this.name = name
-		this.Component = SubSection
-	}
-
-	addProps(props: any) {
-		this.props = props
-		this.props.buttonText = this.name
-		return this
-	}
-
-	addFormField(fields: any) {
-		this.formFields = fields
-		return this
-	}
-
-	addHook(hook: Function) {
-		this.hook = hook
-		return this
-	}
-}
-
-export const personal = new Section(PERSONAL)
-	.addProps(addButtonText('Change details'))
-	.addFormField(['date of Birth', 'bio'])
-
-export const education = new Section('Education')
-	.addProps(addButtonText('School'))
-	.addFormField(['School'])
-
-export const experience = new Section('Experience')
-	.addProps(addButtonText('Experience'))
-	.addFormField(['Company'])
-
-export const placesLived = new Section('Places Lived')
-	.addProps(addButtonText('Place'))
-	.addFormField(['city', 'town', 'country'])
-
-export const options: Section[] = [personal, education, experience, placesLived]
-
-export default options
+export default SubSection
