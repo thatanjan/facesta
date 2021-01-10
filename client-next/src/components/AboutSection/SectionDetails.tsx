8e0930b71cfe6ffef9726d473f391ec0a723a5fd
Anjan18 <anjanbiber@gmail.com>
2021-01-10 17:30:12 +0600

    Title: Pesonal detail section Body: A seperate Section for Personal details which will show the personal details.

--- client-next/src/components/AboutSection/SectionDetails.tsx
+++ client-next/src/components/AboutSection/SectionDetails.tsx
@@ -3,6 +3,7 @@ import Box from '@material-ui/core/Box'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
+import { nanoid } from 'nanoid'
 
 const useStyles = makeStyles((theme: Theme) => ({
 	box: {
@@ -22,6 +23,9 @@ const useStyles = makeStyles((theme: Theme) => ({
 const EachField = ({ property, value }: any) => {
 	const { fieldContainer, propertyField, colon } = useStyles()
 
+	if (!value) {
+		return null
+	}
 	return (
 		<Grid container className={fieldContainer}>
 			<Grid item className={propertyField}>
@@ -35,6 +39,39 @@ const EachField = ({ property, value }: any) => {
 				<Typography>{value}</Typography>
 			</Grid>
 		</Grid>
+	)
+}
+
+export const PersonalDetails = ({ data }: any) => {
+	const personalDetailsField = [
+		'name',
+		'bio',
+		'date of birth',
+		'status',
+		'website',
+		'skills',
+		'location',
+	]
+
+	const newData = data
+	const { box } = useStyles()
+
+	const skills: string[] = newData.skill
+
+	let skillsString: string | undefined
+
+	if (skills && typeof skills === 'array' && skills.length >= 1) {
+		skillsString = skills.join(' ')
+	}
+
+	newData.skills = skillsString
+
+	return (
+		<Box className={box}>
+			{personalDetailsField.map((item: string) => (
+				<EachField property={item} value={newData[`${item}`]} key={nanoid()} />
+			))}
+		</Box>
 	)
 }
 
