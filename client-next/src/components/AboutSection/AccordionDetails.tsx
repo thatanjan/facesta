cbe15be6e74b6508dddd150036f4493444415e62
thatAnjan <anjan.cules@gmail.com>
2021-01-12 17:33:39 +0600

    userId is passed in object

--- client-next/src/components/AboutSection/AccordionDetails.tsx
+++ client-next/src/components/AboutSection/AccordionDetails.tsx
@@ -17,7 +17,7 @@ const Details = ({
 	const [isAddingNewDetail, setIsAddingNewDetail] = useState(false)
 
 	const userId = '5ff9939e53c3e8c7a2c4a833'
-	const { error, data } = hook(userId)
+	const { data, error } = hook({ userId })
 
 	const newProps = {
 		...props,
@@ -25,7 +25,6 @@ const Details = ({
 		setIsAddingNewDetail,
 		...others,
 	}
-	console.log(data)
 
 	if (error) return <div>failed to load</div>
 	if (!data) return <div>loading...</div>
