d3347fd00b4c99d0d57230a0caf83c09b4ef0e60
Anjan18 <anjanbiber@gmail.com>
2021-01-13 14:09:04 +0600

    input form will add new string to allskills array and will be rendered in the dom

--- client-next/src/components/arrayChips/chipsForm.tsx
+++ client-next/src/components/arrayChips/chipsForm.tsx
@@ -1,55 +1,64 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field } from 'formik'
 import { Button, LinearProgress } from '@material-ui/core'
 import { TextField } from 'formik-material-ui'
+import { List } from 'immutable'
 
 interface Values {
 	skill: string
 }
 
 const ChipsForm = () => {
+	const [allSkills, setAllSkills] = useState<string[]>([])
 	return (
-		<Formik
-			initialValues={{
-				skill: '',
-			}}
-			validate={values => {
-				const errors: Partial<Values> = {}
-				if (!values.skill) {
-					errors.skill = 'Required'
-				}
-				return errors
-			}}
-			onSubmit={(values, { setSubmitting }) => {
-				setTimeout(() => {
-					setSubmitting(false)
+		<>
+			<Formik
+				initialValues={{
+					skill: '',
+				}}
+				validate={values => {
+					const errors: Partial<Values> = {}
+					if (!values.skill) {
+						errors.skill = 'Required'
+					}
+					return errors
+				}}
+				onSubmit={({ skill }, { setSubmitting }) => {
+					setTimeout(() => {
+						setAllSkills([...allSkills, skill])
+						allSkills.push(skill)
+						setSubmitting(false)
 
-					// eslint-disable-next-line no-alert
-					alert(JSON.stringify(values, null, 2))
-				}, 500)
-			}}
-		>
-			{({ submitForm, isSubmitting }) => (
-				<Form>
-					<Field
-						component={TextField}
-						type='text'
-						label='add a new skill'
-						name='skill'
-					/>
-					{isSubmitting && <LinearProgress />}
-					<br />
-					<Button
-						variant='contained'
-						color='primary'
-						disabled={isSubmitting}
-						onClick={submitForm}
-					>
-						add
-					</Button>
-				</Form>
-			)}
-		</Formik>
+						// eslint-disable-next-line no-alert
+						alert(JSON.stringify(skill, null, 2))
+					}, 500)
+				}}
+			>
+				{({ submitForm, isSubmitting }) => (
+					<>
+						<Field
+							component={TextField}
+							type='text'
+							label='add a new skill'
+							name='skill'
+						/>
+						{isSubmitting && <LinearProgress />}
+						<br />
+						<Button
+							variant='contained'
+							color='primary'
+							disabled={isSubmitting}
+							onClick={submitForm}
+						>
+							add
+						</Button>
+					</>
+				)}
+			</Formik>
+			{allSkills.map((x, y) => (
+				<div key={x}>{x}</div>
+			))}
+		</>
 	)
 }
 
