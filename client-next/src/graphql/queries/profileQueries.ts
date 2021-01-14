afb9b153565fbfb523e01b03fbc64d6e72ccc399
Anjan18 <anjanbiber@gmail.com>
2021-01-14 13:15:36 +0600

    more output is added

--- client-next/src/graphql/queries/profileQueries.ts
+++ client-next/src/graphql/queries/profileQueries.ts
@@ -1,6 +1,6 @@
 import { gql } from 'graphql-request'
 
-const expectedOutput = 'dateOfBirth skills name bio'
+const expectedOutput = 'dateOfBirth skills name bio website status location'
 
 export const getPersonalData = (output?: string | undefined): string => gql`
 	 query getPersonalData($userId: ID!) {
