036e16314f4304155dd77e0fc46f863cddf0c1e4
thatAnjan <anjan.cules@gmail.com>
2021-01-14 20:28:59 +0600

    Accordion data is shown based on data availability

--- client-next/src/components/AboutSection/AccordionDetails.tsx
+++ client-next/src/components/AboutSection/AccordionDetails.tsx
@@ -3,20 +3,12 @@ import React, { useState } from 'react'
 import { Props } from './AboutSection'
 
 const Details = ({ Component, props, hook, ...others }: Props) => {
-	const userId = '5ff9939e53c3e8c7a2c4a833'
-	const { data, error } = hook({ userId })
-
-	const newProps = {
-		...props,
-		...others,
+	{
+		/* if (error) return <div>failed to load</div> */
 	}
+	// const { getPersonal } = data
 
-	if (error) return <div>failed to load</div>
-	if (!data) return <div>loading...</div>
-
-	const { getPersonal } = data
-
-	return <Component data={getPersonal} {...newProps} />
+	return <>data</>
 }
 
 export default Details
