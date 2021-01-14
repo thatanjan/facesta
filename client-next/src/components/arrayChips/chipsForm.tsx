d928ccd35484c07f4809a878b43ff895696ba134
Anjan18 <anjanbiber@gmail.com>
2021-01-14 18:18:29 +0600

    chips added which will represent each skill. but not working now

--- client-next/src/components/arrayChips/chipsForm.tsx
+++ client-next/src/components/arrayChips/chipsForm.tsx
@@ -1,17 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import { Formik, Field } from 'formik'
-import { Button, LinearProgress } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+import Chip from '@material-ui/core/Button'
+import LinearProgress from '@material-ui/core/Button'
 import { TextField } from 'formik-material-ui'
 import useGetPersonal from 'hooks/useGetPersonal'
 import createRequest from 'utils/createRequest'
 import { mutation } from 'components/AboutSection/NewDetailForm'
+import Paper from '@material-ui/core/Paper'
+import { nanoid } from 'nanoid'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
 interface Values {
 	skills: string
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			display: 'flex',
+			justifyContent: 'center',
+			flexWrap: 'wrap',
+			listStyle: 'none',
+			padding: theme.spacing(0.5),
+			margin: 0,
+		},
+		chip: {
+			margin: theme.spacing(0.5),
+		},
+	})
+)
+
 const ChipsForm = () => {
+	const classes = useStyles()
+
 	const userId = '5ff9939e53c3e8c7a2c4a833'
+
 	const {
 		data: {
 			getPersonal: { skills },
@@ -20,10 +44,15 @@ const ChipsForm = () => {
 
 	const [allSkills, setAllSkills] = useState<string[]>([])
 
+	const handleDelete = (chipToDelete: string, chipIndex: number) => () => {
+		setAllSkills(chips => chips.filter((chip, index) => index !== chipIndex))
+	}
+
 	useEffect(() => {
 		if (skills) {
 			setAllSkills([...skills])
 		}
+		console.log(skills)
 	}, [])
 
 	return (
@@ -72,9 +101,22 @@ const ChipsForm = () => {
 				)}
 			</Formik>
 
-			{allSkills.map((x, y) => (
-				<div key={x}>{x}</div>
-			))}
+			<Paper component='ul' className={classes.root}>
+				{allSkills.map((data: string, index: number) => {
+					return (
+						<li key={nanoid()}>
+							{data}
+							{/* <Chip */}
+							{/* 	label={data} */}
+							{/* 	onDelete={handleDelete(data, index)} */}
+							{/* 	className={classes.chip} */}
+							{/* 	href='' */}
+							{/* 	component='span' */}
+							{/* /> */}
+						</li>
+					)
+				})}
+			</Paper>
 		</>
 	)
 }
