cbdf57fe41256b8fe006c440cde40d1f43eb97fb
Anjan18 <anjanbiber@gmail.com>
2021-01-14 18:27:13 +0600

    AccordioDetails component is moved to AboutSection File

--- client-next/src/components/AboutSection/AccordionDetails.tsx
+++ client-next/src/components/AboutSection/AccordionDetails.tsx
@@ -1,19 +1,8 @@
 import React, { useState } from 'react'
-import AccordionDetails from '@material-ui/core/AccordionDetails'
 
-import { Props as AccordionProps } from './AboutSection'
+import { Props } from './AboutSection'
 
-interface Props extends AccordionProps {
-	accordionDetails: string
-}
-
-const Details = ({
-	Component,
-	props,
-	accordionDetails,
-	hook,
-	...others
-}: Props) => {
+const Details = ({ Component, props, hook, ...others }: Props) => {
 	const userId = '5ff9939e53c3e8c7a2c4a833'
 	const { data, error } = hook({ userId })
 
@@ -22,18 +11,12 @@ const Details = ({
 		...others,
 	}
 
-	console.log(error)
-
 	if (error) return <div>failed to load</div>
 	if (!data) return <div>loading...</div>
 
 	const { getPersonal } = data
 
-	return (
-		<AccordionDetails className={accordionDetails}>
-			<Component data={getPersonal} {...newProps} />
-		</AccordionDetails>
-	)
+	return <Component data={getPersonal} {...newProps} />
 }
 
 export default Details
