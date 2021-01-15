583fa179e20b2ceb4478469f495c48800dbfae95
Anjan18 <anjanbiber@gmail.com>
2021-01-15 08:46:26 +0600

    context provider will accept also userId as props

--- client-next/src/context/profileContext.tsx
+++ client-next/src/context/profileContext.tsx
@@ -3,18 +3,23 @@ import React, { createContext, ReactNode, useEffect, useState } from 'react'
 interface Props {
 	children: ReactNode
 	isSelf: boolean
+	userId: string
 }
 
 const initialState = {}
 
 const ProfileContext = createContext(initialState)
 
-const ProfileContextProvider = ({ children, isSelf }: Props) => {
+const ProfileContextProvider = ({ children, isSelf, userId }: Props) => {
 	const [state, setState] = useState(initialState)
 
 	useEffect(() => {
 		setState({ ...state, isSelf })
 	}, [isSelf])
+
+	useEffect(() => {
+		setState({ ...state, userId })
+	}, [userId])
 
 	return (
 		<ProfileContext.Provider value={[state, setState]}>
