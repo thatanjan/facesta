d0564c8840786e01be70dcf08bd7678c54f9cb59
Anjan18 <anjanbiber@gmail.com>
2021-01-15 21:19:35 +0600

    this component will have button and form for update details

---
+++ client-next/src/components/About/personal/NewDetailsForm.tsx
@@ -0,0 +1,18 @@
+import Button from '@material-ui/core/Button'
+import React from 'react'
+
+interface Props {}
+
+const NewDetailsForm = (props: Props) => {
+	return <div>hello woelf</div>
+}
+
+const NewDetails = () => {
+	return (
+		<>
+			<Button>update your profile</Button>
+		</>
+	)
+}
+
+export default NewDetails
