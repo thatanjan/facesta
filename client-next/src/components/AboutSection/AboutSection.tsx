1c681e7b1db98ce7c41b13dd9b7ede7f9b51dc7e
Anjan18 <anjanbiber@gmail.com>
2021-01-15 13:48:09 +0600

    personal Accordion added. code is refactored

--- client-next/src/components/AboutSection/AboutSection.tsx
+++ client-next/src/components/AboutSection/AboutSection.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
-import dynamic from 'next/dynamic'
 import { makeStyles } from '@material-ui/core/styles'
-import Accordion from '@material-ui/core/Accordion'
-import AccordionSummary from '@material-ui/core/AccordionSummary'
-import Typography from '@material-ui/core/Typography'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import AccordionDetails from '@material-ui/core/AccordionDetails'
 
-import { personal } from './AccordionOption'
+import PersonalAccordion from 'components/About/Personal'
 
-import Details from './AccordionDetails'
-// const Details = dynamic(() => import('./AccordionDetails'))
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
 	root: {
 		width: '100%',
-	},
-	heading: {
-		// fontSize: theme.typography.pxToRem(15),
-		fontWeight: theme.typography.fontWeightRegular,
-		textTransform: 'capitalize',
-	},
-	accordionDetails: {
-		flexWrap: 'wrap',
 	},
 }))
 
@@ -32,55 +15,6 @@ export interface Props {
 	Component: Function
 	formFields: string[]
 	hook: Function
-}
-
-const EachAccordion = ({ hook, name, props, Component, formFields }: Props) => {
-	const { heading, accordionDetails } = useStyles()
-
-	const detailProps = {
-		name,
-		props,
-		Component,
-		formFields,
-		accordionDetails,
-		hook,
-	}
-
-	const userId = '5ff9939e53c3e8c7a2c4a833'
-	const { data, error } = hook({ userId })
-
-	return (
-		<>
-			<Accordion TransitionProps={{ unmountOnExit: true }}>
-				<AccordionSummary
-					expandIcon={<ExpandMoreIcon />}
-					aria-controls='panel1a-content'
-					id='panel1a-header'
-				>
-					<Typography variant='h6' className={heading}>
-						{name}
-					</Typography>
-				</AccordionSummary>
-				<AccordionDetails className={accordionDetails}>
-					{data ? (
-						<Component data={data?.getPersonal} {...detailProps} />
-					) : (
-						<div>loading...</div>
-					)}
-				</AccordionDetails>
-			</Accordion>
-		</>
-	)
-}
-
-const pullData = (obj: Props) => {
-	const { name, props, Component, formFields, hook } = obj
-
-	return { name, props, Component, formFields, hook }
-}
-
-const PersonalAccordion = () => {
-	return <EachAccordion {...pullData(personal)} />
 }
 
 const SimpleAccordion = () => {
