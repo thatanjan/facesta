c4bf028095da35b759bb2cf5683e2999d16292ac
thatAnjan <anjan.cules@gmail.com>
2021-01-15 21:34:40 +0600

    some styles added to the button component

--- client-next/src/components/About/personal/NewDetailsForm.tsx
+++ client-next/src/components/About/personal/NewDetailsForm.tsx
@@ -1,5 +1,7 @@
-import Button from '@material-ui/core/Button'
 import React from 'react'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles, Theme } from '@material-ui/core'
 
 interface Props {}
 
@@ -7,10 +9,28 @@ const NewDetailsForm = (props: Props) => {
 	return <div>hello woelf</div>
 }
 
+const useStyles = makeStyles(({ spacing }: Theme) => ({
+	buttonStyle: {
+		marginTop: spacing(4),
+	},
+	dividerStyle: {
+		marginTop: spacing(2),
+		height: spacing(0.3),
+		width: '100%',
+	},
+}))
+
 const NewDetails = () => {
+	const { buttonStyle } = useStyles()
 	return (
 		<>
-			<Button>update your profile</Button>
+			<Grid container justify='flex-end'>
+				<Grid item>
+					<Button variant='contained' color='secondary' className={buttonStyle}>
+						update your profile
+					</Button>
+				</Grid>
+			</Grid>
 		</>
 	)
 }
