59fe771d61587aa54b68103ecc1aaee6567f6fdc
Anjan18 <anjanbiber@gmail.com>
2021-01-15 13:54:07 +0600

    file renamed

--- client-next/src/components/AboutSection/AboutSection.tsx
+++ client-next/src/components/AboutSection/AboutSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
-import PersonalAccordion from 'components/About/Personal'
+import PersonalAccordion from 'components/About/Personal/personal'
 
 const useStyles = makeStyles(() => ({
 	root: {
