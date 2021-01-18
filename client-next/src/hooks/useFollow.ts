23e1e0bfbe77e0e87a1822c80cc768ea17b98440
thatAnjan <anjan.cules@gmail.com>
2021-01-18 23:24:49 +0600

    two hooks created

--- client-next/src/hooks/useFollow.ts
+++ client-next/src/hooks/useFollow.ts
@@ -1,4 +1,9 @@
-import { getFollowing, getFollowers } from 'graphql/queries/followQueries'
+import {
+	getFollowing,
+	getFollowers,
+	getIsFollower,
+	getIsFollowing,
+} from 'graphql/queries/followQueries'
 import uswSWRgql from './useSWRgql'
 
 export const useFollowers = (userId: string) =>
@@ -11,6 +16,20 @@ export const useFollowers = (userId: string) =>
 export const useFollowing = (userId: string) =>
 	uswSWRgql({
 		mutation: getFollowing,
+		swrDependencies: userId,
+		options: { userId },
+	})
+
+export const useIsFollowing = (userId: string) =>
+	uswSWRgql({
+		mutation: getIsFollowing,
+		swrDependencies: userId,
+		options: { userId },
+	})
+
+export const useIsFollower = (userId: string) =>
+	uswSWRgql({
+		mutation: getIsFollower,
 		swrDependencies: userId,
 		options: { userId },
 	})
