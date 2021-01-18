708671f072cedbf5f6511afea95d058cb62a9332
thatAnjan <anjan.cules@gmail.com>
2021-01-18 22:56:44 +0600

    two resolver created and refactored

--- server/src/graphql/Follow/getFollowersAndFollowings.js
+++ server/src/graphql/Follow/getFollowersAndFollowings.js
@@ -1,15 +1,40 @@
 import Follow from 'models/Follow'
 
+const FOLLOWERS = 'followers'
+const FOLLOWING = 'following'
+
 const getUsers = (field) => async (_, { Input: { userId } }) =>
     await Follow.findOne({ user: userId }, field).populate(field)
 
-const FOLLOWERS = 'followers'
-const FOLLOWING = 'following'
+const checkIfUser = (field) => async (
+    _,
+    { Input: { userId } },
+    { user: { id: ownerId } }
+) => {
+    if (userId === ownerId) {
+        return false
+    }
+    const query = await Follow.findOne({ user: ownerId }, field)
+
+    const ifUserExist = query[field].includes(userId)
+
+    let result = {}
+
+    if (field === FOLLOWING) {
+        result.isFollowing = ifUserExist
+    } else {
+        result.isFollower = ifUserExist
+    }
+
+    return result
+}
 
 const resolver = {
     Query: {
         getFollowers: getUsers(FOLLOWERS),
         getFollowing: getUsers(FOLLOWING),
+        getIsFollower: checkIfUser(FOLLOWERS),
+        getIsFollowing: checkIfUser(FOLLOWING),
     },
 }
 
