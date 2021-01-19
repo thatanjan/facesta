1777beca5280aae1aec2202f4b966d75a634ac05
thatAnjan <anjan.cules@gmail.com>
2021-01-19 17:05:19 +0600

    tab options for about tab

---
+++ client-next/src/components/About/AboutTabOptions.ts
@@ -0,0 +1,58 @@
+import useGetPersonal from 'hooks/useGetPersonal'
+
+const addButtonText = (text: string) => ({ buttonText: `Add a new ${text}` })
+
+class Section {
+	name: string
+
+	Component: Function
+
+	// eslint-disable-next-line
+	props: any
+
+	formFields: any
+
+	hook: Function
+
+	constructor(name: string) {
+		this.name = name
+	}
+
+	addProps(props: any) {
+		this.props = props
+		this.props.buttonText = this.name
+		return this
+	}
+
+	addFormField(fields: any) {
+		this.formFields = fields
+		return this
+	}
+
+	addHook(hook: Function) {
+		this.hook = hook
+		return this
+	}
+}
+
+export const PERSONAL = 'personal'
+
+export const personal = new Section(PERSONAL)
+	.addProps(addButtonText('Change details'))
+	.addHook(useGetPersonal)
+
+export const education = new Section('Education')
+	.addProps(addButtonText('School'))
+	.addFormField(['School'])
+
+export const experience = new Section('Experience')
+	.addProps(addButtonText('Experience'))
+	.addFormField(['Company'])
+
+export const placesLived = new Section('Places Lived')
+	.addProps(addButtonText('Place'))
+	.addFormField(['city', 'town', 'country'])
+
+export const options: Section[] = [personal, education, experience, placesLived]
+
+export default options
