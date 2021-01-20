838c368bbd65d70bbdcb1c8cba4365c76ba6d200
Anjan18 <anjanbiber@gmail.com>
2021-01-20 23:46:54 +0600

    new pages added

---
+++ client-next/src/pages/videos.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import
+import React from 'react'
+
+interface Props {
+	
+}
+
+const Setting = (props: Props) => {
+	return (
+	  <>
+			<Head>
+
+				<title>Settings</title>
+				 
+			</Head>
+		<div>
+		  feature is not complete yet
+		</div>
+	  </>
+	)
+}
+
+export default Setting
+
