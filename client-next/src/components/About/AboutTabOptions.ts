91f37ebee2fae2f58e1452c26e5ffa1a491872bd
thatAnjan <anjan.cules@gmail.com>
2021-01-20 17:57:57 +0600

    education and experience component is created

--- client-next/src/components/About/AboutTabOptions.ts
+++ client-next/src/components/About/AboutTabOptions.ts
@@ -2,6 +2,8 @@ import dynamic from 'next/dynamic'
 import useGetPersonal from 'hooks/useGetPersonal'
 
 const Personal = dynamic(() => import('./personal/Personal'))
+const Education = dynamic(() => import('./OtherTabs/Education'))
+const Experience = dynamic(() => import('./OtherTabs/Experience'))
 
 const addButtonText = (text: string) => ({ buttonText: `Add a new ${text}` })
 
@@ -56,10 +58,12 @@ export const personal = new Section(PERSONAL)
 	.addHook(useGetPersonal)
 
 export const education = new Section('Education')
+	.addComponent(Education)
 	.addProps(addButtonText('School'))
 	.addFormField(['School'])
 
 export const experience = new Section('Experience')
+	.addComponent(Experience)
 	.addProps(addButtonText('Experience'))
 	.addFormField(['Company'])
 
---
+++ client-next/src/components/About/OtherTabs/Education.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+const Education = (props: Props) => {
+	return <div>education</div>
+}
+
+export default Education
