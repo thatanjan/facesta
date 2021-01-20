adfdff06b7848f81d51bb382a79642483b095850
thatAnjan <anjan.cules@gmail.com>
2021-01-20 09:34:42 +0600

    chips are wrapped in paper

--- client-next/src/components/arrayChips/arrayChips.tsx
+++ client-next/src/components/arrayChips/arrayChips.tsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import { nanoid } from 'nanoid'
 import Chip from '@material-ui/core/Chip'
-import { makeStyles, Theme } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
 interface Props {
 	skills: string[]
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-	chip: {
-		margin: theme.spacing(0.5),
-	},
-}))
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			display: 'flex',
+			justifyContent: 'center',
+			flexWrap: 'wrap',
+			listStyle: 'none',
+			padding: theme.spacing(0.5),
+			margin: 0,
+		},
+		chip: {
+			margin: theme.spacing(0.5),
+		},
+	})
+)
 
 const ArrayChips = ({ skills }: Props) => {
-	const { chip } = useStyles()
+	const { chip, root } = useStyles()
 
 	return (
 		<>
-			{skills.map((item: string) => (
-				<li key={nanoid()}>
-					<Chip label={item} className={chip} />
-				</li>
-			))}
+			<Paper component='ul' className={root}>
+				{skills.map((item: string) => (
+					<li key={nanoid()}>
+						<Chip label={item} className={chip} />
+					</li>
+				))}
+			</Paper>
 		</>
 	)
 }
