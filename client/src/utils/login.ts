0dfa443b59de4e96c4295219e361d87c2322c7e5
thatAnjan <anjan.cules@gmail.com>
2021-01-26 01:33:13 +0600

    user can now login

--- client/src/utils/login.ts
+++ client/src/utils/login.ts
@@ -1,18 +1,7 @@
-import jwtDecode from 'jwt-decode'
 import { setToken } from 'utils/cookieToken'
 
-interface params {
-	setUser: Function
-	token: string
-}
-
-const login = async ({ setUser, token }: params) => {
+const login = async (token: string) => {
 	setToken(token)
-
-	const decodedToken: { [key: string]: string | number } = jwtDecode(token)
-
-	setUser(decodedToken)
-
 	return true
 }
 
