40f10a36997deb0412677e32dc13b44bfc520bff
Anjan18 <anjanbiber@gmail.com>
2021-01-26 00:35:15 +0600

    two more var is added

--- client/src/variables/global.ts
+++ client/src/variables/global.ts
@@ -1,6 +1,9 @@
 const TOKEN_NAME: string = 'jwt'
 
+const LOGIN = 'login'
+const SIGN_UP = 'sign-up'
+
 const DATE_OF_BIRTH = 'dateOfBirth'
 const SKILLS = 'skills'
 
-export { TOKEN_NAME, DATE_OF_BIRTH, SKILLS }
+export { TOKEN_NAME, DATE_OF_BIRTH, SKILLS, LOGIN, SIGN_UP }
