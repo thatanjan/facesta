8b5d9a676f1700accbc05dd24c2b6d30992ce30c
thatAnjan <anjan.cules@gmail.com>
2021-01-25 20:39:26 +0600

    mui is set up

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import React from 'react'
+import Head from 'next/head'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp(props) {
+	const { Component, pageProps } = props
+
+	React.useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector('#jss-server-side')
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles)
+		}
+	}, [])
+
+	return (
+		<>
+			<Head>
+				<title>My page</title>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width'
+				/>
+			</Head>
+			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+			<CssBaseline />
+			<Component {...pageProps} />
+		</>
+	)
 }
-
-export default MyApp
