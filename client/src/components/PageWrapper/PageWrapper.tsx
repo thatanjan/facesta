a103c18791557a7a20983a2d028d142aed0cc27d
thatAnjan <anjan.cules@gmail.com>
2021-01-25 21:13:28 +0600

    context provider wraps the PageWrapper

--- client/src/components/PageWrapper/PageWrapper.tsx
+++ client/src/components/PageWrapper/PageWrapper.tsx
@@ -1,11 +1,12 @@
 import React, { ReactNode } from 'react'
+import UserContextProvider from 'context/UserContext'
 
 interface Props {
 	children: ReactNode
 }
 
 const PageWrapper = ({ children }: Props) => {
-	return <>{children}</>
+	return <UserContextProvider userData={{}}>{children}</UserContextProvider>
 }
 
 export default PageWrapper
