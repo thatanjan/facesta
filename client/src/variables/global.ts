14f8b42892ed997cb0bd1cc108c565600d951fc3
Anjan18 <anjanbiber@gmail.com>
2021-01-26 16:07:00 +0600

    variable for checking if production

--- client/src/variables/global.ts
+++ client/src/variables/global.ts
@@ -8,9 +8,10 @@ const SKILLS = 'skills'
 
 const PRODUCTION = 'production'
 
-const END_POINT =
-	process.env.VERCEL_ENV === PRODUCTION
-		? process.env.SERVER_URL
-		: 'http://localhost:8000/graphql'
+export const ifProduction = process.env.VERCEL_ENV === PRODUCTION
+
+const END_POINT = ifProduction
+	? process.env.SERVER_URL
+	: 'http://localhost:8000/graphql'
 
 export { TOKEN_NAME, DATE_OF_BIRTH, SKILLS, LOGIN, SIGN_UP, END_POINT }
