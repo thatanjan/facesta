c5350a3a51c5d1f107a683b66b86535744b796da
Anjan18 <anjanbiber@gmail.com>
2021-01-26 16:09:46 +0600

    validation rule will change if env is changed

--- client/src/utils/checkValidJwt.ts
+++ client/src/utils/checkValidJwt.ts
@@ -1,15 +1,20 @@
 import axios from 'axios'
+import { ifProduction } from 'variables/global'
 
 // eslint-disable-next-line
 const checkValidJwt = async (jwt: string) => {
-	const END_POINT = 'http://localhost:8000/validate'
+	const END_POINT = ifProduction
+		? process.env.SERVER_VALIDATE
+		: 'http://localhost:8000/validate'
+
 	try {
-		await axios.post(END_POINT, { data: { jwt } })
+		await axios.post(END_POINT as string, { data: { jwt } })
 		return true
 	} catch (error) {
 		const statusCode = error.response.status
 
 		if (statusCode === 401) {
+			// eslint-disable-next-line
 			console.log(error.response.data.message)
 			return false
 		}
