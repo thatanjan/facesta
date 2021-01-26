39e43e3ebe1f60211002f6d852b9744e023180ee
thatAnjan <anjan.cules@gmail.com>
2021-01-26 16:01:29 +0600

    url will be based on if env is produciton or not

--- client/src/variables/global.ts
+++ client/src/variables/global.ts
@@ -6,6 +6,11 @@ const SIGN_UP = 'sign-up'
 const DATE_OF_BIRTH = 'dateOfBirth'
 const SKILLS = 'skills'
 
-const END_POINT = 'http://localhost:8000/graphql'
+const PRODUCTION = 'production'
+
+const END_POINT =
+	process.env.VERCEL_ENV === PRODUCTION
+		? process.env.SERVER_URL
+		: 'http://localhost:8000/graphql'
 
 export { TOKEN_NAME, DATE_OF_BIRTH, SKILLS, LOGIN, SIGN_UP, END_POINT }
