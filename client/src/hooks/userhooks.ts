1deb78b8dc5ad4598b9aeaf86db28aa560aa1c69
thatAnjan <anjan.cules@gmail.com>
2021-01-27 08:24:17 +0600

    path name was incorrect, fixed

--- client/src/hooks/userhooks.ts
+++ client/src/hooks/userhooks.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { UserContext } from 'context/userContext'
+import { UserContext } from 'context/UserContext'
 
 const useGetUser = () => {
 	const [user]: any = useContext(UserContext)
