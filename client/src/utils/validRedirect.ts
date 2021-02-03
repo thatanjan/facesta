0a7829b2b6a68edef092d7fa8f6d2901dab70fb2
thatanjan <anjan.cules@gmail.com>
2021-02-03 07:49:25 +0600

    token existence and token validation checking is now seperated

--- client/src/utils/validRedirect.ts
+++ client/src/utils/validRedirect.ts
@@ -7,9 +7,19 @@ const validRedirect = async (req: any, res: any) => {
 
 	const token = cookies?.[TOKEN_NAME]
 
-	if (!token || !(await checkValidJwt(token))) {
+	if (!token) {
 		redirectToAuth(res)
+
+		return false
 	}
+
+	const isValid = await checkValidJwt(token)
+
+	if (!isValid) return false
+
+	// if (!token || !(await checkValidJwt(token))) {
+	// 	redirectToAuth(res)
+	// }
 }
 
 export default validRedirect
