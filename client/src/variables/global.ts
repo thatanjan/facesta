755281982f05fcbb0134a4e1f70510d96cbfebca
thatanjan <anjan.cules@gmail.com>
2021-02-04 13:24:43 +0600

    auth urls are created through functions rather than hard coded

--- client/src/variables/global.ts
+++ client/src/variables/global.ts
@@ -1,7 +1,15 @@
+const AUTHENTICATION = 'authentication'
+const createAuthUrl = (restUrl: string): string => `${AUTHENTICATION}${restUrl}`
+
 const TOKEN_NAME: string = 'jwt'
 
 const LOGIN = 'login'
+const LOGOUT = 'logout'
 const SIGN_UP = 'sign-up'
+
+const LOGOUT_URL = createAuthUrl(`/${LOGOUT}`)
+const LOGIN_URL = createAuthUrl(`/${LOGIN}`)
+const SIGN_UP_URL = createAuthUrl(`${SIGN_UP}`)
 
 const DATE_OF_BIRTH = 'dateOfBirth'
 const SKILLS = 'skills'
@@ -14,4 +22,16 @@ const END_POINT = ifProduction
 	? process.env.NEXT_PUBLIC_SERVER_URL
 	: 'http://localhost:8000/graphql'
 
-export { TOKEN_NAME, DATE_OF_BIRTH, SKILLS, LOGIN, SIGN_UP, END_POINT }
+export {
+	AUTHENTICATION,
+	TOKEN_NAME,
+	DATE_OF_BIRTH,
+	SKILLS,
+	LOGIN,
+	SIGN_UP,
+	END_POINT,
+	LOGOUT,
+	LOGOUT_URL,
+	SIGN_UP_URL,
+	LOGIN_URL,
+}
