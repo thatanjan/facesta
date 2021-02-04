3cb43066775f2dd795a491baef3d61c6d18b4bff
thatanjan <anjan.cules@gmail.com>
2021-02-04 14:40:26 +0600

    logout and redirecting is working but not without getServerSideProps

--- client/src/pages/authentication/logout.tsx
+++ client/src/pages/authentication/logout.tsx
@@ -1,3 +1,26 @@
-const logout = () => null
+import React from 'react'
+import Cookies from 'js-cookie'
+import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
 
-export default logout
+import logout from 'utils/logout'
+import { redirectToAuth } from 'utils/authRedirect'
+
+const Logout = () => {
+	const router = useRouter()
+
+	console.log(Cookies.remove('jwt'))
+
+	setTimeout(() => {
+		router.push('/authentication/login')
+	}, 3000)
+
+	return <div>You are logging out.</div>
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const { req, res } = ctx
+
+	return { props: {} }
+}
+export default Logout
