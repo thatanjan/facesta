07cdb54d63f67ba208a1289d1ad7e33516085555
thatanjan <anjan.cules@gmail.com>
2021-02-07 12:09:21 +0600

    function will take either token(string) or request object. if object then will get the token from object. At last return the decoded object

--- client/src/utils/decodeToken.ts
+++ client/src/utils/decodeToken.ts
@@ -2,8 +2,15 @@ import Requset from 'interfaces/requsetResponse'
 import jwtDecode from 'jwt-decode'
 import getToken from 'utils/getToken'
 
-export default (req: Requset) => {
-	const token = getToken(req)
+export default (param: Requset | string) => {
+	let token: string
+
+	if (typeof param === 'string') {
+		token = param
+		return jwtDecode(token)
+	}
+
+	token = getToken(param as Requset)
 
 	return jwtDecode(token)
 }
