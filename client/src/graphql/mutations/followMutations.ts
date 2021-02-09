0bd7bdb83b40b7f096a6188768c2db51fd19a158
thatanjan <anjan.cules@gmail.com>
2021-02-09 12:10:14 +0600

    a mutation for following and unfollowng a user

---
+++ client/src/graphql/mutations/followMutations.ts
@@ -0,0 +1,20 @@
+import { gql } from 'graphql-request'
+
+// eslint-disable-next-line
+export const unfollow: string = gql`
+	mutation unfollowUser($userId: ID!) {
+		unfollowUser(Input: { userId: $userId }) {
+			success
+			errorMessage
+		}
+	}
+`
+
+export const follow: string = gql`
+	mutation followUser($userId: ID!) {
+		followUser(Input: { userId: $userId }) {
+			success
+			errorMessage
+		}
+	}
+`
