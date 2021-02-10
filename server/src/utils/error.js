dbcd331af63ea9b20dff89b7b8f2c00c2e0050f9
thatanjan <anjan.cules@gmail.com>
2021-02-10 12:33:24 +0600

    checking if the errorMessage is null or object or string

--- server/src/utils/error.js
+++ server/src/utils/error.js
@@ -1,6 +1,18 @@
+const theErrorMessage = errorMessage => {
+	console.log(errorMessage)
+	if (
+		errorMessage instanceof Error ||
+		(errorMessage === 'object' && errorMessage.message)
+	)
+		return errorMessage.message
+
+	if (errorMessage === null) return null
+
+	return errorMessage
+}
+
 export default (success, errorMessage, message) => ({
-    success,
-    message,
-    errorMessage:
-        typeof errorMessage === 'object' ? errorMessage.message : errorMessage,
+	success,
+	message,
+	errorMessage: theErrorMessage(errorMessage),
 })
