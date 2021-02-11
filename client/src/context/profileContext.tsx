828b41025d1d8ff902f258f846a18dea9728f41c
thatanjan <anjan.cules@gmail.com>
2021-02-11 23:54:13 +0600

    a context to provide profile data

---
+++ client/src/context/profileContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, ReactNode, useEffect, useState } from 'react'
+
+interface Props {
+	children: ReactNode
+	isSelf: boolean
+	profileUserId: string
+}
+
+interface State {
+	profileUserId: string
+	isSelf: boolean
+}
+
+interface ContextData {
+	state: State
+	setState: Function
+}
+
+const initialState: State = { profileUserId: '', isSelf: false }
+
+export const ProfileContext = createContext({} as ContextData)
+
+const ProfileContextProvider = ({ children, isSelf, profileUserId }: Props) => {
+	const [state, setState] = useState<State>(initialState)
+
+	useEffect(() => {
+		setState({ ...state, isSelf, profileUserId })
+	}, [isSelf, profileUserId])
+
+	return (
+		<ProfileContext.Provider value={{ state, setState }}>
+			{children}
+		</ProfileContext.Provider>
+	)
+}
+
+export default ProfileContextProvider
