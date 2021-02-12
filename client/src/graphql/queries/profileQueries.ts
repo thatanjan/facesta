efb3c9024217119123c3973e8f754c93348c7fe1
thatanjan <anjan.cules@gmail.com>
2021-02-12 17:35:01 +0600

    profileUserId instead of userId

--- client/src/graphql/queries/profileQueries.ts
+++ client/src/graphql/queries/profileQueries.ts
@@ -3,22 +3,22 @@ import { gql } from 'graphql-request'
 const expectedOutput = 'dateOfBirth skills name bio website status location'
 
 export const getPersonalData = (output?: string | undefined): string => gql`
-	 query getPersonalData($userId: ID!) {
-		 getPersonal(Input: { userId: $userId }) {
+	 query getPersonalData($profileUserId: ID!) {
+		 getPersonal(Input: { profileUserId: $profileUserId }) {
 			    ${output || expectedOutput}
 		 }
 } `
 
 export const getExperience = (output: string): string => gql`
-	query getExperience($userId: ID!) {
-		getExperience(Input: { userId: $userId }){
+	query getExperience($profileUserId: ID!) {
+		getExperience(Input: { profileUserId: $profileUserId }){
 			    ${output}
 		}
 } `
 
 export const getEducation: string = gql`
-	query getEducation($userId: ID!) {
-		getEducation(Input: { userId: $userId }) {
+	query getEducation($profileUserId: ID!) {
+		getEducation(Input: { profileUserId: $profileUserId }) {
 			id
 			school
 			degree
