34309a84648b81dfbb95e00b54b1ae9c561fe9af
thatanjan <anjan.cules@gmail.com>
2021-02-12 21:21:45 +0600

    personal detail is added to About tab component

--- client/src/components/Profile/Tabs/About/About.tsx
+++ client/src/components/Profile/Tabs/About/About.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
+
+const Personal = dynamic(import('./personal/Personal'))
 
 interface Props {}
 
 const About = (props: Props) => {
-	return <div></div>
+	return <Personal />
 }
 
 export default About
