0b1508e81601a7d2aa9143002e89a9cbc0b36776
thatanjan <anjan.cules@gmail.com>
2021-02-12 13:04:07 +0600

    fetcher will receiver Parameters type parameter and Parameters is exported

--- client/src/utils/swrFetcher.ts
+++ client/src/utils/swrFetcher.ts
@@ -1,6 +1,13 @@
 import createRequest from 'utils/createRequest'
+import { keyInterface } from 'swr'
+import { AnyObject } from 'interfaces/global'
 
-const fetcher = ({ operation, options: { jwt, ...data } }: any) => async () =>
-	createRequest({ operation, values: data }, jwt)
+export interface Parameters {
+	operation: keyInterface
+	values: AnyObject | undefined
+}
+
+const fetcher = ({ operation, values }: Parameters) => async () =>
+	createRequest({ operation, values })
 
 export default fetcher
