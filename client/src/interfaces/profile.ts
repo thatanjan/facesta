090dbccdc6124a75cb7b94cd25ea8a37128c2a5a
thatanjan <anjan.cules@gmail.com>
2021-02-12 21:12:49 +0600

    a interface for personal data

---
+++ client/src/interfaces/profile.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+export interface PersonalData {
+	dateOfBirth: Date | string
+	website: string
+	status: string
+	location: string
+	bio: string
+	name: string
+	skills: string[]
+}
