c8358adbeea5a9096f5e44091818228a2df88f50
thatanjan <anjan.cules@gmail.com>
2021-02-13 21:30:46 +0600

    component to display bunch of post

---
+++ client/src/components/Post/AllPosts.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import SinglePost from 'components/Post/SinglePost'
+
+const PostsSection = () => {
+	return (
+		<div>
+			<SinglePost />
+			<SinglePost />
+			<SinglePost />
+		</div>
+	)
+}
+
+export default PostsSection
