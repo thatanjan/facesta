a632102a96e194e32f945db04bfa6b78b62bc4f1
thatanjan <anjan.cules@gmail.com>
2021-02-13 21:11:47 +0600

    a simple dropdown menu which will be dynamic. menu item will be based on props

---
+++ client/src/components/DropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Fade from '@material-ui/core/Fade'
+import IconButton from '@material-ui/core/IconButton'
+import { nanoid } from 'nanoid'
+
+import { AnyObject } from 'interfaces/global'
+
+const DropDownMenu = ({ options, IconComponent, ...props }: AnyObject) => {
+	const [anchorEl, setAnchorEl] = useState(null)
+	const open: boolean = Boolean(anchorEl)
+
+	const handleClick = (event: AnyObject) => {
+		setAnchorEl(event.currentTarget)
+	}
+
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
+	return (
+		<>
+			<IconButton onClick={handleClick} {...props}>
+				<IconComponent />
+			</IconButton>
+
+			<Menu
+				id='fade-menu'
+				anchorEl={anchorEl}
+				keepMounted
+				open={open}
+				onClose={handleClose}
+				TransitionComponent={Fade}
+			>
+				{options.map((item: string) => (
+					<MenuItem
+						key={nanoid()}
+						component='li'
+						onClick={handleClose}
+						style={{ textTransform: 'capitalize' }}
+					>
+						{item}
+					</MenuItem>
+				))}
+			</Menu>
+		</>
+	)
+}
+
+export default DropDownMenu
