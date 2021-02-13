9b6b0f5163cd4e99752d1ba8d475ea2d6e287efb
thatanjan <anjan.cules@gmail.com>
2021-02-13 14:30:45 +0600

    profile user id will be fetched inside the hook. hook will not take any arguement

--- client/src/hooks/useGetPersonalProfile.ts
+++ client/src/hooks/useGetPersonalProfile.ts
@@ -1,8 +1,10 @@
 import { getPersonalData } from 'graphql/queries/profileQueries'
 import useSWRgql from 'hooks/useSWRgql'
+import { useProfileUserId } from 'hooks/profileContextHooks'
 
-const useGetPersonal = (profileUserId: string) => {
+const useGetPersonal = () => {
 	const mutation = getPersonalData()
+	const profileUserId = useProfileUserId()
 	const values = { profileUserId }
 
 	return useSWRgql({
