d687d526567d3a664498f7fdebc3975370f6a831
thatanjan <anjan.cules@gmail.com>
2021-02-14 09:42:19 +0600

    mutation for creating a post

---
+++ client/src/graphql/mutations/postMutations.ts
@@ -0,0 +1,10 @@
+import { gql } from 'graphql-request'
+
+// eslint-disable-next-line
+export const createPost = gql`
+mutation createPost($text:String!) {
+			createPost({Input:{text:$text}}) {
+				text, id
+		}
+	}
+`
