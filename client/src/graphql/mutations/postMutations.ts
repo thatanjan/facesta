a7176c5e7f4658fb47fd8537fecf89d1f6a967b1
thatanjan <anjan.cules@gmail.com>
2021-02-14 12:19:51 +0600

    error fixed

--- client/src/graphql/mutations/postMutations.ts
+++ client/src/graphql/mutations/postMutations.ts
@@ -2,9 +2,9 @@ import { gql } from 'graphql-request'
 
 // eslint-disable-next-line
 export const createPost = gql`
-mutation createPost($text:String!) {
-			createPost({Input:{text:$text}}) {
-				text
+	mutation createPost($text: String!) {
+		createPost(Input: { text: $text }) {
+			text
 		}
 	}
 `
