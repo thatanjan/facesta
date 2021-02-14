da9ce409123eefa85604216ca405272c982058d2
thatanjan <anjan.cules@gmail.com>
2021-02-14 13:25:02 +0600

    a header menu for app bar. where user will have message, settings , profile etc

---
+++ client/src/components/AppBars/AppHeaderMenus.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import AddIcon from '@material-ui/icons/Add'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import TelegramIcon from '@material-ui/icons/Telegram'
+import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import { useRouter } from 'next/router'
+
+import DropDownMenu from 'components/DropDownMenu/DropDownMenu'
+import MuiLink from 'components/Links/MuiLink'
+import useOwnUser from 'hooks/userhooks'
+import splitText from 'utils/splitText'
+
+const useStyles = makeStyles((theme: Theme) => ({
+	AccountIconTextStyle: {
+		marginLeft: theme.spacing(2),
+	},
+}))
+
+const AppHeaderMenus = () => {
+	const options = ['settings & privacy', 'help and support', 'logout']
+
+	const { AccountIconTextStyle } = useStyles()
+
+	const { push } = useRouter()
+
+	const { name, id } = useOwnUser()
+
+	const firstName = splitText({ text: name, position: 0, divider: ' ' })
+
+	return (
+		<>
+			<IconButton style={{ borderRadius: '10px' }}>
+				<AccountCircleIcon />
+				<MuiLink
+					MuiComponent={Typography}
+					href={`/profile/${id}`}
+					className={AccountIconTextStyle}
+				>
+					{firstName}
+				</MuiLink>
+			</IconButton>
+
+			<IconButton>
+				<AddIcon />{' '}
+			</IconButton>
+
+			<IconButton edge='end' onClick={() => push('/message')}>
+				<TelegramIcon color='secondary' />
+			</IconButton>
+
+			<IconButton>
+				<NotificationsIcon />
+			</IconButton>
+
+			<DropDownMenu
+				options={options}
+				aria-controls='fade-menu'
+				aria-haspopup='true'
+				IconComponent={ArrowDropDownCircleIcon}
+			/>
+		</>
+	)
+}
+
+export default AppHeaderMenus
