853943220815770adf1cc37ce618cc61e5e74107
thatanjan <anjan.cules@gmail.com>
2021-02-15 08:13:23 +0600

    return keyword added to useFollowrs hook

--- client/src/hooks/useFollow.ts
+++ client/src/hooks/useFollow.ts
@@ -1,44 +1,44 @@
 import {
-  getFollowing,
-  getFollowers,
-  getIsFollower,
-  getIsFollowing,
-} from "graphql/queries/followQueries";
-import uswSWRgql from "./useSWRgql";
-import { useProfileUserId } from "./profileContextHooks";
+	getFollowing,
+	getFollowers,
+	getIsFollower,
+	getIsFollowing,
+} from 'graphql/queries/followQueries'
+import uswSWRgql from './useSWRgql'
+import { useProfileUserId } from './profileContextHooks'
 
 export const useFollowers = () => {
-  const otherUserId = useProfileUserId();
-  uswSWRgql({
-    key: getFollowers,
-    swrDependencies: otherUserId,
-    values: { otherUserId },
-  });
-};
+	const otherUserId = useProfileUserId()
+	return uswSWRgql({
+		key: getFollowers,
+		swrDependencies: otherUserId,
+		values: { otherUserId },
+	})
+}
 
 export const useFollowing = () => {
-  const otherUserId = useProfileUserId();
-  return uswSWRgql({
-    key: getFollowing,
-    swrDependencies: otherUserId,
-    values: { otherUserId },
-  });
-};
+	const otherUserId = useProfileUserId()
+	return uswSWRgql({
+		key: getFollowing,
+		swrDependencies: otherUserId,
+		values: { otherUserId },
+	})
+}
 
 export const useIsFollowing = () => {
-  const otherUserId = useProfileUserId();
-  return uswSWRgql({
-    key: getIsFollowing,
-    swrDependencies: otherUserId,
-    values: { otherUserId },
-  });
-};
+	const otherUserId = useProfileUserId()
+	return uswSWRgql({
+		key: getIsFollowing,
+		swrDependencies: otherUserId,
+		values: { otherUserId },
+	})
+}
 
 export const useIsFollower = () => {
-  const otherUserId = useProfileUserId();
-  return uswSWRgql({
-    key: getIsFollower,
-    swrDependencies: otherUserId,
-    values: { otherUserId },
-  });
-};
+	const otherUserId = useProfileUserId()
+	return uswSWRgql({
+		key: getIsFollower,
+		swrDependencies: otherUserId,
+		values: { otherUserId },
+	})
+}
