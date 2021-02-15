e759f35bc3070fe5bdb91c8dea06397f77aa533c
thatanjan <anjan.cules@gmail.com>
2021-02-15 16:41:06 +0600

    chat and dropdown item is removed. search and add item is added

--- client/src/components/Navigation/BottomNavigation/BottomNavigationData.ts
+++ client/src/components/Navigation/BottomNavigation/BottomNavigationData.ts
@@ -1,7 +1,7 @@
 import NotificationsIcon from '@material-ui/icons/Notifications'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
-import TelegramIcon from '@material-ui/icons/Telegram'
-import ArrowDropDownCircleIcon from '@material-ui/icons/ArrowDropDownCircle'
+import AddIcon from '@material-ui/icons/Add'
+import SearchIcon from '@material-ui/icons/Search'
 
 export class NavigationItem {
 	label: string
@@ -18,15 +18,10 @@ export class NavigationItem {
 }
 
 const notification = new NavigationItem('notification', NotificationsIcon)
+const add = new NavigationItem('add', AddIcon)
 const profile = new NavigationItem('profile', AccountCircleIcon)
-const chat = new NavigationItem('chat', TelegramIcon)
-const dropdown = new NavigationItem('dropdown', ArrowDropDownCircleIcon)
+const search = new NavigationItem('search', SearchIcon)
 
-const navigationItems: NavigationItem[] = [
-	notification,
-	profile,
-	chat,
-	dropdown,
-]
+const navigationItems: NavigationItem[] = [profile, search, add, notification]
 
 export default navigationItems
