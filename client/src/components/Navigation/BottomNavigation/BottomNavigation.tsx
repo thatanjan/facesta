17e449131016d4568f8b0cbafb6d3436f5b573d5
thatanjan <anjan.cules@gmail.com>
2021-02-15 13:38:59 +0600

    bottom navigation created for mobile. items will be based on BottomNavigationData.ts  file

---
+++ client/src/components/Navigation/BottomNavigation/BottomNavigation.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import { nanoid } from 'nanoid'
+
+import navigationItems, { NavigationItem } from './BottomNavigationData'
+
+const useStyles = makeStyles({
+	root: {
+		width: 500,
+	},
+})
+
+export default function LabelBottomNavigation() {
+	const { root } = useStyles()
+	const [value, setValue] = React.useState('recents')
+
+	const handleChange = (_: React.ChangeEvent<{}>, newValue: string) => {
+		setValue(newValue)
+	}
+
+	return (
+		<BottomNavigation value={value} onChange={handleChange} className={root}>
+			{navigationItems.map(({ icon: Icon, ...props }: NavigationItem) => (
+				<BottomNavigationAction key={nanoid()} {...props} icon={<Icon />} />
+			))}
+		</BottomNavigation>
+	)
+}
