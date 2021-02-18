383d479723316d8564d01c891247d6aa66bd8141
thatanjan <anjan.cules@gmail.com>
2021-02-18 21:56:25 +0600

    a variable file for common text

---
+++ server/src/variables/commonText.js
@@ -0,0 +1,7 @@
+const POST_ID = 'postID'
+const POST_OWNER_ID = 'postOwnerID'
+const OWNER_ID = 'owenerID'
+const OTHER_USER_ID = 'otherUserID'
+const ANY_USER_ID = 'anyUserID'
+
+export { POST_ID, POST_OWNER_ID, OWNER_ID, OTHER_USER_ID, ANY_USER_ID }
