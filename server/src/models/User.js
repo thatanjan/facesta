c5efb7d364f6cfa5164cebda36610b2b6d83f2b3
thatanjan <anjan.cules@gmail.com>
2021-02-18 16:45:21 +0600

    usermodel will now have new field called newsfeed

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,34 +5,35 @@ const Schema = mongoose.Schema
 const objectId = mongoose.Schema.Types.ObjectId
 
 const schema = {
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    avatar: {
-        type: String,
-    },
-    profile: {
-        type: objectId,
-        required: true,
-        ref: 'profile',
-    },
-    posts: {
-        type: objectId,
-        ref: 'posts',
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	avatar: {
+		type: String,
+	},
+	profile: {
+		type: objectId,
+		required: true,
+		ref: 'profile',
+	},
+	posts: {
+		type: objectId,
+		ref: 'posts',
+	},
+	newsfeed: { type: objectId, ref: 'newsfeed', required: true },
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 }
 
 const UserSchema = new Schema(schema, { versionKey: '1' })
