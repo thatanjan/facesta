75fbbd63b78392cc1e36897e130e6b6f713eb9e8
thatanjan <anjan.cules@gmail.com>
2021-02-18 22:33:40 +0600

    types are refactored with new types with common text

--- server/src/graphql/Post/PostType.js
+++ server/src/graphql/Post/PostType.js
@@ -1,4 +1,5 @@
 import { gql } from 'apollo-server-express'
+import { POST_ID_TYPE, POST_OWNER_ID_TYPE } from 'variables/commonText'
 
 const text = `text: String!`
 const postId = `postId: ID!`
@@ -11,9 +12,9 @@ const userAndPostId = `
 
 const PostTypedefs = gql`
     extend type Query {
-        getSinglePost(Input: userAndPostId!): returnSinglePost!
-        getAllPost(Input: getAllPostInput!): returnAllPost!
-        getNewsFeedPost(Input: ownUserId!): returnAllPost!
+        getSinglePost(Input: PostAndPostOwnerIDInput!): returnSinglePost!
+        getAllPost(Input: PostOwnerIDInput!): returnAllPost!
+        getNewsFeedPost: returnAllPost!
     }
 
     extend type Mutation {
@@ -67,6 +68,20 @@ const PostTypedefs = gql`
     input ownUserId {
         ownUserId: ID!
     }
+
+
+  input PostIDInput{
+    ${POST_ID_TYPE}!
+  }
+
+  input PostOwnerIDInput {
+    ${POST_OWNER_ID_TYPE}!
+  }
+
+  input PostAndPostOwnerIDInput{
+    ${POST_ID_TYPE}!
+    ${POST_OWNER_ID_TYPE}!
+  }
 
 `
 export default PostTypedefs
