490e3b11f41cb74f5ebd2d47811bd48333ded281
thatanjan <anjan.cules@gmail.com>
2021-02-18 13:42:05 +0600

    two variables imported from global file and removed from local file

--- server/src/graphql/Follow/getFollowersAndFollowings.js
+++ server/src/graphql/Follow/getFollowersAndFollowings.js
@@ -1,41 +1,39 @@
 import Follow from 'models/Follow'
+import { FOLLOWING, FOLLOWERS } from 'variables/global'
 
-const FOLLOWERS = 'followers'
-const FOLLOWING = 'following'
+const getUsers = field => async (_, { Input: { otherUserId } }) =>
+	await Follow.findOne({ user: otherUserId }, field).populate(field)
 
-const getUsers = (field) => async (_, { Input: { otherUserId } }) =>
-    await Follow.findOne({ user: otherUserId }, field).populate(field)
-
-const checkIfUser = (field) => async (
-    _,
-    { Input: { otherUserId } },
-    { user: { id: ownerId } }
+const checkIfUser = field => async (
+	_,
+	{ Input: { otherUserId } },
+	{ user: { id: ownerId } }
 ) => {
-    if (otherUserId === ownerId) {
-        return false
-    }
-    const query = await Follow.findOne({ user: ownerId }, field)
+	if (otherUserId === ownerId) {
+		return false
+	}
+	const query = await Follow.findOne({ user: ownerId }, field)
 
-    const ifUserExist = query[field].includes(otherUserId)
+	const ifUserExist = query[field].includes(otherUserId)
 
-    let result = {}
+	let result = {}
 
-    if (field === FOLLOWING) {
-        result.isFollowing = ifUserExist
-    } else {
-        result.isFollower = ifUserExist
-    }
+	if (field === FOLLOWING) {
+		result.isFollowing = ifUserExist
+	} else {
+		result.isFollower = ifUserExist
+	}
 
-    return result
+	return result
 }
 
 const resolver = {
-    Query: {
-        getFollowers: getUsers(FOLLOWERS),
-        getFollowing: getUsers(FOLLOWING),
-        getIsFollower: checkIfUser(FOLLOWERS),
-        getIsFollowing: checkIfUser(FOLLOWING),
-    },
+	Query: {
+		getFollowers: getUsers(FOLLOWERS),
+		getFollowing: getUsers(FOLLOWING),
+		getIsFollower: checkIfUser(FOLLOWERS),
+		getIsFollowing: checkIfUser(FOLLOWING),
+	},
 }
 
 export default resolver
