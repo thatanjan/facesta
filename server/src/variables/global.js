1f3f3e0ede3b677df8bc6f92dc6995b9e4c9618b
thatanjan <anjan.cules@gmail.com>
2021-02-19 20:50:57 +0600

    new variable added

--- server/src/variables/global.js
+++ server/src/variables/global.js
@@ -2,5 +2,6 @@ const FOLLOWERS = 'followers'
 const FOLLOWING = 'following'
 const ERROR_MESSAGE = 'errorMessage'
 const MESSAGE = 'message'
+const ERROR = 'error'
 
-export { FOLLOWERS, FOLLOWING, MESSAGE, ERROR_MESSAGE }
+export { FOLLOWERS, FOLLOWING, MESSAGE, ERROR_MESSAGE, ERROR }
