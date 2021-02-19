9478697a8a2cb914a5c72a495d7bd16c7943b5b8
thatanjan <anjan.cules@gmail.com>
2021-02-19 19:48:38 +0600

    follow types are updated

--- server/src/graphql/Follow/FollowType.js
+++ server/src/graphql/Follow/FollowType.js
@@ -1,46 +1,49 @@
 import { gql } from 'apollo-server-express'
+import { ANY_USER_ID_TYPE, OTHER_USER_ID_TYPE } from 'variables/commonText'
 
 const FollowType = gql`
     extend type Query {
-        getFollowers(Input: otherUserId): Followers!
-        getFollowing(Input: otherUserId): Following!
-        getIsFollowing(Input: otherUserId): IsFollowing!
-        getIsFollower(Input: otherUserId): IsFollower!
+        getFollowers(Input: otherUserId!): returnFollowers!
+        getFollowees(Input: otherUserId!): returnFollowees!
+        getIsFollowee(Input: otherUserId!): returnIsFollowee!
+        getIsFollower(Input: otherUserId!): returnIsFollower!
     }
 
     extend type Mutation {
-        followUser(Input: otherUserId): Success!
-        unfollowUser(Input: otherUserId): Success!
+        followUser(Input: otherUserId!): ErrorOrMessage!
+        unfollowUser(Input: otherUserId!): ErrorOrMessage!
     }
+
+    union returnFollowers = Followers | Error
+    union returnFollowees = Followee | Error
+
+    union returnIsFollowee = IsFollowee | Error
+    union returnIsFollower = IsFollower | Error
 
     type UserList {
         name: String!
-        id: ID!
+        ${ANY_USER_ID_TYPE}!
     }
 
     type Followers {
         followers: [UserList]!
     }
 
-    type Following {
-        following: [UserList]!
+    type Followees{
+        followees: [UserList]!
     }
 
-    type Success {
-        message: String
-        errorMessage: String
-        success: Boolean!
-    }
 
-    type IsFollowing {
-        isFollowing: Boolean!
+    type IsFollowee{
+        isFollowee: Boolean!
     }
 
     type IsFollower {
         isFollower: Boolean!
     }
+
     input otherUserId {
-        otherUserId: ID!
+        ${OTHER_USER_ID_TYPE}!
     }
 `
 
