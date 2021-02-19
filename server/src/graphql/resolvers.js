7c9d1a1b0090f355f48925d4592486065b15c7a5
thatanjan <anjan.cules@gmail.com>
2021-02-19 21:07:57 +0600

    unionTypeResolvers are merged with all resolvers

--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -4,14 +4,16 @@ import { UserResolvers } from './User/User'
 import { PostResolvers } from './Post/Post'
 import { ProfileResolvers } from './Profile/Profile'
 import { FollowResolvers } from './Follow/Follow'
-import { DateResolvers } from 'graphql/customScalars'
+import unionTypeResolvers from './unionTypeResolvers'
+import { DateResolvers } from './customScalars'
 
 const resolvers = merge(
-    DateResolvers,
-    UserResolvers,
-    PostResolvers,
-    ProfileResolvers,
-    FollowResolvers
+	DateResolvers,
+	UserResolvers,
+	PostResolvers,
+	ProfileResolvers,
+	FollowResolvers,
+	unionTypeResolvers
 )
 
 export default resolvers
