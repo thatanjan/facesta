3c449257ac803c0a0eaea440360707e6d97a27b4
thatanjan <anjan.cules@gmail.com>
2021-02-21 17:48:04 +0600

    not needed query and mutations are removed from permissions file

--- server/src/config/permissions.js
+++ server/src/config/permissions.js
@@ -1,47 +1,39 @@
 import { rule, shield } from 'graphql-shield'
 
 const isAuthenticated = rule()((_, __, { user, error }) => {
-    if (user) {
-        return true
-    }
+	if (user) {
+		return true
+	}
 
-    if (error) {
-        return new Error(error)
-    }
+	if (error) {
+		return new Error(error)
+	}
 })
 
 export default shield(
-    {
-        Mutation: {
-            createPost: isAuthenticated,
-            deletePost: isAuthenticated,
-            updatePersonal: isAuthenticated,
-            addExperience: isAuthenticated,
-            addEducation: isAuthenticated,
-            updateSocial: isAuthenticated,
-            updatePrivacy: isAuthenticated,
-            followUser: isAuthenticated,
-            unfollowUser: isAuthenticated,
-            likePost: isAuthenticated,
-            removeLikePost: isAuthenticated,
-            commentPost: isAuthenticated,
-            removeCommentPost: isAuthenticated,
-        },
-        Query: {
-            getSinglePost: isAuthenticated,
-            getAllPost: isAuthenticated,
-            getPersonal: isAuthenticated,
-            getExperience: isAuthenticated,
-            getEducation: isAuthenticated,
-            getSocial: isAuthenticated,
-            getPrivacy: isAuthenticated,
-            getFollowers: isAuthenticated,
-            getFollowing: isAuthenticated,
-            getIsFollowing: isAuthenticated,
-            getIsFollower: isAuthenticated,
-        },
-    },
-    {
-        allowExternalErrors: true,
-    }
+	{
+		Mutation: {
+			createPost: isAuthenticated,
+			deletePost: isAuthenticated,
+			updatePersonal: isAuthenticated,
+			followUser: isAuthenticated,
+			unfollowUser: isAuthenticated,
+			likePost: isAuthenticated,
+			removeLikePost: isAuthenticated,
+			commentPost: isAuthenticated,
+			removeCommentPost: isAuthenticated,
+		},
+		Query: {
+			getSinglePost: isAuthenticated,
+			getAllPost: isAuthenticated,
+			getPersonalData: isAuthenticated,
+			getFollowers: isAuthenticated,
+			getFollowees: isAuthenticated,
+			getIsFollowee: isAuthenticated,
+			getIsFollower: isAuthenticated,
+		},
+	},
+	{
+		allowExternalErrors: true,
+	}
 )
