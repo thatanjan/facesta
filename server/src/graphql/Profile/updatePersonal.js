ace92a8d9aba20acd86a78bdfd98248606dff479
thatanjan <anjan.cules@gmail.com>
2021-02-21 13:01:53 +0600

    refactored

--- server/src/graphql/Profile/updatePersonal.js
+++ server/src/graphql/Profile/updatePersonal.js
@@ -1,27 +1,26 @@
 import Profile from 'models/Profile'
 
 const resolver = {
-    Mutation: {
-        updatePersonal: async (_, { Input }, { user: { id } }) => {
-            const updateObject = {}
+	Mutation: {
+		updatePersonal: async (_, { Input }, { user: { id } }) => {
+			const updateObject = {}
 
-            for (let i in Input) {
-                updateObject[`personal.${i}`] = Input[i]
-            }
+			// eslint-disable-next-line
+			for (const i in Input) {
+				if (Input) {
+					updateObject[`personal.${i}`] = Input[i]
+				}
+			}
 
-            const update = await Profile.findOneAndUpdate(
-                { user: id },
-                updateObject,
-                {
-                    projection: 'personal',
-                    useFindAndModify: false,
-                    new: true,
-                }
-            )
+			const update = await Profile.findOneAndUpdate({ user: id }, updateObject, {
+				projection: 'personal',
+				useFindAndModify: false,
+				new: true,
+			})
 
-            return update.personal
-        },
-    },
+			return update.personal
+		},
+	},
 }
 
 export default resolver
