156ae8a28e0b5ac901cee96ba5c31678e2a7a4e5
thatanjan <anjan.cules@gmail.com>
2021-02-21 12:49:31 +0600

    if deletion is successful then it will send a message else sends a error message

--- server/src/graphql/User/deleteUser.js
+++ server/src/graphql/User/deleteUser.js
@@ -1,52 +1,50 @@
 import { createConnection } from 'mongoose'
 
-import sendMessage from 'utils/errorMessage'
+import sendErrorMessage from 'utils/errorMessage'
+import sendMessage from 'utils/message'
 import User from 'models/User'
 import Profile from 'models/Profile'
 import Follow from 'models/Follow'
 
-const deletePostCollection = async (id) => {
-    const connection = createConnection(process.env.POSTS_DB_URI)
+const deletePostCollection = async id => {
+	const connection = createConnection(process.env.POSTS_DB_URI)
 
-    try {
-        await connection.dropCollection(id)
-    } catch (error) {
-        return false
-    }
+	try {
+		await connection.dropCollection(id)
+		return true
+	} catch (error) {
+		return false
+	}
 }
 
 const resolver = {
-    Mutation: {
-        deleteUser: async (_, __, { user: { id } }) => {
-            try {
-                const user = await User.findById(id)
+	Mutation: {
+		deleteUser: async (_, __, { user: { id } }) => {
+			try {
+				const user = await User.findById(id)
 
-                if (!user) {
-                    return sendMessage(false, 'no user found')
-                }
+				if (!user) {
+					return sendErrorMessage('no user found')
+				}
 
-                const queryCondition = { user: id }
+				const queryCondition = { user: id }
 
-                await Profile.findOneAndDelete(queryCondition)
-                await deletePostCollection(id)
-                await Follow.findOneAndDelete(queryCondition)
+				await Profile.findOneAndDelete(queryCondition)
+				await deletePostCollection(id)
+				await Follow.findOneAndDelete(queryCondition)
 
-                const removedUser = await user.remove()
+				const removedUser = await user.remove()
 
-                if (!removedUser) {
-                    return sendMessage(false, 'user account is not deleted')
-                }
+				if (!removedUser) {
+					return sendErrorMessage('user account is not deleted')
+				}
 
-                return sendMessage(
-                    true,
-                    null,
-                    'your account is successfully deleted'
-                )
-            } catch (error) {
-                return sendMessage(false, error)
-            }
-        },
-    },
+				return sendMessage('your account is successfully deleted')
+			} catch (error) {
+				return sendErrorMessage(error)
+			}
+		},
+	},
 }
 
 export default resolver
