1c05edd155ef80d3613e16bc31e50eca14e1e99f
thatanjan <anjan.cules@gmail.com>
2021-02-21 12:55:09 +0600

    mutation resolver is removed that is not needed

--- server/src/graphql/Profile/addToProfile.js
+++ server/src/graphql/Profile/addToProfile.js
@@ -1,31 +1,28 @@
 import Profile from 'models/Profile'
 
 const addFields = async ({ field, Input: data, id }) => {
-    const result = await Profile.findOne({ user: id }, field)
+	const result = await Profile.findOne({ user: id }, field)
 
-    result[field].push(data)
+	result[field].push(data)
 
-    result.save()
+	result.save()
 
-    let returnData = result[field]
+	let returnData = result[field]
 
-    return returnData[returnData.length - 1]
+	return returnData[returnData.length - 1]
 }
 
 const EXPERIENCE = 'experience'
 const EDUCATION = 'education'
 
-const resolverFunction = (field) => {
-    return async (_, { Input }, { user: { id } }) => {
-        return await addFields({ field, Input, id })
-    }
+const resolverFunction = field => {
+	return async (_, { Input }, { user: { id } }) => {
+		return await addFields({ field, Input, id })
+	}
 }
 
 const resolver = {
-    Mutation: {
-        addExperience: resolverFunction(EXPERIENCE),
-        addEducation: resolverFunction(EDUCATION),
-    },
+	Mutation: {},
 }
 
 export default resolver
