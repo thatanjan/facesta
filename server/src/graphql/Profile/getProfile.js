070c6147606cbaafe91707398e9eb9412adc6fde
thatanjan <anjan.cules@gmail.com>
2021-02-21 12:56:38 +0600

    mutation resolver is removed that is not needed and one muation is renamed. unnecessary else statement is removed

--- server/src/graphql/Profile/getProfile.js
+++ server/src/graphql/Profile/getProfile.js
@@ -5,35 +5,31 @@ const EXPERIENCE = 'experience'
 const EDUCATION = 'education'
 const SOCIAL = 'social'
 
-const resolverFunction = (field) => {
-    return async (_, { Input: { profileUserId } }) => {
-        let query
+const resolverFunction = field => {
+	return async (_, { Input: { profileUserId } }) => {
+		let query
 
-        if (field === PERSONAL) {
-            query = await Profile.findOne(
-                { user: profileUserId },
-                field
-            ).populate('user')
+		if (field === PERSONAL) {
+			query = await Profile.findOne({ user: profileUserId }, field).populate(
+				'user'
+			)
 
-            const data = query[`${field}`]
+			const data = query[`${field}`]
 
-            data.name = query.user.name
+			data.name = query.user.name
 
-            return data
-        } else {
-            query = await Profile.findOne({ user: profileUserId }, field)
-            return query[`${field}`]
-        }
-    }
+			return data
+		}
+
+		query = await Profile.findOne({ user: profileUserId }, field)
+		return query[`${field}`]
+	}
 }
 
 const resolver = {
-    Query: {
-        getPersonal: resolverFunction(PERSONAL),
-        getExperience: resolverFunction(EXPERIENCE),
-        getEducation: resolverFunction(EDUCATION),
-        getSocial: resolverFunction(SOCIAL),
-    },
+	Query: {
+		getPersonalData: resolverFunction(PERSONAL),
+	},
 }
 
 export default resolver
