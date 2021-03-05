932829679401ac6f7094ab6a12802b381dc7fa51
thatanjan <anjan.cules@gmail.com>
2021-03-05 18:29:21 +0600

    USER_ID_TYPE is used instead of  ANY_USER_ID_TYPE

--- server/src/graphql/Follow/FollowType.js
+++ server/src/graphql/Follow/FollowType.js
@@ -1,27 +1,27 @@
 import { gql } from 'apollo-server-express'
-import { ANY_USER_ID_TYPE, ERROR_MESSAGE_TYPE } from 'variables/commonText'
+import { USER_ID_TYPE, ERROR_MESSAGE_TYPE } from 'variables/commonText'
 
 const FollowType = gql`
     extend type Query {
-        getFollowers(${ANY_USER_ID_TYPE}): Followers!
-        getFollowees(${ANY_USER_ID_TYPE}): Followees!
-        getIsFollowee(${ANY_USER_ID_TYPE}!): IsFollowee!
-        getIsFollower(${ANY_USER_ID_TYPE}!): IsFollower!
+        getFollowers(${USER_ID_TYPE}): Followers!
+        getFollowees(${USER_ID_TYPE}): Followees!
+        getIsFollowee(${USER_ID_TYPE}!): IsFollowee!
+        getIsFollower(${USER_ID_TYPE}!): IsFollower!
     }
 
     extend type Mutation {
-        followUser(${ANY_USER_ID_TYPE}!): ErrorOrMessage!
-        unfollowUser(${ANY_USER_ID_TYPE}!): ErrorOrMessage!
+        followUser(${USER_ID_TYPE}!): ErrorOrMessage!
+        unfollowUser(${USER_ID_TYPE}!): ErrorOrMessage!
     }
 
 
     type UserList {
         name: String!
-        ${ANY_USER_ID_TYPE}!
+        ${USER_ID_TYPE}!
     }
 
     type Followers {
-        followers: [UserList]
+        followers: [UserList]!
         ${ERROR_MESSAGE_TYPE}
     }
 
