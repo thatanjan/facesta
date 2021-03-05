89a0e133515c251f3f36a364fb0dba3b6757029b
thatanjan <anjan.cules@gmail.com>
2021-03-05 17:48:37 +0600

    queries are updated

--- client/src/graphql/queries/postQueries.ts
+++ client/src/graphql/queries/postQueries.ts
@@ -1,35 +1,24 @@
 import { gql } from 'graphql-request'
+import { ERROR_OR_MESSAGE } from 'variables/global'
 
 export const getAllPost = gql`
-	query getAllPost($start: Int!, $postUserId: ID) {
-		getAllPost(Input: { start: $start, postUserId: $postUserId }) {
-			... on AllPost {
-				posts {
-					text
-				}
-			}
-
-			... on Success {
-				message
-				errorMessage
-				success
+	query getAllPost($start: Int!, $postOwnerID: ID!) {
+		getAllPost(Input: { start: $start, postOwnerID: $postOwnerID }) {
+			posts {
+				text
+				postID
+				${ERROR_OR_MESSAGE}
 			}
 		}
 	}
 `
 
 export const getSinglePost = gql`
-	query getSinglePost($postId: ID!, $postUserId: ID) {
-		getSinglePost(Input: { postId: $postId, postUserId: $postUserId }) {
-			... on Post {
+	query getSinglePost($postID: ID!, $postOwnerID: ID) {
+		getSinglePost(Input: { postID: $postID, postOwnerID: $postOwnerID }) {
 				text
-			}
-
-			... on Success {
-				message
-				errorMessage
-				success
-			}
+				postID
+				${ERROR_OR_MESSAGE}
 		}
 	}
 `
