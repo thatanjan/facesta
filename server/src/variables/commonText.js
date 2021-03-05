e6fd675cfcf6c11fce6dd7bbe5fe53d285f109b0
thatanjan <anjan.cules@gmail.com>
2021-03-05 18:23:38 +0600

    userId is added

--- server/src/variables/commonText.js
+++ server/src/variables/commonText.js
@@ -1,5 +1,6 @@
 import { ERROR_MESSAGE, MESSAGE } from 'variables/global'
 
+const USER_ID = 'userID'
 const POST_ID = 'postID'
 const POST_OWNER_ID = 'postOwnerID'
 const OWNER_ID = 'ownerID'
@@ -7,6 +8,7 @@ const OTHER_USER_ID = 'otherUserID'
 const ANY_USER_ID = 'anyUserID'
 const PROFILE_OWNER_ID = 'profileOwnerID'
 
+const USER_ID_TYPE = `${USER_ID}: ID`
 const POST_ID_TYPE = `${POST_ID}: ID`
 const POST_OWNER_ID_TYPE = `${POST_OWNER_ID}: ID`
 const OWNER_ID_TYPE = `${OWNER_ID}: ID`
@@ -33,4 +35,5 @@ export {
 	ERROR_OR_MESSAGE_TYPE,
 	MESSAGE_TYPE,
 	ERROR_MESSAGE_TYPE,
+	USER_ID_TYPE,
 }
