b464e18da9600fcf98c55e37a1d89920601518cf
thatanjan <anjan.cules@gmail.com>
2021-03-05 17:35:38 +0600

    createPost mutation is updated

--- client/src/graphql/mutations/postMutations.ts
+++ client/src/graphql/mutations/postMutations.ts
@@ -1,10 +1,11 @@
 import { gql } from 'graphql-request'
+import { ERROR_OR_MESSAGE } from 'variables/global'
 
 // eslint-disable-next-line
 export const createPost = gql`
 	mutation createPost($text: String!) {
 		createPost(Input: { text: $text }) {
-			text
+			${ERROR_OR_MESSAGE}
 		}
 	}
 `
