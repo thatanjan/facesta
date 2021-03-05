32cd8dc6daba9702e3f4b668ee156927cd57e644
thatanjan <anjan.cules@gmail.com>
2021-03-05 13:22:36 +0600

    no need to send personal data. will be send message or error message

--- server/src/graphql/Profile/updatePersonal.js
+++ server/src/graphql/Profile/updatePersonal.js
@@ -1,24 +1,28 @@
 import Profile from 'models/Profile'
+import sendErrorMessage from 'utils/errorMessage'
+import sendMessage from 'utils/message'
 
 const resolver = {
 	Mutation: {
 		updatePersonalData: async (_, { Input }, { user: { id } }) => {
-			const updateObject = {}
+			try {
+				const updateObject = {}
 
-			// eslint-disable-next-line
-			for (const i in Input) {
-				if (Input) {
-					updateObject[`personal.${i}`] = Input[i]
+				// eslint-disable-next-line
+				for (const i in Input) {
+					if (Input) {
+						updateObject[`personal.${i}`] = Input[i]
+					}
 				}
+
+				const update = await Profile.findOneAndUpdate({ user: id }, updateObject)
+
+				if (!update) return sendErrorMessage('update unsuccessful')
+
+				return sendMessage('Update Successful')
+			} catch (error) {
+				sendErrorMessage(error)
 			}
-
-			const update = await Profile.findOneAndUpdate({ user: id }, updateObject, {
-				projection: 'personal',
-				useFindAndModify: false,
-				new: true,
-			})
-
-			return update.personal
 		},
 	},
 }
