ef8c3e2410f60f456ae4b088338867946b34398f
thatanjan <anjan.cules@gmail.com>
2021-03-07 07:32:34 +0600

    useMemoizeSWROption.ts   file delted

--- client/src/hooks/useMemoizeSWROption.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo, DependencyList } from 'react'
-import { AnyObject } from 'interfaces/global'
-
-const useMemoizeSWROption = (
-	options: AnyObject,
-	dependencies: DependencyList
-) => useMemo(() => options, dependencies)
-
-export default useMemoizeSWROption
