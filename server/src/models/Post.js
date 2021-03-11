5ed9851d840f29571a6553a56d2d887361634929
thatanjan <anjan.cules@gmail.com>
2021-03-11 08:40:53 +0600

    post model can have image

--- server/src/models/Post.js
+++ server/src/models/Post.js
@@ -1,40 +1,39 @@
-import mongoose from 'mongoose'
+import { Schema, createConnection } from 'mongoose'
 
 import User from 'models/User'
-
-const Schema = mongoose.Schema
 
 const objectId = Schema.Types.ObjectId
 
 const user = {
-    type: objectId,
-    ref: User,
+	type: objectId,
+	ref: User,
 }
 
 const CommentedUserSchema = new Schema({
-    user,
-    text: { type: String, required: true },
-    date: { type: Date, default: Date.now() },
+	user,
+	text: { type: String, required: true },
+	date: { type: Date, default: Date.now() },
 })
 
 const schema = {
-    text: { type: String, required: true },
-    likes: [user],
-    comments: [CommentedUserSchema],
-    public: {
-        type: Boolean,
-        default: true,
-    },
-    date: { type: Date, default: Date.now() },
+	text: { type: String, required: true },
+	likes: [user],
+	comments: [CommentedUserSchema],
+	public: {
+		type: Boolean,
+		default: true,
+	},
+	imageURL: { type: String },
+	date: { type: Date, default: Date.now() },
 }
 
 export const PostSchema = new Schema(schema, { versionKey: '1' })
 
-const PostModel = (modelName) => {
-    const PostConnection = mongoose.createConnection(process.env.POSTS_DB_URI)
-    const Post = PostConnection.model(modelName, PostSchema)
+const PostModel = modelName => {
+	const PostConnection = createConnection(process.env.POSTS_DB_URI)
+	const Post = PostConnection.model(modelName, PostSchema)
 
-    return Post
+	return Post
 }
 
 export default PostModel
