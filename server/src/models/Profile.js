bb1c83cccb6165c2adb95760acf47b016613c251
thatanjan <anjan.cules@gmail.com>
2021-03-11 08:41:10 +0600

    a user can have profile and cover picture

--- server/src/models/Profile.js
+++ server/src/models/Profile.js
@@ -1,112 +1,118 @@
-import mongoose from 'mongoose'
-
-const Schema = mongoose.Schema
+import { Schema, model } from 'mongoose'
 
 const objectId = Schema.Types.ObjectId
 
+const images = {
+	profile: {
+		type: String,
+	},
+	cover: { type: String },
+}
+
 const ExperienceSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    company: {
-        type: String,
-        required: true,
-    },
-    from: {
-        type: Date,
-    },
-    to: {
-        type: Date,
-    },
+	title: {
+		type: String,
+		required: true,
+	},
+	company: {
+		type: String,
+		required: true,
+	},
+	from: {
+		type: Date,
+	},
+	to: {
+		type: Date,
+	},
 })
 
 const EducationSchema = new Schema({
-    school: {
-        type: String,
-        required: true,
-    },
-    degree: {
-        type: String,
-        required: true,
-    },
-    field_of_study: {
-        type: String,
-    },
-    from: {
-        type: Date,
-    },
-    to: {
-        type: Date,
-    },
+	school: {
+		type: String,
+		required: true,
+	},
+	degree: {
+		type: String,
+		required: true,
+	},
+	field_of_study: {
+		type: String,
+	},
+	from: {
+		type: Date,
+	},
+	to: {
+		type: Date,
+	},
 })
 
 const SocialSchema = {
-    youtube: {
-        type: String,
-        default: '',
-    },
-    twitter: {
-        type: String,
-        default: '',
-    },
-    facebook: {
-        type: String,
-        default: '',
-    },
-    linkedin: {
-        type: String,
-        default: '',
-    },
-    instagram: {
-        type: String,
-        default: '',
-    },
+	youtube: {
+		type: String,
+		default: '',
+	},
+	twitter: {
+		type: String,
+		default: '',
+	},
+	facebook: {
+		type: String,
+		default: '',
+	},
+	linkedin: {
+		type: String,
+		default: '',
+	},
+	instagram: {
+		type: String,
+		default: '',
+	},
 }
 
 const data = {
-    user: {
-        type: objectId,
-        default: null,
-        ref: 'users',
-    },
-    personal: {
-        dateOfBirth: {
-            type: Date,
-        },
-        website: {
-            type: String,
-        },
-        status: {
-            type: String,
-        },
-        location: {
-            type: String,
-        },
+	user: {
+		type: objectId,
+		default: null,
+		ref: 'users',
+	},
+	personal: {
+		dateOfBirth: {
+			type: Date,
+		},
+		website: {
+			type: String,
+		},
+		status: {
+			type: String,
+		},
+		location: {
+			type: String,
+		},
 
-        skills: {
-            type: [String],
-        },
-        bio: {
-            type: String,
-        },
-    },
-    experience: [ExperienceSchema],
-    education: [EducationSchema],
+		skills: {
+			type: [String],
+		},
+		bio: {
+			type: String,
+		},
+	},
+	experience: [ExperienceSchema],
+	education: [EducationSchema],
 
-    social: SocialSchema,
-    public: {
-        type: Boolean,
-        default: true,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+	social: SocialSchema,
+	public: {
+		type: Boolean,
+		default: true,
+	},
+	images,
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 }
 
-const ProfileSchema = new Schema(data, { versionKey: '1' })
+const ProfileSchema = new Schema(data, { versionKey: '2' })
 
-const ProfileModel = mongoose.model('profile', ProfileSchema)
+const ProfileModel = model('profile', ProfileSchema)
 
 export default ProfileModel
