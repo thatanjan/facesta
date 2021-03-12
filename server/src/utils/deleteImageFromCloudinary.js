914a75b82f531e7adc224a78cc2d9eda7dc15258
thatanjan <anjan.cules@gmail.com>
2021-03-12 17:19:55 +0600

    function will return await result of delting image

--- server/src/utils/deleteImageFromCloudinary.js
+++ server/src/utils/deleteImageFromCloudinary.js
@@ -4,12 +4,10 @@ const deleteImage = async imageID => {
 	if (!imageID) return false
 
 	try {
-		await cloudinary.v2.uploader.destroy(imageID)
+		return await cloudinary.v2.uploader.destroy(imageID)
 	} catch (err) {
 		return err
 	}
-
-	return false
 }
 
 export default deleteImage
