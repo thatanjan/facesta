adb69fac43bebf4645fe8df2c1748ce0531df866
thatanjan <anjan.cules@gmail.com>
2021-03-12 14:41:34 +0600

    image upload is removed from updatePersonalData mutation

--- server/src/graphql/Profile/updatePersonal.js
+++ server/src/graphql/Profile/updatePersonal.js
@@ -2,54 +2,21 @@ import User from 'models/User'
 import Profile from 'models/Profile'
 import sendErrorMessage from 'utils/errorMessage'
 import sendMessage from 'utils/message'
-import uploadImage from 'utils/uploadToCloudinary'
-import imageConfig from 'variables/cloudinaryVariables'
-import cloudinary from 'cloudinary'
-import deleteOldImage from 'utils/deleteImageFromCloudinary'
-
-const PATH = 'profile/'
 
 const resolver = {
 	Mutation: {
 		updatePersonalData: async (_, { Input }, { user: { id } }) => {
 			try {
-				const updateObject = {}
-
 				const inputKeys = Object.keys(Input)
 
 				const personalData = await Profile.findOne({ user: id }, 'personal')
 
 				const { personal } = personalData
 
-				console.log(personalData)
 				// eslint-disable-next-line
 				inputKeys.forEach(async item => {
 					switch (item) {
 						case 'name':
-							break
-
-						case 'image':
-							const {
-								personal: { image: oldImageID },
-							} = personalData
-
-							const deleteImageRes = await deleteOldImage({
-								path: PATH,
-								imageID: oldImageID,
-							})
-
-							if (deleteImageRes.message)
-								return sendErrorMessage('some thing went wrong')
-
-							const { image } = Input
-
-							const publicID = await uploadImage(image, {
-								...imageConfig,
-								folder: PATH,
-							})
-
-							console.log(publicID)
-
 							break
 
 						default:
