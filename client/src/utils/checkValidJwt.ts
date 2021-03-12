180eb04373bd0466f4e80d3a457fb98e1b637f64
thatanjan <anjan.cules@gmail.com>
2021-03-12 20:21:57 +0600

    localhost port changed

--- client/src/utils/checkValidJwt.ts
+++ client/src/utils/checkValidJwt.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
-import { ifProduction } from 'variables/global'
+import { ifProduction, END_POINT } from 'variables/global'
 
 // eslint-disable-next-line
 const checkValidJwt = async (jwt: string) => {
 	const END_POINT = ifProduction
 		? process.env.NEXT_PUBLIC_SERVER_VALIDATE
-		: 'http://localhost:8000/validate'
+		: 'http://localhost:9000/validate'
 
 	try {
 		const isValid = await axios.post(END_POINT as string, { data: { jwt } })
