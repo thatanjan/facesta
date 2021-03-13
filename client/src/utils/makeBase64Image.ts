a3e23cea830b58f26cdab6609ad2b649402e0bfa
thatanjan <anjan.cules@gmail.com>
2021-03-13 21:25:18 +0600

    makeBase64 function is taking 2nd param as setState to provide the base64 value

--- client/src/utils/makeBase64Image.ts
+++ client/src/utils/makeBase64Image.ts
@@ -1,10 +1,8 @@
-const makeBase64 = (file: File) => {
+const makeBase64 = (file: File, setState: Function) => {
 	const fileReader = new FileReader()
 	fileReader.readAsDataURL(file)
 
-	console.log(fileReader.result)
-
-	return (fileReader.onload = () => fileReader.result)
+	fileReader.onload = () => setState(fileReader.result)
 }
 
 export default makeBase64
