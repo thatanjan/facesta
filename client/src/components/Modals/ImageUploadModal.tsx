4ce2c80e763027bdc1ed147592385144580a8b3c
thatanjan <anjan.cules@gmail.com>
2021-03-13 12:57:25 +0600

    image upload modal is styled but not responsive

--- client/src/components/Modals/ImageUploadModal.tsx
+++ client/src/components/Modals/ImageUploadModal.tsx
@@ -1,9 +1,15 @@
 import { useDropzone } from 'react-dropzone'
 import React, { useState } from 'react'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import MuiDialogContent from '@material-ui/core/DialogContent'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/DialogContentText'
+import MuiDialogActions from '@material-ui/core/DialogActions'
+import PublishIcon from '@material-ui/icons/Publish'
 import Dialog from '@material-ui/core/Dialog'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
+import DialogActions from '@material-ui/core/DialogActions'
 
 interface Props {
 	open: boolean
@@ -35,9 +41,17 @@ const useStyles = makeStyles({
 	rejectStyle: {
 		borderColor: '#ff1744',
 	},
+	dialogContentStyle: {
+		display: 'grid',
+		height: '30vh',
+		placeItems: 'center',
+	},
+	uploadIconStyle: {},
 })
 
 const UploadModal = ({ open, setOpen }: Props) => {
+	const { dialogContentStyle, uploadIconStyle } = useStyles()
+
 	const handleClose = () => {
 		setOpen(false)
 	}
@@ -48,9 +62,20 @@ const UploadModal = ({ open, setOpen }: Props) => {
 				onClose={handleClose}
 				aria-labelledby='simple-dialog-title'
 				open={open}
+				maxWidth='sm'
+				fullWidth
 			>
-				<DialogTitle id='simple-dialog-title'>Upload your Image</DialogTitle>
-				<Button onClick={handleClose}>Done</Button>
+				<DialogTitle style={{ textAlign: 'center' }} id='simple-dialog-title'>
+					Upload your Image
+				</DialogTitle>
+				<MuiDialogContent className={dialogContentStyle}>
+					<PublishIcon className={uploadIconStyle} fontSize='large' />
+				</MuiDialogContent>
+				<DialogActions>
+					<Button autoFocus onClick={handleClose} color='primary'>
+						Done
+					</Button>
+				</DialogActions>
 			</Dialog>
 		</>
 	)
