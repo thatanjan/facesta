ab8e2c1b953e11150c03eefe2f5f67d85d946cdb
thatanjan <anjan.cules@gmail.com>
2021-03-13 23:40:01 +0600

    action will do the real work when ok button is triggered

--- client/src/components/Images/ImagePreview.tsx
+++ client/src/components/Images/ImagePreview.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image'
 import Dialog from '@material-ui/core/Dialog'
-import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import Button from '@material-ui/core/Button'
 
-import { AnyObject } from 'interfaces/global'
 import makeBase64Image from 'utils/makeBase64Image'
 
 interface CustomFile extends File {
@@ -25,12 +22,11 @@ const ImagePreviewModal = ({
 	setPreviewOpen,
 	previewOpen,
 	file,
-	action,
 	setRejected,
+	action,
 }: Props) => {
 	const handleClose = () => {
-		const base64Image = makeBase64Image(file)()
-		action(base64Image)
+		makeBase64Image(file, action)
 		setPreviewOpen(false)
 	}
 
@@ -54,7 +50,7 @@ const ImagePreviewModal = ({
 			</DialogContent>
 
 			<DialogActions>
-				<Button onClick={handleReject}>disgard</Button>
+				<Button onClick={handleReject}>discard</Button>
 				<Button onClick={handleClose}>OK</Button>
 			</DialogActions>
 		</Dialog>
