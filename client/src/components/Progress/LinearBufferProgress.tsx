1b8b9c32d7af8ac544ec6e8a9396756cb6e6b7b8
thatanjan <anjan.cules@gmail.com>
2021-03-14 07:51:56 +0600

    loader customized. it will react if things fail or succeed

--- client/src/components/Progress/LinearBufferProgress.tsx
+++ client/src/components/Progress/LinearBufferProgress.tsx
@@ -2,23 +2,52 @@ import { useState, useRef, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import LinearProgress from '@material-ui/core/LinearProgress'
 
+const FAILED = 'failed'
+
 const useStyles = makeStyles({
 	root: {
 		width: '100%',
 	},
 })
 
-const LinearBuffer = () => {
+interface Props {
+	success: boolean | string
+	shouldStop: boolean
+	setShouldStop: (bool: boolean) => void
+}
+
+const LinearBuffer = ({ shouldStop, setShouldStop, success }: Props) => {
 	const classes = useStyles()
 	const [progress, setProgress] = useState(0)
 	const [buffer, setBuffer] = useState(10)
 
+	useEffect(() => {
+		if (success === FAILED) {
+			setShouldStop(true)
+			setProgress(0)
+			setBuffer(0)
+		}
+
+		if (success) {
+			setProgress(100)
+			setBuffer(100)
+
+			setShouldStop(true)
+		}
+	}, [success, shouldStop])
+
 	const progressRef = useRef(() => {})
 	useEffect(() => {
 		progressRef.current = () => {
+			if (shouldStop) {
+				return true
+			}
+
 			if (progress > 100) {
-				setProgress(0)
-				setBuffer(10)
+				setTimeout(() => {
+					setProgress(0)
+					setBuffer(10)
+				}, 2000)
 			} else {
 				const diff = Math.random() * 10
 				const diff2 = Math.random() * 10
