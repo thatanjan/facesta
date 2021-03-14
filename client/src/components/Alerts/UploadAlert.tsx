5871cc140b51220787b36285aa6b0a11191a55ac
thatanjan <anjan.cules@gmail.com>
2021-03-14 08:33:28 +0600

    slide animation added

--- client/src/components/Alerts/UploadAlert.tsx
+++ client/src/components/Alerts/UploadAlert.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import { Alert, AlertTitle } from '@material-ui/lab'
+import Slide from '@material-ui/core/Slide'
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -16,17 +17,30 @@ const useStyles = makeStyles((theme: Theme) =>
 interface Props {
 	severity: 'error' | 'info' | 'success' | 'warning'
 	message: 'string'
+	checked: boolean
 }
 
-export default function DescriptionAlerts({ severity, message }: Props) {
+export default function DescriptionAlerts({
+	severity,
+	message,
+	checked,
+}: Props) {
 	const classes = useStyles()
 
 	return (
-		<div className={classes.root}>
-			<Alert severity={severity}>
-				<AlertTitle style={{ textTransform: 'capitalize' }}>{severity}</AlertTitle>
-				{message}
-			</Alert>
-		</div>
+		<Slide
+			direction='up'
+			in={checked}
+			mountOnEnter
+			unmountOnExit
+			timeout={{ exit: 3 }}
+		>
+			<div className={classes.root}>
+				<Alert severity={severity}>
+					<AlertTitle style={{ textTransform: 'capitalize' }}>{severity}</AlertTitle>
+					{message}
+				</Alert>
+			</div>
+		</Slide>
 	)
 }
