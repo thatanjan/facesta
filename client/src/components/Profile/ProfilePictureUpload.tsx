b33419e741764d3ed72bc65151c8325d0a04bc73
thatanjan <anjan.cules@gmail.com>
2021-03-15 14:22:57 +0600

    when a component is shown other will be hidden

--- client/src/components/Profile/ProfilePictureUpload.tsx
+++ client/src/components/Profile/ProfilePictureUpload.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import EditIcon from '@material-ui/icons/Edit'
 import { makeStyles } from '@material-ui/core/styles'
 import { mutate } from 'swr'
 
-import ImageUploadModal from 'components/Modals/ImageUploadModal'
+import ImageUploadModal, {
+	ApprovedType,
+} from 'components/Modals/ImageUploadModal'
+import ImagePreview from 'components/Images/ImagePreview'
 import LoadingModal from 'components/Modals/LoadingModal'
 
 import { useProfileUserID } from 'hooks/profileContextHooks'
@@ -12,37 +15,76 @@ import createRequest from 'utils/createRequest'
 import { getProfilePicture } from 'graphql/queries/profileQueries'
 import { uploadProfilePicture } from 'graphql/mutations/userMutations'
 
+import { CustomFile } from 'interfaces/upload'
+
 const useStyles = makeStyles(() => ({
 	editIconStyle: { marginLeft: '100%', transform: 'translate(-100%, -100%)' },
 }))
 
 const ProfilePictureUpload = () => {
-	const [file, setFile] = useState({})
-	const [approved, setApproved] = useState(false)
+	const [file, setFile] = useState<CustomFile | {}>({})
+	const [approved, setApproved] = useState<ApprovedType>(null)
 	const [loading, setLoading] = useState(false)
 	const [success, setSuccess] = useState(false)
 	const [uploadModalOpen, setUploadModalOpen] = useState(false)
+	const [showPreview, setShowPreview] = useState(false)
+	const [previewLink, setPreviewLink] = useState('')
 
 	const { editIconStyle } = useStyles()
 
-	const uploadModalProps = { setFile, uploadModalOpen, setUploadModalOpen }
+	const uploadModalProps = {
+		setFile,
+		uploadModalOpen,
+		setUploadModalOpen,
+		setApproved,
+	}
 
-	// const profileUserID = useProfileUserID()
+	const profileUserID = useProfileUserID()
 
 	const openUploadModal = () => setUploadModalOpen(true)
 
-	// const action = async (image: ArrayBuffer | string | null) => {
-	// 	setLoadingModalOpen(true)
+	const action = async (image: ArrayBuffer | string | null) => {
+		const res = await createRequest({
+			key: uploadProfilePicture,
+			values: { image },
+		})
 
-	// 	const res = await createRequest({
-	// 		key: uploadProfilePicture,
-	// 		values: { image },
-	// 	})
+		if (res?.uploadProfilePicture.message) {
+			mutate([getProfilePicture, profileUserID])
+		}
+	}
 
-	// 	if (res?.uploadProfilePicture.message) {
-	// 		mutate([getProfilePicture, profileUserID])
-	// 	}
-	// }
+	useEffect(() => {
+		const { valid, previewLink: link } = file as CustomFile
+
+		if (valid) {
+			setPreviewLink(link)
+			setShowPreview(true)
+		}
+
+		return () => console.log('unmounted')
+	}, [file])
+
+	useEffect(() => {
+		if (approved) {
+			setLoading(true)
+			setTimeout(() => {
+				setLoading(false)
+			}, 2000)
+		}
+
+		if (approved === false) {
+			setShowPreview(false)
+			setUploadModalOpen(true)
+		}
+	}, [approved])
+
+	const imagePreviewProps = { previewLink, showPreview, setApproved }
+	const loadingModalProps = {
+		open: loading,
+		setOpen: setLoading,
+		title: 'profile picture is uploading',
+	}
 
 	return (
 		<>
@@ -52,13 +94,9 @@ const ProfilePictureUpload = () => {
 
 			{uploadModalOpen && <ImageUploadModal {...uploadModalProps} />}
 
-			{/* {loading && ( */}
-			{/* 	<LoadingModal */}
-			{/* 		title='profile picture is uploading' */}
-			{/* 		open={loadingModalOpen} */}
-			{/* 		setOpen={setLoadingModalOpen} */}
-			{/* 	/> */}
-			{/* )} */}
+			{showPreview && <ImagePreview {...imagePreviewProps} />}
+
+			{loading && <LoadingModal {...loadingModalProps} />}
 		</>
 	)
 }
