6d1453bd3f0e0f05d6e5a3e285fbfb40a3335e9f
thatanjan <anjan.cules@gmail.com>
2021-03-16 17:21:47 +0600

    typof fixed

--- client/src/graphql/mutations/postMutations.ts
+++ client/src/graphql/mutations/postMutations.ts
@@ -3,7 +3,7 @@ import { ERROR_OR_MESSAGE } from 'variables/global'
 
 // eslint-disable-next-line
 export const createPost = gql`
-	mutation createPost($text: String!, $image:String, headline:String! markdown:Boolean! ) {
+	mutation createPost($text: String!, $image:String, $headline:String! $markdown:Boolean! ) {
 		createPost(Input:{text: $text, image:$image, headline:$headline, markdown:$markdown}) {
 					${ERROR_OR_MESSAGE}
 				}
