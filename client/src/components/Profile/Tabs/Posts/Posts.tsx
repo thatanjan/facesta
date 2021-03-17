7f11862f556ef2fc9f13a935667e947bb86ea642
thatanjan <anjan.cules@gmail.com>
2021-03-17 08:49:38 +0600

    a tab for posts section for profile is created

---
+++ client/src/components/Profile/Tabs/Posts/Posts.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import PostType from 'interfaces/post'
+import SinglePost from 'components/Post/SinglePost'
+import useGetAllPosts from 'hooks/useGetAllpost'
+import { nanoid } from 'nanoid'
+
+const Posts = () => {
+	const { data, error } = useGetAllPosts(0)
+
+	console.log(data)
+
+	if (!data) return <div>... loading</div>
+	if (error) return 'error happened'
+
+	const {
+		getAllPost: { posts },
+	} = data
+
+	return (
+		<div>
+			{Array.isArray(posts) &&
+				posts.map(({ image, headline, text }: PostType) => {
+					const props = {
+						image,
+						headline,
+						text,
+						markdown: false,
+					}
+					return <SinglePost key={nanoid()} {...props} />
+				})}{' '}
+		</div>
+	)
+}
+
+export default Posts
