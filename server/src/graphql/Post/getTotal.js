c9bf51c530311a9eec391d6f04eedb8d4619e9f9
thatanjan <anjan.cules@gmail.com>
2021-03-18 21:19:43 +0600

    getting total comments with main resolver

--- server/src/graphql/Post/getTotal.js
+++ server/src/graphql/Post/getTotal.js
@@ -1,21 +1,29 @@
 import createPostModel from 'models/Post'
 import sendErrorMessage from 'utils/errorMessage'
 
+const TOTAL_LIKES = 'totalLikes'
+const TOTAL_COMMENTS = 'totalComments'
+
+const mainResolver = field => async (_, { Input: { postID, user } }) => {
+	const PostModel = createPostModel(user)
+
+	const post = await PostModel.findById(postID, field)
+
+	if (!post) {
+		return sendErrorMessage('no post found')
+	}
+
+	const response = {}
+
+	response[field] = post[field]
+
+	return response
+}
+
 const resolvers = {
 	Query: {
-		getTotalLikes: async (_, { Input: { postID, user } }) => {
-			const PostModel = createPostModel(user)
-
-			const post = await PostModel.findById(postID, 'totalLikes')
-
-			if (!post) {
-				return sendErrorMessage('no post found')
-			}
-
-			const { totalLikes } = post
-
-			return { totalLikes }
-		},
+		getTotalLikes: mainResolver(TOTAL_LIKES),
+		getTotalComments: mainResolver(TOTAL_COMMENTS),
 	},
 }
 
