5d96d27f4fa4e9d958d048770e8feb7ded689f3c
thatanjan <anjan.cules@gmail.com>
2021-03-18 12:23:51 +0600

    unnecessary files are deleted

--- server/src/graphql/Profile/Profile.js
+++ server/src/graphql/Profile/Profile.js
@@ -4,18 +4,12 @@ import ProfileType from './ProfileType'
 
 import updatePersonal from './updatePersonal'
 import getProfile from './getProfile'
-import addToProfile from './addToProfile'
-import updateSocial from './updateSocial'
-import updateSingleField from './updateSingleField'
-import updateProfilePicture from './profilePicture'
+import ProfilePicture from './profilePicture'
 
 export const ProfileTypedefs = [ProfileType]
 
 export const ProfileResolvers = merge(
 	updatePersonal,
 	getProfile,
-	addToProfile,
-	updateSocial,
-	updateSingleField,
-	updateProfilePicture
+	ProfilePicture
 )
