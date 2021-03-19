c5b5c744757d72b94112457354dd61beb260223a
thatanjan <anjan.cules@gmail.com>
2021-03-20 00:05:11 +0600

    Checkin likes or adding or removing likes is done through mongodb rather than locally. resolvers are wrapped with try catch block.

--- server/src/graphql/Post/likePost.js
+++ server/src/graphql/Post/likePost.js
@@ -3,11 +3,6 @@ import sendErrorMessage from 'utils/errorMessage'
 
 const LIKE = 'like'
 const REMOVE_LIKE = 'removeLike'
-const LIKES = 'likes'
-
-// eslint-disable-next-line
-const queryPostLikes = async (model, id) =>
-	await model.findById(id, `${LIKES} totalLikes`)
 
 const hasLiked = async ({ postID, postOwnerID, myID }) => {
 	const PostModel = createPostModel(postOwnerID.toString())
@@ -15,24 +10,28 @@ const hasLiked = async ({ postID, postOwnerID, myID }) => {
 	return PostModel.findOne({ _id: postID, likes: { $in: myID } }, 'likes')
 }
 
-const modifyLikes = ({ operation, likesQuery, id }) => {
-	const { likes } = likesQuery
+const modifyLikes = async ({ postID, postOwnerID, myID }, operation) => {
+	const PostModel = createPostModel(postOwnerID.toString())
 
 	switch (operation) {
 		case LIKE:
-			likes.push(id)
+			const res = await PostModel.updateOne(
+				{ _id: postID },
+				{ $push: { likes: myID }, $inc: { totalLikes: 1 } }
+			)
 
-			// eslint-disable-next-line
-			likesQuery.totalLikes++
+			if (res || res.nModified < 0) return true
 
-			break
+			return false
 
 		case REMOVE_LIKE:
-			likes.pull(id)
+			const res2 = await PostModel.updateOne(
+				{ _id: postID },
+				{ $pull: { likes: myID }, $inc: { totalLikes: -1 } }
+			)
 
-			// eslint-disable-next-line
-			likesQuery.totalLikes--
-			break
+			if (res2 || res2.nModified < 0) return true
+			return false
 
 		default:
 			return false
@@ -56,29 +55,25 @@ const responseMessage = operation => {
 
 const mainFunction = operation => {
 	return async (_, { Input: { postID, user } }, { user: { id } }) => {
-		const Post = createPostModel(user)
+		try {
+			const params = { postID, postOwnerID: user, myID: id }
 
-		const { likes } = likesQuery
+			if (operation === LIKE && (await hasLiked(params))) {
+				return sendErrorMessage('you have already liked this post')
+			}
 
-		if (
-			operation === LIKE &&
-			(await hasLiked({ postID, postOwnerID: user, myID: id }))
-		) {
-			return sendErrorMessage('you have already liked this post')
+			if (operation === REMOVE_LIKE && !(await hasLiked(params))) {
+				return sendErrorMessage('you have not liked this post.')
+			}
+
+			const hasModified = await modifyLikes(params, operation)
+
+			if (!hasModified) return sendErrorMessage('something went wrong')
+
+			return responseMessage(operation)
+		} catch (err) {
+			return sendErrorMessage(err)
 		}
-
-		if (
-			operation === REMOVE_LIKE &&
-			!(await hasLiked({ postID, postOwnerID: user, myID: id }))
-		) {
-			return sendErrorMessage('you have not liked this post.')
-		}
-
-		modifyLikes({ operation, id, likesQuery })
-
-		likesQuery.save()
-
-		return responseMessage(operation)
 	}
 }
 
@@ -89,14 +84,19 @@ const resolver = {
 	},
 	Query: {
 		hasLiked: async (_, { Input: { postID, user } }, { user: { id } }) => {
-			const doesUserExist = await hasLiked({
-				postID,
-				postOwnerID: user,
-				myID: id,
-			})
-			if (doesUserExist) return true
+			try {
+				const doesUserExist = await hasLiked({
+					postID,
+					postOwnerID: user,
+					myID: id,
+				})
 
-			return false
+				if (!doesUserExist) return true
+
+				return false
+			} catch (err) {
+				return sendErrorMessage(err)
+			}
 		},
 	},
 }
