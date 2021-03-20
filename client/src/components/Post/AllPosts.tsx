477af73c0ba75057cb03c6d43d5049b285c21eb1
thatanjan <anjan.cules@gmail.com>
2021-03-20 12:30:32 +0600

    fetched all newsFeed data and rendereing on the dom

--- client/src/components/Post/AllPosts.tsx
+++ client/src/components/Post/AllPosts.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 
+import Post, { PostUser } from 'interfaces/post'
 import SinglePost from 'components/Post/SinglePost'
+import { useGetNewsFeedPost } from 'hooks/useGetPost'
+
+interface EachPosts {
+	post: Post
+	user: PostUser
+}
 
 const PostsSection = () => {
-	return <div></div>
+	const { data, error } = useGetNewsFeedPost(0)
+
+	if (error) return <div>failed to load</div>
+	if (!data) return <div>loading...</div>
+
+	const {
+		getNewsFeedPost: { posts },
+	} = data
+
+	return (
+		<>
+			{posts.map(({ post }: EachPosts) => (
+				<SinglePost key={nanoid()} {...post} />
+			))}
+		</>
+	)
 }
 
 export default PostsSection
