903b9d383a278da2b8453b14b16d15b774af5f34
thatanjan <anjan.cules@gmail.com>
2021-03-21 21:08:29 +0600

    EachPosts interface  is removed and Post type is used

--- client/src/components/Post/AllPosts.tsx
+++ client/src/components/Post/AllPosts.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import { nanoid } from 'nanoid'
 
-import Post, { PostUser } from 'interfaces/post'
+import Post from 'interfaces/post'
 import SinglePost from 'components/Post/SinglePost'
 import { useGetNewsFeedPost } from 'hooks/useGetPost'
-
-interface EachPosts {
-	post: Post
-	user: PostUser
-}
 
 const PostsSection = () => {
 	const { data, error } = useGetNewsFeedPost(0)
@@ -22,7 +17,7 @@ const PostsSection = () => {
 
 	return (
 		<>
-			{posts.map(({ post }: EachPosts) => (
+			{posts.map((post: Post) => (
 				<SinglePost key={nanoid()} {...post} />
 			))}
 		</>
