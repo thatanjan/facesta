90d45d0b9ccd013cb9dfd5a2938652c378a75277
thatanjan <anjan.cules@gmail.com>
2021-03-24 09:04:11 +0600

    checking if user exist functionality is added to permission

--- server/src/config/permissions.js
+++ server/src/config/permissions.js
@@ -1,13 +1,20 @@
+import User from 'models/User'
 import { rule, shield } from 'graphql-shield'
 
-const isAuthenticated = rule()((_, __, { user, error }) => {
-	if (user) {
-		return true
+const isAuthenticated = rule()(async (_, __, { user, error }) => {
+	if (!user) {
+		return false
 	}
+
+	const doesUserExist = await User.findById(user.id, 'name')
+
+	if (!doesUserExist) return false
 
 	if (error) {
 		return new Error(error)
 	}
+
+	return true
 })
 
 export default shield(
