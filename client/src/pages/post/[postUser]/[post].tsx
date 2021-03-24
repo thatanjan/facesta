92b5f7d23c905c1d235383d7dacaffe320cc8b56
thatanjan <anjan.cules@gmail.com>
2021-03-24 16:50:23 +0600

    fetching a single post based on query parameter

--- client/src/pages/post/[postUser]/[post].tsx
+++ client/src/pages/post/[postUser]/[post].tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { GetServerSideProps } from 'next'
 
 import PageWrapper from 'components/Layout/PageWrapper'
@@ -11,10 +12,28 @@ import shouldRedirectToAuth from 'utils/shouldRedirectToAuth'
 import getToken from 'utils/getToken'
 import createRedirectObject from 'utils/createRedirectObject'
 import { LOGIN_URL, APP_NAME } from 'variables/global'
+import { useGetSinglePost } from 'hooks/useGetPost'
 
 interface Props extends PropsWithUserData {}
 
-const PageContent = () => null
+const PageContent = () => {
+	const {
+		query: { post, postUser },
+		asPath,
+	} = useRouter()
+
+	const { data, error } = useGetSinglePost({
+		user: postUser as string,
+		postID: post as string,
+	})
+
+	if (error) return <div>failed to load</div>
+	if (!data) return <div>loading...</div>
+
+	console.log(data)
+
+	return null
+}
 
 const Post = ({ userData }: Props) => {
 	return (
