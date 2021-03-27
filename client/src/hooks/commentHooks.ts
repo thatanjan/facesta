686b8ff9fe0ed49933862b974b97f1ffde99dd60
thatanjan <anjan.cules@gmail.com>
2021-03-27 18:30:39 +0600

    a hook for getting infinite comments

---
+++ client/src/hooks/commentHooks.ts
@@ -0,0 +1,27 @@
+import { getAllComments } from 'graphql/queries/postQueries'
+import createRequest from 'utils/createRequest'
+import { useSWRInfinite } from 'swr'
+import { useOwnUserId } from 'hooks/userhooks'
+
+interface Input {
+	postID: string
+	postUserID: string
+}
+
+// eslint-disable-next-line
+export const useGetAllComments = ({ postUserID, postID }: Input) => {
+	const userID = useOwnUserId()
+
+	const getKey = (index: number) => {
+		const skipnum: number = (index + 1) * 10
+
+		return [getAllComments, skipnum, userID]
+	}
+
+	return useSWRInfinite(
+		getKey,
+		async (key, num) =>
+			createRequest({ key, values: { skip: num, postID, user: postUserID } }),
+		{ revalidateOnFocus: false }
+	)
+}
