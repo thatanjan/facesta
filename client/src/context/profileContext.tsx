bb93b0a72c7c710891cce2a9a161592f356edf13
thatanjan <anjan.cules@gmail.com>
2021-03-28 13:33:49 +0600

    context will have the name and profile picture

--- client/src/context/profileContext.tsx
+++ client/src/context/profileContext.tsx
@@ -4,11 +4,15 @@ interface Props {
 	children: ReactNode
 	isSelf: boolean
 	profileUserID: string
+	name: string
+	profilePicture: string
 }
 
 export interface State {
 	profileUserID: string
 	isSelf: boolean
+	name: string
+	profilePicture: string
 }
 
 interface ContextData {
@@ -16,15 +20,25 @@ interface ContextData {
 	setState: Function
 }
 
-const initialState: State = { profileUserID: '', isSelf: false }
+const initialState: State = {
+	profileUserID: '',
+	isSelf: false,
+	name: '',
+	profilePicture: '',
+}
 
 export const ProfileContext = createContext({} as ContextData)
 
-const ProfileContextProvider = ({ children, isSelf, profileUserID }: Props) => {
+const ProfileContextProvider = ({
+	children,
+	isSelf,
+	profileUserID,
+	...others
+}: Props) => {
 	const [state, setState] = useState<State>(initialState)
 
 	useEffect(() => {
-		setState({ ...state, isSelf, profileUserID })
+		setState({ ...state, isSelf, profileUserID, ...others })
 	}, [isSelf, profileUserID])
 
 	return (
