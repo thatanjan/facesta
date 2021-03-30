22ffa79742d24a2638929f7da2ff17f5023751d5
thatanjan <anjan.cules@gmail.com>
2021-03-30 18:35:34 +0600

    after loading all data hasMore will be false

--- client/src/components/Post/AllPosts.tsx
+++ client/src/components/Post/AllPosts.tsx
@@ -1,54 +1,29 @@
-import React, { useState, UIEvent } from 'react'
+import React from 'react'
 import { nanoid } from 'nanoid'
-import Box from '@material-ui/core/Box'
-import { makeStyles } from '@material-ui/core/styles'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 import Post from 'interfaces/post'
 import SinglePost from 'components/Post/SinglePost'
 import { useGetNewsFeedPost } from 'hooks/useGetPost'
 
-const useStyles = makeStyles({
-	postContainerStyle: {
-		maxHeight: '100vh',
-		overflowY: 'scroll',
-		'-ms-overflow-style': 'none',
-		scrollbarWidth: 'none',
-
-		'& ::-webkit-scrollbar': {
-			display: 'none',
-		},
-	},
-})
-
 const PostsSection = () => {
-	const { postContainerStyle } = useStyles()
+	const { data, error, setSize, size } = useGetNewsFeedPost()
 
-	const { data, error, setSize, size, isValidating } = useGetNewsFeedPost()
-
-	const isLoadingInitialData = !data && !error
-	const isLoadingMore =
-		isLoadingInitialData ||
-		(size > 0 && data && typeof data[size - 1] === 'undefined')
-
-	const handleScroll = (event: any) => {
-		const {
-			target: { scrollHeight, clientHeight, scrollTop },
-		} = event
-
-		if (scrollHeight - scrollTop === clientHeight) {
-			setSize(size + 1)
-		}
-	}
+	let isLoadingMore = true
 
 	let allPost: Post[] = []
 
 	if (data) {
+		if (data[size - 1]) {
+			if (data[size - 1].getNewsFeedPost?.posts.length === 0) {
+				isLoadingMore = false
+			}
+		}
+
 		data.forEach(element => {
 			allPost = [...allPost, ...element.getNewsFeedPost.posts]
 		})
 	}
-	/* hasMore={ data?[data?.length- 1].getNewsFeedPost.posts.length === 0 ? false:   true} */
 
 	return (
 		<>
@@ -64,7 +39,7 @@ const PostsSection = () => {
 			<InfiniteScroll
 				dataLength={allPost.length}
 				next={() => setSize(size + 1)}
-				hasMore
+				hasMore={isLoadingMore as boolean}
 				loader={<h4>Loading...</h4>}
 			>
 				{allPost.map((post: Post) => (
@@ -73,8 +48,6 @@ const PostsSection = () => {
 			</InfiniteScroll>
 
 			{error && 'error happened'}
-
-			{/* {isLoadingMore && <div>loading</div>} */}
 		</>
 	)
 }
