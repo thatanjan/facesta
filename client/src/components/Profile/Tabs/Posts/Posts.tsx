1d930aa4fa938d41588ba057b42af5820582c6e1
thatanjan <anjan.cules@gmail.com>
2021-03-31 13:49:59 +0600

    infinite scroll is added to the all post of a profile

--- client/src/components/Profile/Tabs/Posts/Posts.tsx
+++ client/src/components/Profile/Tabs/Posts/Posts.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
-import PostType from 'interfaces/post'
+import Post from 'interfaces/post'
 import SinglePost from 'components/Post/SinglePost'
 import useGetAllPosts from 'hooks/useGetPost'
 import { useProfileInfo } from 'hooks/useGetProfileData'
-import { nanoid } from 'nanoid'
 
 const Posts = () => {
-	const { data, error } = useGetAllPosts()
+	const { data, error, size, setSize } = useGetAllPosts()
 	const { data: profileData, error: profileError } = useProfileInfo()
 	console.log(data)
 
@@ -16,9 +17,16 @@ const Posts = () => {
 
 	const { getUser: user } = profileData
 
-	let allPost: PostType[] = []
+	let isLoadingMore = true
+	let allPost: Post[] = []
 
 	if (data) {
+		if (data[size - 1]) {
+			if (data[size - 1].getNewsFeedPost?.posts.length === 0) {
+				isLoadingMore = false
+			}
+		}
+
 		data.forEach(element => {
 			allPost = [...allPost, ...element.getAllPost.posts]
 		})
@@ -26,10 +34,18 @@ const Posts = () => {
 
 	return (
 		<div>
-			{Array.isArray(allPost) &&
-				allPost.map((post: PostType) => (
+			<InfiniteScroll
+				dataLength={allPost.length}
+				next={() => setSize(size + 1)}
+				hasMore={isLoadingMore as boolean}
+				loader={<h4>Loading...</h4>}
+			>
+				{allPost.map((post: Post) => (
 					<SinglePost key={nanoid()} {...post} postPage={false} user={user} />
 				))}
+			</InfiniteScroll>
+
+			{error && 'error happened'}
 		</div>
 	)
 }
