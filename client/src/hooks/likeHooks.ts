321f6eca094ffbc6a7d79d3477ebd55e0d3708b6
thatanjan <anjan.cules@gmail.com>
2021-04-01 12:11:18 +0600

    a hook created for getting all user who liked the post

--- client/src/hooks/likeHooks.ts
+++ client/src/hooks/likeHooks.ts
@@ -1,16 +1,34 @@
-import { hasLiked } from 'graphql/queries/postQueries'
+import { useSWRInfinite } from 'swr'
+
+import createRequest from 'utils/createRequest'
+import { hasLiked, getAllLikes } from 'graphql/queries/postQueries'
 import useSWRgql from './useSWRgql'
 
-interface GetPost {
+interface Input {
 	postID: string
 	user: string
 }
 
-// eslint-disable-next-line
-export const useHasLiked = ({ postID, user }: GetPost) => {
+export const useHasLiked = ({ postID, user }: Input) => {
 	return useSWRgql({
 		key: hasLiked,
 		swrDependencies: postID,
 		values: { postID, user },
 	})
+}
+
+export const useGetAllLikes = ({ user, postID }: Input) => {
+	const getKey = (index: number) => {
+		const skipnum: number = (index + 1) * 10
+
+		return [getAllLikes, skipnum, user, postID]
+	}
+
+	return useSWRInfinite(
+		getKey,
+		// eslint-disable-next-line
+		async (key, num, user, postID) =>
+			createRequest({ key, values: { skip: num, postID, user } }),
+		{ revalidateOnFocus: false }
+	)
 }
