74a08eb418bfd2f7eec08e32d6e4179e2df5f81d
thatanjan <anjan.cules@gmail.com>
2021-04-01 09:18:30 +0600

    a modal for showing list of users

---
+++ client/src/components/Modals/UserListModal.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Dialog, { DialogProps } from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
+interface Props {
+	showUsers: boolean
+	setShowUsers: (e: boolean) => void
+	title: string
+}
+
+const UserListModal = ({ setShowUsers, showUsers, title }: Props) => {
+	const [scroll] = React.useState<DialogProps['scroll']>('paper')
+
+	const handleClose = () => {
+		setShowUsers(false)
+	}
+
+	const descriptionElementRef = React.useRef<HTMLElement>(null)
+	React.useEffect(() => {
+		if (showUsers) {
+			const { current: descriptionElement } = descriptionElementRef
+			if (descriptionElement !== null) {
+				descriptionElement.focus()
+			}
+		}
+	}, [showUsers])
+
+	return (
+		<div>
+			<Dialog
+				open={showUsers}
+				onClose={handleClose}
+				scroll={scroll}
+				aria-labelledby='scroll-dialog-title'
+				aria-describedby='scroll-dialog-description'
+			>
+				<DialogTitle id='scroll-dialog-title'>{title}</DialogTitle>
+				<DialogContent dividers>Users</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color='primary'>
+						close
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+	)
+}
+
+export default UserListModal
