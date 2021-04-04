c2055afcc02cc545a76b99a56fc36fab634ec8bd
thatanjan <anjan.cules@gmail.com>
2021-04-04 08:27:35 +0600

    two hooks for getting followers and followees has been created for infinite scrolling

--- client/src/hooks/followHooks.ts
+++ client/src/hooks/followHooks.ts
@@ -4,25 +4,47 @@ import {
 	getIsFollower,
 	getIsFollowee,
 } from 'graphql/queries/followQueries'
+import { useSWRInfinite } from 'swr'
+
+import createRequest from 'utils/createRequest'
+import { AnyObject } from 'interfaces/global'
 import uswSWRgql from './useSWRgql'
 import { useProfileUserID } from './profileContextHooks'
 
-export const useGetFollowers = (skip: number) => {
+export const useGetFollowers = () => {
 	const user = useProfileUserID()
-	return uswSWRgql({
-		key: getFollowers,
-		swrDependencies: user,
-		values: { user, skip },
-	})
+
+	const getKey = (index: number, previousPageData: AnyObject) => {
+		if (previousPageData && previousPageData.getFollowers.followers.length === 0)
+			return null
+		const skipnum: number = (index + 1) * 10
+
+		return [getFollowers, skipnum, user]
+	}
+
+	return useSWRInfinite(
+		getKey,
+		async (key, num) => createRequest({ key, values: { skip: num, user } }),
+		{ revalidateOnFocus: false }
+	)
 }
 
-export const useGetFollowees = (skip: number) => {
+export const useGetFollowees = () => {
 	const user = useProfileUserID()
-	return uswSWRgql({
-		key: getFollowees,
-		swrDependencies: user,
-		values: { user, skip },
-	})
+
+	const getKey = (index: number, previousPageData: AnyObject) => {
+		if (previousPageData && previousPageData.getFollowees?.followees.length === 0)
+			return null
+		const skipnum: number = (index + 1) * 10
+
+		return [getFollowees, skipnum, user]
+	}
+
+	return useSWRInfinite(
+		getKey,
+		async (key, num) => createRequest({ key, values: { skip: num, user } }),
+		{ revalidateOnFocus: false }
+	)
 }
 
 export const useIsFollowee = () => {
