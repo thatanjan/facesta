bb516f63fce13714ae03678d39dc4d778338ea05
thatanjan <anjan.cules@gmail.com>
2021-04-04 08:30:14 +0600

    getting infinite number of users functionality has been done

--- client/src/components/Profile/Tabs/Follow/Follow.tsx
+++ client/src/components/Profile/Tabs/Follow/Follow.tsx
@@ -6,10 +6,13 @@ import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import { nanoid } from 'nanoid'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
+import { PostUser as User } from 'interfaces/post'
 import { useProfileUserID } from 'hooks/profileContextHooks'
 import MuiLink from 'components/Links/MuiLink'
 import { FOLLOWEES, FOLLOWERS } from 'variables/global'
+import { cloudinaryURL } from 'variables/global'
 
 const useStyles = makeStyles((theme: Theme) => ({
 	root: {
@@ -29,15 +32,9 @@ interface Props {
 	hook: Function
 }
 
-interface Users {
-	name: string
-	// eslint-disable-next-line react/no-unused-prop-types
-	id: string
-}
-
 export const FollowComponent = ({ name, hook }: Props) => {
 	const profileUserId = useProfileUserID()
-	const { data, error } = hook(10)
+	const { data, error, setSize, size } = hook()
 	const { root } = useStyles()
 
 	if (error) {
@@ -50,20 +47,43 @@ export const FollowComponent = ({ name, hook }: Props) => {
 
 	console.log(data)
 
-	let users: Users[]
+	let isLoadingMore = true
 
-	switch (name) {
-		case FOLLOWEES:
-			users = data.getFollowees.followees
+	let users: User[] = []
 
-			break
+	if (data) {
+		switch (name) {
+			case FOLLOWEES:
+				if (data[size - 1]) {
+					if (data[size - 1].getFollowees?.followees.length === 0) {
+						isLoadingMore = false
+					}
+				}
 
-		case FOLLOWERS:
-			users = data.getFollowers.followers
-			break
+				data.forEach(element => {
+					users = [...users, ...element.getFollowees.followees]
+				})
 
-		default:
-			users = []
+				users = data.getFollowees?.followees
+				break
+
+			case FOLLOWERS:
+				if (data[size - 1]) {
+					if (data[size - 1].getFollowers?.followers.length === 0) {
+						isLoadingMore = false
+					}
+				}
+
+				data.forEach(element => {
+					users = [...users, ...element.getFollowers.followers]
+				})
+
+				users = data.getFollowers?.followers
+				break
+
+			default:
+				users = []
+		}
 	}
 
 	const avatar =
@@ -72,20 +92,21 @@ export const FollowComponent = ({ name, hook }: Props) => {
 	return (
 		<>
 			<List className={root}>
-				{users.map(({ name: userName, id }: Users) => (
-					<MuiLink
-						MuiComponent={ListItem}
-						button
-						key={nanoid()}
-						href={`/profile/${id}`}
-					>
-						<ListItemAvatar>
-							<Avatar src={avatar} />
-						</ListItemAvatar>
+				{Array.isArray(users) &&
+					users.map(({ name: userName, _id, profile: { profilePicture } }: User) => (
+						<MuiLink
+							MuiComponent={ListItem}
+							button
+							key={nanoid()}
+							href={`/profile/${_id}`}
+						>
+							<ListItemAvatar>
+								<Avatar alt={userName} src={cloudinaryURL(profilePicture)} />
+							</ListItemAvatar>
 
-						<ListItemText primary={userName} />
-					</MuiLink>
-				))}
+							<ListItemText primary={userName} />
+						</MuiLink>
+					))}
 			</List>
 		</>
 	)
