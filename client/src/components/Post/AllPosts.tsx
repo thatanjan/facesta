07d0cf64f5f9cb065addc7ff60f4a52a31878f5d
thatanjan <anjan.cules@gmail.com>
2021-04-06 13:16:38 +0600

    if any error happens during fetching or assigning to the variable, then an alert component will pop up

--- client/src/components/Post/AllPosts.tsx
+++ client/src/components/Post/AllPosts.tsx
@@ -1,29 +1,47 @@
 import React from 'react'
 import { nanoid } from 'nanoid'
+
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 import Post from 'interfaces/post'
 import SinglePost from 'components/Post/SinglePost'
 import { useGetNewsFeedPost } from 'hooks/useGetPost'
+import Alert from 'components/Alerts/Alert'
 
 const PostsSection = () => {
 	const { data, error, setSize, size } = useGetNewsFeedPost()
+
+	if (!data)
+		return (
+			<Alert
+				checked
+				severity='info'
+				message='Please wait we are loading your news feed'
+			/>
+		)
 
 	let isLoadingMore = true
 
 	let allPost: Post[] = []
 
-	if (data) {
-		if (data[size - 1]) {
-			if (data[size - 1].getNewsFeedPost?.posts.length === 0) {
-				isLoadingMore = false
+	try {
+		if (data) {
+			if (data[size - 1]) {
+				if (data[size - 1].getNewsFeedPost?.posts.length === 0) {
+					isLoadingMore = false
+				}
 			}
-		}
 
-		data.forEach(element => {
-			allPost = [...allPost, ...element.getNewsFeedPost.posts]
-		})
+			data.forEach(element => {
+				allPost = [...allPost, ...element.getNewsFeedPost.posts]
+			})
+		}
+	} catch (e) {
+		return <Alert checked severity='error' message='Please try again' />
 	}
+
+	if (allPost.length === 0)
+		return <Alert checked severity='info' message=' Sorry you have no Post' />
 
 	return (
 		<>
@@ -33,12 +51,13 @@ const PostsSection = () => {
 				hasMore={isLoadingMore as boolean}
 				loader={<h4>Loading...</h4>}
 			>
-				{allPost.map((post: Post) => (
-					<SinglePost key={nanoid()} {...post} postPage={false} />
-				))}
+				{Array.isArray(allPost) &&
+					allPost.map((post: Post) => (
+						<SinglePost key={nanoid()} {...post} postPage={false} />
+					))}
 			</InfiniteScroll>
 
-			{error && 'error happened'}
+			{error && <Alert checked severity='error' message='Please try again' />}
 		</>
 	)
 }
