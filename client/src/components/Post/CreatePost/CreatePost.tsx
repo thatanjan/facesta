9cf069229dd64c48f54684b177da97c4a4dce0c9
thatanjan <anjan.cules@gmail.com>
2021-04-09 16:57:10 +0600

    CircularLoader import error is fixed

--- client/src/components/Post/CreatePost/CreatePost.tsx
+++ client/src/components/Post/CreatePost/CreatePost.tsx
@@ -8,6 +8,8 @@ import IconButton from '@material-ui/core/IconButton'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 import Input from '@material-ui/core/Input'
 
+import CircularLoader from 'components/Loaders/CircularLoader'
+
 const CreatePostModal = dynamic(() => import('./CreatePostModal'), {
 	loading: () => <CircularLoader />,
 })
