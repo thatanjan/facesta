f148a7350dcbaf9aaac86465f9b1d28cc7568a81
thatanjan <anjan.cules@gmail.com>
2021-04-11 09:41:06 +0600

    data added to the right Navigation. users will be fetched from useGetRecommendedToFollow hook. if the loggedin user present in the data, then it will be exclued

--- client/src/components/Navigation/RightNavigation/RightNavigation.tsx
+++ client/src/components/Navigation/RightNavigation/RightNavigation.tsx
@@ -1,9 +1,52 @@
+import produce from 'immer'
 import React from 'react'
+import dynamic from 'next/dynamic'
+import { responseInterface } from 'swr'
 
-interface Props {}
+import CircularLoader from 'components/Loaders/CircularLoader'
+import { useGetRecommendedToFollow } from 'hooks/followHooks'
+import { useOwnUserId } from 'hooks/userhooks'
+import { PostUser as User } from 'interfaces/post'
 
-const RightNavigation = (props: Props) => {
-	return <div>this is right navigation</div>
+const ListContainer = dynamic(
+	() => import('components/List/UserListContainer'),
+	{ loading: () => <CircularLoader /> }
+)
+
+const UserList = dynamic(() => import('components/List/UserList'), {
+	loading: () => <CircularLoader />,
+})
+
+const SwrErrorAlert = dynamic(() => import('components/Alerts/SwrErrorAlert'))
+
+const RightNavigation = () => {
+	const {
+		data,
+		error,
+	}: responseInterface<
+		{ getRecommendedToFollow: { users: User[] } },
+		any
+	> = useGetRecommendedToFollow()
+
+	const ownUserID = useOwnUserId()
+
+	if (!data) return <CircularLoader />
+	if (error) return <SwrErrorAlert />
+
+	const {
+		getRecommendedToFollow: { users },
+	} = data
+
+	const newUsers = produce(users, draft => {
+		const index = draft.findIndex(user => user._id === ownUserID)
+		if (index !== -1) draft.splice(index, 1)
+	})
+
+	return (
+		<ListContainer>
+			<UserList users={newUsers} />
+		</ListContainer>
+	)
 }
 
 export default RightNavigation
