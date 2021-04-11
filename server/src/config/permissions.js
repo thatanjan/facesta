9a5e3b17b0c880a652ef7923638c0fc5f63c9da8
thatanjan <anjan.cules@gmail.com>
2021-04-11 21:10:56 +0600

    new rule for checking if post exist and also added to needed queries and mutations

--- server/src/config/permissions.js
+++ server/src/config/permissions.js
@@ -1,15 +1,16 @@
 import { and, rule, shield } from 'graphql-shield'
 
-import { USER_DOES_NOT_EXIST } from 'variables/errors'
+import { USER_DOES_NOT_EXIST, POST_DOES_NOT_EXIST } from 'variables/errors'
 import User from 'models/User'
+import createPostModel from 'models/Post'
 
 const isAuthenticated = rule()(async (_, __, { user, error }) => {
-	if (!user) {
-		return false
-	}
-
 	if (error) {
 		return new Error(error)
+	}
+
+	if (!user) {
+		return false
 	}
 
 	return true
@@ -23,16 +24,26 @@ const doesUserExist = rule()(async (_, __, { user }) => {
 	return true
 })
 
+const doesPostExist = rule()(async (_, { Input: { postID, user } }) => {
+	const postModel = createPostModel(user)
+
+	const post = await postModel.findById(postID)
+
+	if (!post) return new Error(POST_DOES_NOT_EXIST)
+
+	return true
+})
+
 export default shield(
 	{
 		Mutation: {
 			createPost: and(isAuthenticated, doesUserExist),
 			deletePost: and(isAuthenticated, doesUserExist),
-			likePost: and(isAuthenticated, doesUserExist),
-			removeLikePost: and(isAuthenticated, doesUserExist),
-			commentPost: and(isAuthenticated, doesUserExist),
-			removeCommentPost: and(isAuthenticated, doesUserExist),
-			editPost: and(isAuthenticated, doesUserExist),
+			likePost: and(isAuthenticated, doesUserExist, doesPostExist),
+			removeLikePost: and(isAuthenticated, doesUserExist, doesPostExist),
+			commentPost: and(isAuthenticated, doesUserExist, doesPostExist),
+			removeCommentPost: and(isAuthenticated, doesUserExist, doesPostExist),
+			editPost: and(isAuthenticated, doesUserExist, doesPostExist),
 			updatePersonalData: and(isAuthenticated, doesUserExist),
 			uploadProfilePicture: and(isAuthenticated, doesUserExist),
 			removeProfilePicture: and(isAuthenticated, doesUserExist),
@@ -40,14 +51,14 @@ export default shield(
 			unfollowUser: and(isAuthenticated, doesUserExist),
 		},
 		Query: {
-			getSinglePost: and(isAuthenticated, doesUserExist),
+			getSinglePost: and(isAuthenticated, doesUserExist, doesPostExist),
 			getAllPost: and(isAuthenticated, doesUserExist),
 			getNewsFeedPost: and(isAuthenticated, doesUserExist),
-			getTotalLikes: and(isAuthenticated, doesUserExist),
-			getTotalComments: and(isAuthenticated, doesUserExist),
-			getAllComments: and(isAuthenticated, doesUserExist),
-			getAllLikes: and(isAuthenticated, doesUserExist),
-			hasLiked: and(isAuthenticated, doesUserExist),
+			getTotalLikes: and(isAuthenticated, doesUserExist, doesPostExist),
+			getTotalComments: and(isAuthenticated, doesUserExist, doesPostExist),
+			getAllComments: and(isAuthenticated, doesUserExist, doesPostExist),
+			getAllLikes: and(isAuthenticated, doesUserExist, doesPostExist),
+			hasLiked: and(isAuthenticated, doesUserExist, doesPostExist),
 			getUser: and(isAuthenticated, doesUserExist),
 			getPersonalData: and(isAuthenticated, doesUserExist),
 			getProfilePicture: and(isAuthenticated, doesUserExist),
