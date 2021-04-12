529a6fe1c2531aec1938c535a8c3046a307ffa1d
thatanjan <anjan.cules@gmail.com>
2021-04-12 22:42:04 +0600

    theme colors has changed

--- client/src/themes/theme.tsx
+++ client/src/themes/theme.tsx
@@ -4,9 +4,23 @@ const darkTheme = responsiveFontSizes(
 	createMuiTheme({
 		palette: {
 			type: 'dark',
+			primary: {
+				light: '#df6843',
+				main: '#d84315',
+				dark: '#972e0e',
+			},
+			secondary: {
+				light: '#015f92',
+				main: '#0288d1',
+				dark: '#349fda',
+			},
+			background: {
+				paper: '#171717',
+			},
 		},
 	})
 )
+
 export const lightTheme = createMuiTheme({
 	palette: {
 		type: 'light',
