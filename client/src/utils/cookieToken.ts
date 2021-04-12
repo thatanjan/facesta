1c46d003d89e0eec44b49d6f95bab21ae0828df7
thatanjan <anjan.cules@gmail.com>
2021-04-12 20:55:02 +0600

    new token will expire in 3days

--- client/src/utils/cookieToken.ts
+++ client/src/utils/cookieToken.ts
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie'
 import { TOKEN_NAME } from 'variables/global'
 
 export const setToken = (token: string) => {
-	Cookies.set(TOKEN_NAME, token, { path: '/' })
+	Cookies.set(TOKEN_NAME, token, { path: '/', expires: 3 })
 	return true
 }
 
