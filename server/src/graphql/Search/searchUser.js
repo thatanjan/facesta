5a773b8b44529f9077f93246af281ea7c2529006
thatanjan <anjan.cules@gmail.com>
2021-04-12 18:00:32 +0600

    searchUser query input  has been changed from text to query

--- server/src/graphql/Search/searchUser.js
+++ server/src/graphql/Search/searchUser.js
@@ -3,12 +3,12 @@ import sendErrorMessage from 'utils/errorMessage'
 
 const resolvers = {
 	Query: {
-		searchUser: async (_, { text }) => {
+		searchUser: async (_, { query }) => {
 			try {
 				const result = await Profile.find(
 					{
 						$text: {
-							$search: text,
+							$search: query,
 						},
 					},
 					{ name: 1, user: 1, profilePicture: 1 }
