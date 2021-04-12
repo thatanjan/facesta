63782f451c13161898f464ccd9d80a258b47262a
thatanjan <anjan.cules@gmail.com>
2021-04-12 23:34:59 +0600

    followers and followees tab's infinite scroll is now refactored

--- client/src/components/Profile/Tabs/Follow/Followers.tsx
+++ client/src/components/Profile/Tabs/Follow/Followers.tsx
@@ -8,11 +8,13 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import ReactInfiniteScroll from 'react-infinite-scroll-component'
 import { nanoid } from 'nanoid'
+import { SWRInfiniteResponseInterface } from 'swr'
 
-import {  User } from 'interfaces/user'
+import { User } from 'interfaces/user'
 import MuiLink from 'components/Links/MuiLink'
-import { cloudinaryURL, FOLLOWEES, FOLLOWERS } from 'variables/global'
+import { cloudinaryURL } from 'variables/global'
 
+import Alert from 'components/Alerts/Alert'
 import CircularLoader from 'components/Loaders/CircularLoader'
 
 const SwrErrorAlert = dynamic(() => import('components/Alerts/SwrErrorAlert'), {
@@ -36,37 +38,39 @@ const useStyles = makeStyles((theme: Theme) => ({
 }))
 
 interface Props {
-	name: string
 	hook: Function
 	hasSeenBefore: boolean
 	setHasSeenBefore: (val: boolean) => void
 }
 
-export const FollowComponent = ({
-	name,
-	hook,
-	hasSeenBefore,
-	setHasSeenBefore,
-}: Props) => {
-	const { data, error, size, setSize, mutate } = hook()
-	console.log(error)
+const QUERY_NAME = 'getFollowers'
+
+const FollowComponent = ({ hook, hasSeenBefore, setHasSeenBefore }: Props) => {
+	const {
+		data,
+		error,
+		size,
+		setSize,
+		mutate,
+	}: SWRInfiniteResponseInterface<
+		{
+			getFollowers: {
+				followers: User[]
+				errorMessage: string | null
+			}
+		},
+		any
+	> = hook()
+
 	const { root } = useStyles()
 
 	useEffect(() => {
 		if (!hasSeenBefore) {
-			console.log('first ran')
 			setHasSeenBefore(true)
 		}
 
 		if (hasSeenBefore) {
-			console.log('ran')
 			mutate()
-		}
-	}, [])
-
-	useEffect(() => {
-		return () => {
-			console.log(name)
 		}
 	}, [])
 
@@ -78,16 +82,36 @@ export const FollowComponent = ({
 
 	let users: User[] = []
 
-	if (data) {
-		if (data[size - 1]) {
-			if (data[size - 1].getFollowers?.followers.length === 0) {
+	let errorFromServer = false
+
+	try {
+		const lastResponse = data[size - 1]
+
+		if (lastResponse && lastResponse[QUERY_NAME]) {
+			const { errorMessage } = lastResponse[QUERY_NAME]
+
+			if (errorMessage) {
+				errorFromServer = true
+			}
+
+			const { followers } = lastResponse[QUERY_NAME]
+
+			if (
+				Array.isArray(followers) &&
+				(followers.length === 0 || followers.length < 10)
+			) {
 				isLoadingMore = false
 			}
 		}
 
-		data.forEach((element: { getFollowers: { followers: User[] } }) => {
-			users = [...users, ...element.getFollowers.followers]
+		data.forEach(element => {
+			users = [...element.getFollowers.followers]
 		})
+
+		if (users.length === 0)
+			return <Alert checked severity='info' message='You have no followers' />
+	} catch (_) {
+		return <Alert checked severity='error' message='Please try again' />
 	}
 
 	return (
@@ -98,7 +122,7 @@ export const FollowComponent = ({
 				dataLength={users.length}
 				next={() => setSize(size + 1)}
 				hasMore={isLoadingMore as boolean}
-				loader={<h4>Loading...</h4>}
+				loader={<CircularLoader />}
 			>
 				{Array.isArray(users) &&
 					users.map(({ _id, profile: { name: userName, profilePicture } }: User) => (
@@ -119,6 +143,12 @@ export const FollowComponent = ({
 						</MuiLink>
 					))}
 			</List>
+
+			{error && <Alert checked severity='error' message='Please try again' />}
+
+			{!isLoadingMore && (
+				<Alert checked severity='info' message='No more followers to show' />
+			)}
 		</>
 	)
 }
