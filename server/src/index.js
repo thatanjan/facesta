15fcdb4dee74625fd17138a3c430b40505c5435e
thatanjan <anjan.cules@gmail.com>
2021-04-13 21:20:22 +0600

    cloudinary api keys name have changed

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,9 @@ const cloudinary = require('cloudinary').v2
 dotenv.config()
 
 cloudinary.config({
-	cloud_name: process.env.CLOUD_NAME,
-	api_key: process.env.API_KEY,
-	api_secret: process.env.API_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 const app = express()
 
