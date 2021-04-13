58c5668633ce5fe9d1ed1653c6f03e30df5c8cdc
thatanjan <anjan.cules@gmail.com>
2021-04-13 16:43:58 +0600

    mui links will have curor style

--- client/src/components/Links/MuiLink.tsx
+++ client/src/components/Links/MuiLink.tsx
@@ -11,6 +11,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 	linkStyle: {
 		color: 'inherit',
 		textDecoration: 'none',
+		cursor: 'pointer',
 
 		'&:hover': {
 			textDecoration: 'initial',
