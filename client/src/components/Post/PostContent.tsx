7344ae562c524f804aa7f938de310f4d021fd8be
thatanjan <anjan.cules@gmail.com>
2021-04-13 16:24:11 +0600

    post content is now ready. it will show the needed content based on if the location is post page or not

--- client/src/components/Post/PostContent.tsx
+++ client/src/components/Post/PostContent.tsx
@@ -1,33 +1,64 @@
 import React from 'react'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import { nanoid } from 'nanoid'
 
 import MuiLink from 'components/Links/MuiLink'
 
 interface Props {
 	text: string
-	postPage: boolean
 	showMoreLink: string
+	postPage: boolean
 }
 
-const PostContent = ({ showMoreLink, text, postPage }: Props) => {
-	const visibleTextLength = 500
+const PostContentForPage = ({ text }: { text: string }) => {
+	const paragraphs = text.split('\n')
+	return (
+		<CardContent>
+			{paragraphs.map(paragraph => (
+				<Typography key={nanoid()} variant='body1' component='p'>
+					{paragraph}
+				</Typography>
+			))}
+		</CardContent>
+	)
+}
+
+const PostContent = ({ postPage, showMoreLink, text }: Props) => {
+	const idealLength = 500
+
+	let visibleText = text.substr(0, idealLength)
+
+	const visibleTextLength = visibleText.length
+
+	const lastTwoChar = visibleText.endsWith('\n')
+
+	if (lastTwoChar) {
+		visibleText = visibleText.substring(0, visibleTextLength - 2)
+	}
+
+	const paragraphs = visibleText.split('\n')
 
 	return (
 		<CardContent>
-			<Typography variant='body2' color='textSecondary' component='p'>
-				{text.length > visibleTextLength && !postPage ? (
-					<>
-						{text.substr(0, visibleTextLength)}
-						<span>...</span>
-						<MuiLink href={showMoreLink} MuiComponent={Typography} variant='button'>
-							show more
-						</MuiLink>
-					</>
-				) : (
-					text
-				)}
-			</Typography>
+			{!postPage && (
+				<>
+					<Typography variant='body1' color='textSecondary' component='span'>
+						{paragraphs[0]}
+
+						{paragraphs.length >= 1 || visibleText.length > idealLength - 2 ? (
+							<>
+								<span> ...</span>{' '}
+								<MuiLink href={showMoreLink} MuiComponent={Typography} variant='button'>
+									show more
+								</MuiLink>
+							</>
+						) : null}
+					</Typography>
+				</>
+			)}
+
+			{postPage && <PostContentForPage text={text} />}
 		</CardContent>
 	)
 }
