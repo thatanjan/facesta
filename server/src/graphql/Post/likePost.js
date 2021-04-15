446da08bb7e3f6fe2bac5d10447bbd642d6a263d
thatanjan <anjan.cules@gmail.com>
2021-04-15 20:45:29 +0600

    like and remove like mutation is now refactored and seperated to its own function

--- server/src/graphql/Post/likePost.js
+++ server/src/graphql/Post/likePost.js
@@ -11,75 +11,38 @@ const hasLiked = async ({ postID, postOwnerID, myID }) => {
 	return PostModel.findOne({ _id: postID, likes: { $in: myID } }, 'likes')
 }
 
-const modifyLikes = async ({ postID, postOwnerID, myID }, operation) => {
-	const PostModel = createPostModel(postOwnerID.toString())
-
-	switch (operation) {
-		case LIKE:
-			const res = await PostModel.updateOne(
-				{ _id: postID },
-				{ $push: { likes: myID }, $inc: { totalLikes: 1 } }
-			)
-
-			if (res || res.nModified > 0) return true
-
-			return false
-
-		case REMOVE_LIKE:
-			const res2 = await PostModel.updateOne(
-				{ _id: postID },
-				{ $pull: { likes: myID }, $inc: { totalLikes: -1 } }
-			)
-
-			if (res2 || res2.nModified > 0) return true
-			return false
-
-		default:
-			return false
-	}
-}
-
-const responseMessage = operation => {
-	switch (operation) {
-		case LIKE:
-			return sendMessage('you have liked this post')
-
-		case REMOVE_LIKE:
-			return sendMessage('you have removed like from the post')
-
-		default:
-			return false
-	}
-}
-
-const mainFunction = operation => {
-	return async (_, { Input: { postID, user } }, { user: { id } }) => {
-		try {
-			const params = { postID, postOwnerID: user, myID: id }
-
-			if (operation === LIKE && (await hasLiked(params))) {
-				return sendErrorMessage('you have already liked this post')
-			}
-
-			if (operation === REMOVE_LIKE && !(await hasLiked(params))) {
-				return sendErrorMessage('you have not liked this post.')
-			}
-
-			const hasModified = await modifyLikes(params, operation)
-
-			if (!hasModified) return sendErrorMessage('something went wrong')
-
-			return responseMessage(operation)
-		} catch (err) {
-			return sendErrorMessage(err)
-		}
-	}
-}
-
 const resolver = {
 	Mutation: {
-		likePost: mainFunction(LIKE),
-		removeLikePost: mainFunction(REMOVE_LIKE),
+		likePost: async (_, { Input: { postID, user } }, { user: { id } }) => {
+			try {
+				const PostModel = createPostModel(user.toString())
+				const res = await PostModel.updateOne(
+					{ _id: postID },
+					{ $push: { likes: id }, $inc: { totalLikes: 1 } }
+				)
+
+				if (res || res.nModified > 0) return sendMessage('you have liked this post')
+
+				return sendErrorMessage('something went wrong')
+			} catch (__) {
+				return sendErrorMessage('something went wrong')
+			}
+		},
+		removeLikePost: async (_, { Input: { postID, user } }, { user: { id } }) => {
+			try {
+				const PostModel = createPostModel(user.toString())
+
+				const res = await PostModel.updateOne(
+					{ _id: postID },
+					{ $pull: { likes: id }, $inc: { totalLikes: -1 } }
+				)
+
+				if (res || res.nModified > 0)
+					return sendMessage('you have removed like from the post')
+			} catch (___) {
+				return sendErrorMessage('something went wrong')
+			}
+		},
 	},
 	Query: {
 		hasLiked: async (_, { Input: { postID, user } }, { user: { id } }) => {
