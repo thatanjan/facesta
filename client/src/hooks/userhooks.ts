2b19defe321ca4e882874a6cd781c74652e24c75
thatanjan <anjan.cules@gmail.com>
2021-04-20 12:23:09 +0600

    unused hook useHaveSeenFeedOnce is now deleted

--- client/src/hooks/userhooks.ts
+++ client/src/hooks/userhooks.ts
@@ -1,11 +1,3 @@
-import { useContext } from 'react'
-import { UserContext } from 'context/UserContext'
 import { useAppSelector } from 'redux/hooks/hooks'
 
 export const useOwnUserId = () => useAppSelector(state => state.user.id)
-
-export const useHaveSeenFeedOnce = () => {
-	const { haveSeenFeedOnce, setHaveSeenFeedOnce } = useContext(UserContext)
-
-	return { haveSeenFeedOnce, setHaveSeenFeedOnce }
-}
