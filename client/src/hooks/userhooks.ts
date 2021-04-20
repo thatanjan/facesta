074dae331f71a840af9e38b168151699df92ec6e
thatanjan <anjan.cules@gmail.com>
2021-04-20 11:33:01 +0600

    unUsed hook 'useSetUser' is deleted

--- client/src/hooks/userhooks.ts
+++ client/src/hooks/userhooks.ts
@@ -4,12 +4,6 @@ import { useAppSelector } from 'redux/hooks/hooks'
 
 export const useOwnUserId = () => useAppSelector(state => state.user.id)
 
-export const useSetUser = () => {
-	const { setUserID } = useContext(UserContext)
-
-	return setUserID
-}
-
 export const useHaveSeenFeedOnce = () => {
 	const { haveSeenFeedOnce, setHaveSeenFeedOnce } = useContext(UserContext)
 
