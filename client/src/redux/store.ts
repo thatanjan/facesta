72168a79d0b782dfa102c588cfe1dcd019c76931
thatanjan <anjan.cules@gmail.com>
2021-05-01 08:41:04 +0600

    only user reducer is kept and others are deleted

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 
 import userReducer from 'redux/slices/userSlice'
-import hasSeenReducer from 'redux/slices/hasSeenSlice'
 
 const store = configureStore({
 	reducer: {
 		user: userReducer,
-		hasSeen: hasSeenReducer,
 	},
 })
 
