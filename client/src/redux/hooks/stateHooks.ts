7496144e3b8edcf2355a8cedb01144217def9973
thatanjan <anjan.cules@gmail.com>
2021-05-06 17:05:09 +0600

    Hooks weren't really a function but a value. now it's a function. A new hook also created

--- client/src/redux/hooks/stateHooks.ts
+++ client/src/redux/hooks/stateHooks.ts
@@ -1,7 +1,8 @@
 import { useAppSelector } from './hooks'
 
-export const useUserID = useAppSelector(state => state.user.id)
+export const useUserID = () => useAppSelector(state => state.user.id)
 
-export const useProfileUserID = useAppSelector(
-	state => state.profile.profileUserID
-)
+export const useProfileUserID = () =>
+	useAppSelector(state => state.profile.profileUserID)
+
+export const useIsSelf = () => useAppSelector(state => state.profile.isSelf)
