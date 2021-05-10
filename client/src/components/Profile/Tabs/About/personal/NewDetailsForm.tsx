4d36cb1d738ccf39fc36fd7e8b542e3d3985253c
thatanjan <anjan.cules@gmail.com>
2021-05-10 08:46:49 +0600

    On updating personal NewDetailsForm, typing was laggin because of DatePicker. So that's removed. Only normal text field is used to take dateOfBirth. User need to manually type the date. But it will have validation and everything

--- client/src/components/Profile/Tabs/About/personal/NewDetailsForm.tsx
+++ client/src/components/Profile/Tabs/About/personal/NewDetailsForm.tsx
@@ -4,14 +4,11 @@ import { Formik, Form, Field } from 'formik'
 import Button from '@material-ui/core/Button'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Box from '@material-ui/core/Box'
-import { DatePicker } from 'formik-material-ui-pickers'
 import { TextField } from 'formik-material-ui'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import { MuiPickersUtilsProvider } from '@material-ui/pickers'
-import DayUtils from '@date-io/dayjs'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
 
 import CircularLoader from 'components/Loaders/CircularLoader'
@@ -32,35 +29,6 @@ const AutoExpandField = dynamic(
 )
 
 const SwrErrorAlert = dynamic(() => import('components/Alerts/SwrErrorAlert'))
-
-const fieldComponent = (value: string) => {
-	if (value === DATE_OF_BIRTH) {
-		return DatePicker
-	}
-
-	if (value === 'bio') {
-		return AutoExpandField
-	}
-
-	return TextField
-}
-
-const fieldComponentProps = (value: string) => {
-	if (value === DATE_OF_BIRTH) {
-		return {
-			autoOk: true,
-			orientatio: 'landscape',
-			variant: 'static',
-			openTo: 'date',
-		}
-	}
-
-	if (value === 'bio') {
-		return {}
-	}
-
-	return {}
-}
 
 interface Props {
 	setIsAdding: Function
@@ -93,9 +61,15 @@ const NewDetailsForm = ({ setIsAdding, isAdding }: Props) => {
 	const initialData = getPersonalData
 
 	personalDetailsField.forEach((item: string) => {
-		const value = initialData[`${item}`]
+		const value = initialData[item]
 		if (value === null) {
 			initialData[item] = ''
+		}
+
+		if (item === DATE_OF_BIRTH) {
+			const date = new Date(initialData[item])
+
+			initialData[item] = date.toLocaleDateString()
 		}
 	})
 
@@ -108,87 +82,93 @@ const NewDetailsForm = ({ setIsAdding, isAdding }: Props) => {
 		>
 			<DialogTitle id='form-dialog-title'>Update your information </DialogTitle>
 
-			<MuiPickersUtilsProvider utils={DayUtils}>
-				<Formik
-					initialValues={initialData}
-					validate={values => {
-						const errors: Partial<any> = {}
+			<Formik
+				initialValues={initialData}
+				validate={values => {
+					const errors: Partial<any> = {}
 
-						const { name, website } = values
+					const { name, website, dateOfBirth } = values
 
-						if (name.length < 6 || name.length > 30) {
-							errors.name = 'Name should be under 6 to 30 characters long'
-						}
+					if (name.length < 6 || name.length > 30) {
+						errors.name = 'Name should be under 6 to 30 characters long'
+					}
 
-						const validUrl = /^(ftp|http|https):\/\/[^ "]+$/.test(website)
+					const validUrl = /^(ftp|http|https):\/\/[^ "]+$/.test(website)
 
-						if (!validUrl) {
-							errors.website = 'Website url is not valid'
-						}
+					if (!validUrl) {
+						errors.website = 'Website url is not valid'
+					}
 
-						return errors
-					}}
-					onSubmit={async (values, { setSubmitting }) => {
-						// eslint-disable-next-line no-param-reassign
+					if (dateOfBirth === '') return errors
 
-						const FormValues = values
+					const date = new Date(dateOfBirth)
 
-						if (FormValues.dateOfBirth === '') {
-							delete FormValues.dateOfBirth
-						}
+					if (!(date instanceof Date && !Number.isNaN(date))) {
+						errors.dateOfBirth = "Invalid date. Try using 'mm-dd-yyyy'"
+					}
 
-						if (typeof FormValues.dateOfBirth === 'object') {
-							FormValues.dateOfBirth = FormValues.dateOfBirth.$d.toISOString()
-						}
+					return errors
+				}}
+				onSubmit={async (values, { setSubmitting }) => {
+					// eslint-disable-next-line no-param-reassign
 
-						const res = await createRequest({
-							key: updatePersonalData,
-							values: FormValues,
-						})
+					const FormValues = values
 
-						if (res) {
-							mutate()
+					if (FormValues.dateOfBirth === '') {
+						delete FormValues.dateOfBirth
+					}
 
-							setTimeout(() => {
-								setIsAdding(false)
-								setSubmitting(false)
-							}, 500)
-						}
-					}}
-				>
-					{({ submitForm, isSubmitting }) => (
-						<Form>
-							<DialogContent>
-								{personalDetailsField.map((item: string) => (
-									<Box key={item}>
-										<Field
-											className={inputStyle}
-											type='text'
-											component={fieldComponent(item)}
-											name={item}
-											label={generateField(item)}
-											placeholder={item}
-											{...fieldComponentProps(item)}
-										/>
-									</Box>
-								))}
+					FormValues.dateOfBirth = new Date(FormValues.dateOfBirth).toISOString()
 
-								<br />
-								{isSubmitting && <LinearProgress />}
-								<br />
-								<DialogActions>
-									<Button disabled={isSubmitting} onClick={handleClose} color='primary'>
-										Cancel
-									</Button>
-									<Button disabled={isSubmitting} onClick={submitForm} color='primary'>
-										Submit
-									</Button>
-								</DialogActions>
-							</DialogContent>{' '}
-						</Form>
-					)}
-				</Formik>
-			</MuiPickersUtilsProvider>
+					const res = await createRequest({
+						key: updatePersonalData,
+						values: FormValues,
+					})
+
+					if (res) {
+						mutate()
+
+						setTimeout(() => {
+							setIsAdding(false)
+							setSubmitting(false)
+						}, 500)
+					}
+				}}
+			>
+				{({ submitForm, isSubmitting }) => (
+					<Form>
+						<DialogContent>
+							{personalDetailsField.map((item: string) => (
+								<Box key={item}>
+									<Field
+										className={inputStyle}
+										type='text'
+										component={item === 'bio' ? AutoExpandField : TextField}
+										name={item}
+										label={generateField(item)}
+										placeholder={
+											item === DATE_OF_BIRTH ? "Use 'mm-dd-yyyy' format" : item
+										}
+									/>
+									<br />
+								</Box>
+							))}
+
+							<br />
+							{isSubmitting && <LinearProgress />}
+							<br />
+							<DialogActions>
+								<Button disabled={isSubmitting} onClick={handleClose} color='primary'>
+									Cancel
+								</Button>
+								<Button disabled={isSubmitting} onClick={submitForm} color='primary'>
+									Submit
+								</Button>
+							</DialogActions>
+						</DialogContent>{' '}
+					</Form>
+				)}
+			</Formik>
 		</Dialog>
 	)
 }
