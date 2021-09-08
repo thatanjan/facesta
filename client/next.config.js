13ee706c850a6d83d888122d389094df248b0154
thatanjan <anjan.cules@gmail.com>
2021-09-08 16:32:31 +0600

    cloudinary url will come from the environment variable

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,6 @@
 module.exports = {
 	images: {
 		loader: 'cloudinary',
-		path:
-			process.env.NODE_ENV === 'production'
-				? 'https://res.cloudinary.com/anjancules/'
-				: 'https://res.cloudinary.com/thatanjan/',
+		path: process.env.NEXT_PUBLIC_CLOUDINARY_URL,
 	},
 }
