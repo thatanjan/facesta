1806e4cc2436ba4ee367a1b4ebeb9bb74336a8af
thatanjan <thatanjan@gmail.com>
2021-10-05 15:53:19 +0600

    old way of getting post is removed. Only one resolver is added which is `getSinglePost`

--- server/src/graphql/Post/getPost.js
+++ server/src/graphql/Post/getPost.js
@@ -1,83 +1,34 @@
-import User from 'models/User'
-import createPostModel from 'models/Post'
+import Post from 'models/Post'
 import sendErrorMessage from 'utils/errorMessage'
-import { postProjection as projection } from 'variables/global'
-
-const SINGLE_POST = 'singlePost'
-const ALL_POST = 'allPost'
-
-const mainResolver = field => {
-	return async (_, { Input: { postID, user, skip } }, { user: authUser }) => {
-		try {
-			const Post = createPostModel(user)
-
-			const id = authUser?.id || ''
-
-			switch (field) {
-				case SINGLE_POST:
-					const post = await Post.findById(postID, {
-						...projection,
-						likes: { $elemMatch: { $eq: id } },
-					})
-
-					const userInfo = await User.findById(user, 'profile').populate({
-						path: 'profile',
-						select: 'profilePicture name',
-					})
-
-					const response = {
-						post: {
-							...post.toObject(),
-							user: userInfo,
-							hasLiked: post.likes.length === 1,
-						},
-					}
-
-					return response
-
-				case ALL_POST:
-					const allPost = {}
-					let newProjection = {}
-
-					if (id) {
-						newProjection = {
-							likes: { $elemMatch: { $eq: id } },
-						}
-					}
-
-					const posts = await Post.find({}, { ...newProjection, ...projection })
-						.skip(skip - 10)
-						.limit(10)
-						.sort({ _id: '-1' })
-
-					allPost.posts = posts.map(thePost => {
-						const newPost = thePost
-
-						if (id) {
-							newPost.hasLiked = newPost.likes.length === 1
-						}
-						return newPost
-					})
-
-					if (!posts) {
-						return sendErrorMessage('something went wrong')
-					}
-
-					return allPost
-				default:
-					return sendErrorMessage('nothing found')
-			}
-		} catch (error) {
-			return sendErrorMessage(error)
-		}
-	}
-}
+import {
+	populateObjectOfUser,
+	postProjection as projection,
+} from 'variables/global'
 
 const resolver = {
 	Query: {
-		getAllPost: mainResolver(ALL_POST),
-		getAllPostNoAuth: mainResolver(ALL_POST),
-		getSinglePost: mainResolver(SINGLE_POST),
+		getSinglePost: async (_, { Input: { postID } }, { user }) => {
+			try {
+				const authUserID = user.id || ''
+
+				let post = await Post.findById(postID, {
+					...projection,
+					likes: { $elemMatch: { $eq: authUserID } },
+				}).populate(populateObjectOfUser)
+
+				post = post.toObject()
+
+				if (post.likes.length) {
+					post.hasLiked = true
+				} else {
+					post.hasLiked = false
+				}
+
+				return { post }
+			} catch (e) {
+				return sendErrorMessage(e)
+			}
+		},
 	},
 }
 
