1963418abc8291bccd1508b72d48115bf98e746b
thatanjan <thatanjan@gmail.com>
2021-10-05 15:54:49 +0600

    file renamed   getPost.js -> getSinglePost.js

--- server/src/graphql/Post/Post.js
+++ server/src/graphql/Post/Post.js
@@ -2,7 +2,7 @@ import merge from 'lodash/merge'
 
 import PostType from 'graphql/Post/PostType'
 import createDeletePost from 'graphql/Post/createDeletePost'
-import getPost from 'graphql/Post/getPost'
+import getSinglePost from 'graphql/Post/getSinglePost'
 import likePost from 'graphql/Post/likePost'
 import commentPost from 'graphql/Post/commentPost'
 import editPost from 'graphql/Post/editPost'
@@ -14,7 +14,7 @@ export const PostTypedefs = [PostType]
 
 export const PostResolvers = merge(
 	createDeletePost,
-	getPost,
+	getSinglePost,
 	commentPost,
 	likePost,
 	editPost,
