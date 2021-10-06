769516e54fc8f8926d12147a627480f822ec4d1d
thatanjan <thatanjan@gmail.com>
2021-10-06 17:02:21 +0600

    Edit post resolver is updated because of Post model change. Also it will not update any image.

--- server/src/graphql/Post/editPost.js
+++ server/src/graphql/Post/editPost.js
@@ -1,42 +1,12 @@
-import createPostModel from 'models/Post'
+import Post from 'models/Post'
 import sendErrorMessage from 'utils/errorMessage'
 import sendMessage from 'utils/message'
-import uploadImage from 'utils/uploadToCloudinary'
-import deleteImage from 'utils/deleteImageFromCloudinary'
-import imageConfig from 'variables/cloudinaryVariables'
-import { postPath } from './createDeletePost'
 
 const resolvers = {
 	Mutation: {
-		editPost: async (_, { Input }, { user: { id } }) => {
+		editPost: async (_, { Input }) => {
 			try {
-				const PostModel = createPostModel(id)
-
-				const post = await PostModel.findById(Input.postID)
-
-				const { image } = Input
-
-				const oldImageID = post.image
-
-				if (oldImageID) {
-					await deleteImage(oldImageID)
-				}
-
-				if (image) {
-					const imagePublicID = await uploadImage(image, {
-						folder: postPath(id),
-						...imageConfig,
-					})
-
-					if (!imagePublicID || typeof imagePublicID !== 'string')
-						return sendErrorMessage('Could not update the profile')
-
-					post.image = imagePublicID
-				}
-
-				if (image === '') {
-					post.image = ''
-				}
+				const post = await Post.findById(Input.postID, 'text title markdown')
 
 				const inputKeys = Object.keys(Input)
 
