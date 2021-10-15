3b43eff7682ab37f98ea1196ac4441f60f3e5095
thatanjan <thatanjan@gmail.com>
2021-10-15 09:17:23 +0600

    Authorization and authentication added to the settings page

--- client/src/pages/settings/index.tsx
+++ client/src/pages/settings/index.tsx
@@ -6,9 +6,22 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
 import Button from '@material-ui/core/Button'
 import DeleteIcon from '@material-ui/icons/Delete'
+import { GetServerSideProps } from 'next'
+import { NextSeo } from 'next-seo'
 
-import InDevelopmentMenu from 'components/Menus/InDevelopmentMenu'
 import PageLayoutComponent from 'components/Layout/PageLayoutComponent'
+import InDevelopmentMenu from 'components/Menus/InDevelopmentMenu'
+import Requset from 'interfaces/requsetResponse'
+import shouldRedirectToAuth from 'utils/shouldRedirectToAuth'
+import decodeToken from 'utils/decodeToken'
+import getToken from 'utils/getToken'
+import createRedirectObject from 'utils/createRedirectObject'
+import { LOGIN_URL, APP_NAME } from 'variables/global'
+import CreatePost from 'components/Post/CreatePost/CreatePost'
+import NewsFeed from 'components/Post/NewsFeed'
+import { PageProps } from 'interfaces/global'
+
+import useStoreID from 'redux/hooks/useStoreID'
 
 const useStyles = makeStyles((theme: Theme) => ({
 	buttonStyle: {
@@ -64,7 +77,8 @@ const Content = () => {
 	)
 }
 
-const SettingsPage = () => {
+const SettingsPage = ({ id }: PageProps) => {
+	useStoreID(id)
 	return (
 		<>
 			<PageLayoutComponent Content={Content} />
@@ -73,3 +87,15 @@ const SettingsPage = () => {
 }
 
 export default SettingsPage
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+	const token = getToken(req as Requset)
+
+	const shouldRedirect = await shouldRedirectToAuth(token)
+
+	if (shouldRedirect) return createRedirectObject(LOGIN_URL)
+
+	const { id } = decodeToken(token)
+
+	return { props: { id } }
+}
