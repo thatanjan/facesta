a48fa59b68aa30947fc7848e1eced264b3e3cb29
thatanjan <thatanjan@gmail.com>
2021-10-19 23:55:51 +0600

    Post content now have show more button if the it has multiline or first line has more than 300chars

--- client/src/components/Post/PostContent.tsx
+++ client/src/components/Post/PostContent.tsx
@@ -25,37 +25,35 @@ const PostContentForPage = ({ text }: { text: string }) => {
 }
 
 const PostContent = ({ postPage, showMoreLink, text }: Props) => {
-	const idealLength = 200
+	const idealLength = 300
 
 	const visibleText = text.substr(0, idealLength - 1)
-
-	const isThereMore = text.length > idealLength
-
 	const paragraphs = visibleText.split('\n')
+
+	const isThereMore = paragraphs.length > 1 || text.length > idealLength
 
 	return (
 		<CardContent>
 			{!postPage && (
 				<>
-					{paragraphs.map(paragraph => (
-						<Typography key={nanoid()} variant='body1' component='p'>
-							{paragraph}
-						</Typography>
-					))}
-
-					{isThereMore && (
-						<>
-							<span> ...</span>{' '}
-							<MuiLink
-								href={showMoreLink}
-								MuiComponent={Typography}
-								variant='body1'
-								align='center'
-							>
-								show more
-							</MuiLink>
-						</>
-					)}
+					<Typography>
+						{' '}
+						{paragraphs[0]}
+						{isThereMore && (
+							<>
+								<span> ...</span>{' '}
+								<MuiLink
+									href={showMoreLink}
+									MuiComponent={Typography}
+									variant='body1'
+									align='center'
+									style={{ display: 'inline' }}
+								>
+									show more
+								</MuiLink>
+							</>
+						)}
+					</Typography>
 				</>
 			)}
 
